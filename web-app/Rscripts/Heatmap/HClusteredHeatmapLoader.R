4c1d19f7b288e4cf4c4ad005e84c2c57fb61db56
Sai Munikuntla <saikumar.munikuntla@gmail.com>
2012-05-31 17:13:15 -0400

    Fix for HClustered Heatmap.

--- web-app/Rscripts/Heatmap/HClusteredHeatmapLoader.R
+++ web-app/Rscripts/Heatmap/HClusteredHeatmapLoader.R
@@ -20,25 +20,29 @@
 
 HClusteredHeatmap.loader <- function(
 input.filename,
-output.file ="Heatmap"
+output.file ="Heatmap",
+imageWidth = 1200,
+imageHeight = 800,
+pointsize = 15
 )
 {
 
 	library(Cairo)
 	library(ggplot2)
 	library(gplots)
+	library(reshape2)
 	
 	#Pull the GEX data from the file.
 	mRNAData <- data.frame(read.delim(input.filename))
 
 	#Trim the patient.id field.
-	mRNAData$PATIENT_NUM <- gsub("^\\s+|\\s+$", "",mRNAData$PATIENT_NUM)	
+	mRNAData$PATIENT_NUM <- gsub("^\\s+|\\s+$", "",mRNAData$PATIENT_NUM)
 	
 	#Melt the data, leaving 3 columns as the grouping fields.
 	meltedData <- melt(mRNAData, id=c("GROUP","PATIENT_NUM"))
 
 	#Cast the data into a format that puts the ASSAY.ID in a column.
-	castedData <- data.frame(cast(meltedData, GROUP ~ PATIENT_NUM))
+	castedData <- data.frame(dcast(meltedData, GROUP ~ PATIENT_NUM))
 
 	#Set the name of the rows to be the names of the probes.
 	rownames(castedData) = castedData$GROUP
@@ -50,15 +54,18 @@ output.file ="Heatmap"
 	matrixData <- data.matrix(subset(castedData, select = -c(GROUP)))
 	
 	#We can't draw a heatmap for a matrix with only 1 row.
-	if(nrow(matrixData)<2) stop("||FRIENDLY||R cannot plot a heatmap with only 1 row. Please check your variable selection and run again.")
+	if(nrow(matrixData)<2) stop("||FRIENDLY||R cannot plot a heatmap with only 1 Gene/Probe. Please check your variable selection and run again.")
+	if(ncol(matrixData)<2) stop("||FRIENDLY||R cannot plot a heatmap with only 1 Patient data. Please check your variable selection and run again.")
 	
 	#Prepare the package to capture the image file.
-	CairoPNG(file=paste(output.file,".png",sep=""),width=1200,height=800)
+	CairoPNG(file=paste(output.file,".png",sep=""),width=as.numeric(imageWidth),height=as.numeric(imageHeight),pointsize=as.numeric(pointsize))
+	
+	colorPanelList <- colorpanel(100,low="green",mid="black",high="red")
 	
 	#Store the heatmap in a temp variable.
 	tmp <- heatmap(	matrixData,
-					col=redgreen(100),
-					margins=c(13,10),
+					col=colorPanelList,
+					margins=c(18,18),
 					cexRow=1.5,
 					cexCol=1.5)		
 		
