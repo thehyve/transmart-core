ab641313391a661b8064e102e92b94e245a1f044
Gustavo Lopes <gustavo@thehyve.nl>
2013-02-28 15:08:23 +0100

    Initial commit

---
+++ src/test/groovy/org/transmartproject/core/HelperTest.groovy
@@ -0,0 +1,19 @@
+//
+// Generated from archetype; please customize.
+//
+
+package org.transmartproject.core
+
+import org.transmartproject.core.Helper
+import org.transmartproject.core.Example
+
+/**
+ * Tests for the {@link Helper} class.
+ */
+class HelperTest
+    extends GroovyTestCase
+{
+    void testHelp() {
+        new Helper().help(new Example())
+    }
+}
