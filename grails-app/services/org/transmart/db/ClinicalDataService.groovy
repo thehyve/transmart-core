b161bff49e5985cd72b533e74ff7838b3b660fb1
Gustavo Lopes <gustavo@thehyve.nl>
2013-03-01 10:00:05 +0100

    Fix compile error

--- grails-app/services/org/transmart/db/ClinicalDataService.groovy
+++ grails-app/services/org/transmart/db/ClinicalDataService.groovy
@@ -4,7 +4,7 @@ import org.transmart.core.api.*
 
 class ClinicalDataService implements ClinicalDataAPI {
 
-    def getSamples(Trial trial) {
-        return new HashSet<Sample>()
+    Collection<Sample> getSamples(Trial trial) {
+        Collections.emptyList()
     }
 }
