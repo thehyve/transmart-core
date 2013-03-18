de8c158dd8ab123ff1d56e7e7ff8ee88fcadfefa
Gustavo Lopes <gustavo@thehyve.nl>
2013-03-18 14:50:25 +0100

    Test in-memory db has to include schema creation
    Updated example in DataSource.groovy

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -20,24 +20,9 @@ environments {
     /*test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-        }
-    }*/
-    /*production {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
+            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;INIT=" +
+                    "CREATE SCHEMA IF NOT EXISTS i2b2metadata\\;"
+                    "CREATE SCHEMA IF NOT EXISTS i2b2demodata\\;"
         }
     }*/
 }
