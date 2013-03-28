f68d1c5097a18e33d743c1ab50deb02f2a3313b7
Gustavo Lopes <gustavo@thehyve.nl>
2013-03-28 15:26:57 +0100

    Add two methods to QueriesResource
    These are:
      - QueryResult getQueryResultFromId(Long id)
      - QueryDefinition getQueryDefinitionForResult(QueryResult result)
    
    See their Javadoc blocks for more information.

--- src/main/groovy/org/transmartproject/core/querytool/QueriesResource.groovy
+++ src/main/groovy/org/transmartproject/core/querytool/QueriesResource.groovy
@@ -1,6 +1,7 @@
 package org.transmartproject.core.querytool
 
 import org.transmartproject.core.exceptions.InvalidRequestException
+import org.transmartproject.core.exceptions.NoSuchResourceException
 
 /**
  *
@@ -14,5 +15,24 @@ interface QueriesResource {
      * @return the resulting query result
      */
     QueryResult runQuery(QueryDefinition definition) throws InvalidRequestException
+
+    /**
+     * Fetches a {@link QueryResult} using its id.
+     *
+     * @param id the id of the query result to be fetched
+     * @return the query result requested
+     * @throws NoSuchResourceException if the there is no query result with
+     * the given id
+     */
+    QueryResult getQueryResultFromId(Long id) throws NoSuchResourceException
+
+    /**
+     * Fetches the {@link QueryDefinition} used to obtain the passed in result.
+     *
+     * @param result the result whose generating definition is requested
+     * @return the requested definition
+     * @throws NoSuchResourceException if the definition cannot be found
+     */
+    QueryDefinition getQueryDefinitionForResult(QueryResult result) throws NoSuchResourceException
 
 }
