87498456b422b9c97f2d7cf728d61b0668f24513
Gustavo Lopes <gustavo@thehyve.nl>
2013-05-03 16:11:59 +0200

    Add session param to ::runACGHRegionQuery

--- src/main/groovy/org/transmartproject/core/dataquery/DataQueryResource.groovy
+++ src/main/groovy/org/transmartproject/core/dataquery/DataQueryResource.groovy
@@ -8,5 +8,13 @@ import org.transmartproject.core.dataquery.acgh.RegionResult
  */
 public interface DataQueryResource {
 
-    RegionResult runACGHRegionQuery(ACGHRegionQuery spec)
+    /**
+     * Runs a query for aCHG region data. Optionally,  it can be run on a
+	 * separate session; a stateless session may be useful here.
+     *
+     * @param spec the specification of the query
+     * @param session session where to run the query or null to use current
+     * @return the query result result
+     */
+    RegionResult runACGHRegionQuery(ACGHRegionQuery spec, session)
 }
