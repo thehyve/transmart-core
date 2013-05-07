a8bca5d47c4a2a446bc48b215dca77cf35d0d3a2
riza <rnugraha@gmail.com>
2013-05-07 12:02:54 +0200

    refactor overrides to different js file

--- grails-app/controllers/com/recomdata/transmart/data/association/DataAssociationController.groovy
+++ grails-app/controllers/com/recomdata/transmart/data/association/DataAssociationController.groovy
@@ -39,6 +39,7 @@ class DataAssociationController {
 	def loadScripts = {
 		def scripts = [servletContext.contextPath+pluginContextPath+'/js/dataAssociation.js', 
 		servletContext.contextPath+pluginContextPath+'/js/PDFGenerator.js',
+		servletContext.contextPath+pluginContextPath+'/js/ext/tsmart-overrides.js',
 		servletContext.contextPath+pluginContextPath+'/js/ext/tsmart-generic.js',
 		servletContext.contextPath+pluginContextPath+'/js/plugin/SurvivalAnalysis.js',
 		servletContext.contextPath+pluginContextPath+'/js/plugin/SurvivalAnalysisaCGH.js',
