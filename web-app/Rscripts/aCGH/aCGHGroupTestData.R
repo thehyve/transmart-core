f12d453c557db3ffa7341a160fde0cd5f839fa53
Tim Hulsen <tim.hulsen@philips.com>
2013-05-10 15:39:35 +0200

    Update aCGHGroupTestData.R

--- web-app/Rscripts/aCGH/aCGHGroupTestData.R
+++ web-app/Rscripts/aCGH/aCGHGroupTestData.R
@@ -15,25 +15,29 @@
 ###########################################################################
 
 ###########################################################################
-#aCGHgroupTestDataFile
+#aCGHBuildGroupTestDataFile
 #Parse the i2b2 output file and create input files for Group Test.
 ###########################################################################
 
 aCGHGroupTestData.build <- 
 function
 (
-  input.dataFile,
-  input.acghFile,
-  output.dataFile="input",
-  concept.time,
-  concept.category = "",
-  concept.eventNo = ""
+  input.dataFile='clinical.txt',
+  input.acghFile='aCGH.txt',
+  concept.category,
+  concept.group,
+  output.column.group='group',
+  output.dataFile='phenodata.tsv',
+  output.acghFile='regions.tsv'                      
 )
 {
-  print("-------------------")
-	print("aCGHGroupTestData.R")
+	print("-------------------")
+	print("aCGHBuildGroupTestData.R")
 	print("BUILDING ACGH GROUP TEST DATA")
-	
+
+	# Copy the aCGH file
+	file.copy(input.acghFile,output.acghFile,overwrite = TRUE)
+	  
 	#Read the input file.
 	dataFile <- data.frame(read.delim(input.dataFile));
 	
@@ -50,49 +54,26 @@ function
 	colnames(finalData) <- c("PATIENT_NUM")
 	
 	#Add the value for the time to the final data.
-	finalData<-merge(finalData,splitData[[gsub("([\\])","\\\\\\\\",concept.time)]][c('PATIENT_NUM','VALUE')],by="PATIENT_NUM")
-	
-	#If eventNo was not specified, we consider everyone to have had the event.
-	if(concept.eventNo=="")
-	{
-		finalData<-cbind(finalData,0)
-	}	else {
-		#We merge the eventNo in, everything else gets set to NA. We will mark them as censored later.
-		eventNo<-strsplit(gsub("([\\])","\\\\\\\\",concept.eventNo)," [|] ")
-    if(length(eventNo[[1]])>1) {
-      #Multiple eventNo
-		  boundData<-rbind(splitData[eventNo[[1]][1]][[1]],splitData[eventNo[[1]][2]][[1]])
-    } else {
-      #Single eventNo
-      boundData<-splitData[eventNo[[1]][1]][[1]]
-    }
-		finalData<-merge(finalData,boundData[c('PATIENT_NUM','VALUE')],by="PATIENT_NUM",all.x=TRUE)	
+	#group<-strsplit(gsub("([\\])","\\\\\\\\",concept.group)," [|] ")
+	group<-strsplit(concept.group," [|] ")
+	if(length(group[[1]])>1) {
+	#Multiple groups
+	  boundData<-rbind(splitData[group[[1]][1]][[1]],splitData[group[[1]][2]][[1]])
+	} else {
+	#Single eventNo
+	  boundData<-splitData[group[[1]][1]][[1]]
 	}
+	finalData<-merge(finalData,boundData[c('PATIENT_NUM','VALUE')],by="PATIENT_NUM",all.x=TRUE)	
 	
-	#This is the list of columns for the final data object.
-	finalColumnNames <- c("PATIENT_NUM","TIME","CENSOR") #,"CATEGORY")
-	
-	if("GROUP" %in% colnames(finalData)) finalColumnNames <- c(finalColumnNames,'GROUP')
-	
-	#Rename the columns.
+	finalColumnNames <- c("PATIENT_NUM",output.column.group)
 	colnames(finalData) <- finalColumnNames
-	
-	#Make sure we have the value levels for the CENSOR column. This may throw a warning for duplicate values, but we can ignore it.
-	finalData$CENSOR <- factor(finalData$CENSOR, levels = c(levels(finalData$CENSOR), "1"))
-	finalData$CENSOR <- factor(finalData$CENSOR, levels = c(levels(finalData$CENSOR), "0"))
-
-	#Replace the NA values in the CENSOR column with 0 (Censored).
-	finalData$'CENSOR'[is.na(finalData$'CENSOR')] <- 0
-	
-	#Everything that isn't a 0 in the CENSOR column needs to be a 1 (Event happened).
-	finalData$'CENSOR'[!finalData$'CENSOR'=='0'] <- 1		
-	
+	  
 	###################################	
 	
 	#We need MASS to dump the matrix to a file.
 	require(MASS)
 	
 	#Write the final data file.
-	write.matrix(finalData,"outputfile",sep = "\t")
+	write.matrix(finalData,output.dataFile,sep = "\t")
 	print("-------------------")
 }
