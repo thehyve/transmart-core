d2f581b66b88322f0699167adcbf07e009253989
Gustavo Lopes <gustavo@thehyve.nl>
2013-05-17 15:27:12 +0200

    Use CGHtestpar; fix bug when quotes in regions.tsv

--- web-app/Rscripts/aCGH/acgh-survival-test.R
+++ web-app/Rscripts/aCGH/acgh-survival-test.R
@@ -19,16 +19,27 @@ acgh.survival.test <- function
 )
 {
 
-  dat <- read.table('regions.tsv', header=TRUE, sep='\t', quote='', as.is=TRUE, check.names=FALSE)
+  dat <- read.table('regions.tsv', header=TRUE, sep='\t', as.is=TRUE, check.names=FALSE)
   phenodata <- read.table('phenodata.tsv', header=TRUE, sep='\t', check.names=FALSE)
-  
+
   first.data.col <- min(grep('^chip\\.', names(dat)), grep('^flag\\.', names(dat)))
   data.info <- dat[,1:(first.data.col-1)]
   calls <- as.matrix(dat[,grep('^flag\\.', colnames(dat))])
 
-  library(CGHtest)
-  pvs <-  pvalstest_logrank(calls, data.info, dataclinvar=phenodata, whtime=which(colnames(phenodata) == survival), whstatus=which(colnames(phenodata) == status), lgonly=as.integer(test.aberrations), niter=number.of.permutations)
-  fdrs <- fdrperm(pvs)
+  # first try parallel computing
+  prob <- TRUE
+  try({
+    library(CGHtestpar)
+    pvs <-  pvalstest_logrank(calls, data.info, dataclinvar=phenodata, whtime=which(colnames(phenodata) == survival), whstatus=which(colnames(phenodata) == status), lgonly=as.integer(test.aberrations), niter=number.of.permutations, ncpus=4)
+    fdrs <- fdrperm(pvs)
+    prob <- FALSE
+  }, silent=TRUE)
+  # if problems, fall back to sequential computing
+  if (prob) {
+    library(CGHtest)
+    pvs <-  pvalstest_logrank(calls, data.info, dataclinvar=phenodata, whtime=which(colnames(phenodata) == survival), whstatus=which(colnames(phenodata) == status), lgonly=as.integer(test.aberrations), niter=number.of.permutations)
+    fdrs <- fdrperm(pvs)
+  }
 
   fdrs <- cbind(fdrs, dat[,first.data.col:ncol(dat)])
 
