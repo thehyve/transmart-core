bbf76fc49e90bde4bb345f3dc2e337f78988f755
Gustavo Lopes <gustavo@thehyve.nl>
2013-08-01 18:47:05 +0200

    Add macroed functions

---
+++ ddl/postgres/manual_objects_list.php
@@ -0,0 +1,15 @@
+<?php
+$manual_objects = [
+    'tm_cz' => [
+        ['FUNCTION', 'rwg_add_search_term'],
+        ['FUNCTION', 'rwg_add_taxonomy_term'],
+        ['FUNCTION', 'rwg_create_analysis_entry'],
+        ['FUNCTION', 'rwg_import_from_ext'],
+        ['FUNCTION', 'rwg_load_analysis_data'],
+        ['FUNCTION', 'rwg_load_analysis_metadata'],
+        ['FUNCTION', 'rwg_load_heat_map_results'],
+        ['FUNCTION', 'rwg_remove_study'],
+    ],
+];
+
+// vim: et ts=4 sw=4:
