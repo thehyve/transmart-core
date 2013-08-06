898af062dbac787f2a5bf6ca36b8da3fe1d9fac7
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2013-08-06 23:46:54 +0200

    Add extra core to solr for sample explorer

---
+++ solr/data-config/postgres/data-config_sample.xml.php
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<dataConfig>
+  <dataSource name="ds1" driver="org.postgresql.Driver"
+              url="jdbc:postgresql://<?= $_ENV['PGHOST'] ?>:<?= $_ENV['PGPORT'] ?>/<?= $_ENV['PGDATABASE'] ?>"
+              user="biomart_user" password="biomart_user" readOnly="true" />
+  <document>
+    <entity name="id" query="select sample_id,trial_name,disease,tissue_type,data_types,biobank,source_organism,sample_treatment,subject_treatment from i2b2DemoData.sample_categories">
+      <field name="id" column="sample_id"/>
+      <field name="DataSet" column="trial_name"/>
+      <field name="Pathology" column="disease"/>
+      <field name="Tissue" column="tissue_type"/>
+      <field name="DataType" column="data_types"/>
+      <field name="BioBank" column="bobank"/>
+      <field name="Source_Organism" column="source_organism"/>
+      <field name="Subject_Treatment" column="subject_treatment"/>
+      <field name="Sample_Treatment" column="sample_treatment"/>
+    </entity>
+  </document>
+</dataConfig>
