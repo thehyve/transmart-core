7a6a4c287f8ba69873a675e2ff061244aa461622
Ruslan Forostianov <ruslan@thehyve.nl>
2013-08-06 11:30:19 +0200

    [FT-319] Implement architectural decision, on how to retrieve data from back-end (genome browser)
    Transmart changes to support genome browser

--- src/main/groovy/org/transmartproject/core/dataquery/DataQueryResource.groovy
+++ src/main/groovy/org/transmartproject/core/dataquery/DataQueryResource.groovy
@@ -1,7 +1,9 @@
 package org.transmartproject.core.dataquery
 
+import org.transmartproject.core.dataquery.acgh.ChromosomalSegment
 import org.transmartproject.core.dataquery.constraints.ACGHRegionQuery
 import org.transmartproject.core.dataquery.acgh.RegionResult
+import org.transmartproject.core.dataquery.acgh.Region
 
 /**
  * A resource for querying about clinical and high-dimensional data.
@@ -17,4 +19,13 @@ public interface DataQueryResource {
      * @return the query result result
      */
     RegionResult runACGHRegionQuery(ACGHRegionQuery spec, session)
+
+    /**
+     * Retrieve chromosomal regions
+     * @param spec the specification of the query
+     * @return Segment for each chromosome where start = min(region.start)
+     * and stop = max(region.stop)
+     */
+    List<ChromosomalSegment> getChromosomalSegments(ACGHRegionQuery spec)
+
 }
