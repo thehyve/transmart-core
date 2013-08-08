ca64de713c6f146bbef8b892c0f8697d9a505259
Ruslan Forostianov <ruslan@thehyve.nl>
2013-08-08 11:43:39 +0200

    [FT-319] Code simplification.
    Use `between` hql operator instead of  `<=` and `>=`

--- grails-app/services/org/transmartproject/db/dataquery/DataQueryResourceService.groovy
+++ grails-app/services/org/transmartproject/db/dataquery/DataQueryResourceService.groovy
@@ -103,8 +103,8 @@ class DataQueryResourceService implements DataQueryResource {
                 if(segment.start && segment.end) {
                     params["start$indx"] = segment.start
                     params["end$indx"] = segment.end
-                    subClauses << "((region.start >= :start$indx and region.start <= :end$indx)" +
-                            " or (region.end >= :start$indx and region.end <= :end$indx)" +
+                    subClauses << "(region.start between :start$indx and :end$indx" +
+                            " or region.end between :start$indx and :end$indx" +
                             " or (region.start < :start$indx and region.end > :end$indx))"
                 }
                 regionsWhereClauses << "(${subClauses.join(' and ')})"
