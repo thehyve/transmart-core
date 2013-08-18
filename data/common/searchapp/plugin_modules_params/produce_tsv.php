24a2a4569d8ba71d3cbfcefa2809df90c38460a2
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2013-08-19 01:42:36 +0200

    Fix bug in produce_tsv.php
    Due to odd behavior of fputcsv(). See comment in code.

--- data/common/searchapp/plugin_modules_params/produce_tsv.php
+++ data/common/searchapp/plugin_modules_params/produce_tsv.php
@@ -1,5 +1,21 @@
 <?php
 
+function myfputcsv($stream, $data) {
+	//php's fputcsv interprets the backslash as an escape character that, when
+	//present before the enclosure, inhibits the duplication of the enclosure
+	//as a means of escaping it
+	//however, it is not an actual "escape" because a sequence \" will still
+	//mean the same literally, not just ", unlike "" which is the escape
+	//sequence for ". If then \ ia thens given as the escape character to
+	//fgetcsv, the sequence \" will be inerpreted as a literal \" instead of
+	//having the " end the enclosed sequence.
+	//This behavior doesn't seem very useful and postgres can't deal with it
+
+	fwrite($stream, implode("\t", array_map(function($field) {
+		return '"' . str_replace('"', '""', $field) . '"';
+	}, $data)) . "\n");
+}
+
 while (($data = fgetcsv(STDIN, 0, "\t", '"', '"')) !== false) {
 	$filename = $data[9] /* module_name */ . ".params";
 
@@ -8,5 +24,5 @@ while (($data = fgetcsv(STDIN, 0, "\t", '"', '"')) !== false) {
 		$data[3] = json_encode($data[3], JSON_UNESCAPED_SLASHES);
 	}
 
-	fputcsv(STDOUT, $data, "\t", '"');
+	myfputcsv(STDOUT, $data);
 }
