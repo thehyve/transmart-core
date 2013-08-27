c949edbfd8a726dfc46c010f7a6220f052bf5871
Wim van der Linden <wim.van.der.linden@philips.com>
2013-08-27 11:14:44 +0200

    New RNASeq group test advanced analysis

---
+++ web-app/Rscripts/RNASeq/RNASeq-Build-DEanalysisData.R
@@ -0,0 +1,105 @@
+###########################################################################
+# Copyright 2008-2012 Janssen Research & Development, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###########################################################################
+
+###########################################################################
+#RNASeq-Build-DEanalysisData
+#Parse the i2b2 output file and create input files for RNASeq-EdgeR-DEanalysis.
+###########################################################################
+
+RNASeqDEanalysisData.build <- 
+function
+(
+  input.clinicalFile='clinical.txt',
+  input.rnaseqFile='RNASeq.txt',
+  concept.readcount,
+  concept.group,
+  output.column.group='group',
+  output.clinicalFile='phenodata.tsv',
+  output.rnaseqFile='readcount.tsv'                      
+)
+{
+	print("-------------------")
+	print("RNASeq-Build-DEanalysisData.R")
+	print("BUILDING RNASeq DEanalysis DATA")
+
+	# Check parameters
+	if( missing(concept.group) || is.null(concept.group) || nchar(concept.group) == 0) stop("||FRIENDLY||No grouping specified. Please check your group variable selection and run again.")
+
+	# Check presence of rnaseq data file and clinical data file
+	if(!file.exists(input.rnaseqFile)) stop("||FRIENDLY||No RNASeq data file found. Please check your region variable selection and run again.")
+	if(!file.exists(input.clinicalFile)) stop("||FRIENDLY||No clinical data file found. Please check your group variable selection and run again.")
+
+	# Copy the RNAseq file
+	file.copy(input.rnaseqFile,output.rnaseqFile,overwrite = TRUE)
+	
+	# Extract patient list from RNASeq data column names for which readcounts have been observed
+	headernames <- gsub("\"", "", strsplit(readLines(input.rnaseqFile,1),' *\t *')[[1]])
+	
+	#pids <- sub("readcount.", "" , headernames[grep('readcount.', headernames)] )
+	pids <- headernames
+	if(!length(pids)>0) stop("||FRIENDLY||No subjects with readcount data found in RNASeq data file.")
+
+	#Read the input file.
+	clinicaldataFile <- read.table(input.clinicalFile, header=TRUE, sep='\t', quote='\"', strip.white=TRUE, as.is=TRUE, check.names=FALSE)
+
+	#Set the column names.
+	colnames(clinicaldataFile) <- c("PATIENT_NUM","SUBSET","CONCEPT_CODE","CONCEPT_PATH_SHORT","VALUE","CONCEPT_PATH")
+
+	#Filter on patients for which RNASeq data is available
+	filteredData <- matrix(pids)
+	colnames(filteredData) <- c("PATIENT_NUM")
+	filteredData <- merge(filteredData, clinicaldataFile)
+
+	#Split the data by the CONCEPT_CD.
+	splitData <- split(filteredData,filteredData$CONCEPT_PATH)
+	
+	#List of available CONCEPT_PATH values to check availability of concepts specified as arguments
+	allConcepts <- unique(filteredData$CONCEPT_PATH)
+
+	#Add the value for the group to the group data table.
+	group <- strsplit(concept.group," *[|] *")
+	
+	# Check if at least one of the censor concepts is observed
+	if (! any(group[[1]] %in% allConcepts)) stop(paste("||FRIENDLY||No observations found for group variable:",group[[1]]))
+
+	groupData <- splitData[group[[1]][1]][[1]]
+	if(length(group[[1]])>1) {
+		#Multiple groups
+		for (i in 2:length(group[[1]]) )
+		{
+			groupData<-rbind(groupData,splitData[group[[1]][i]][[1]])
+		}
+	}
+
+	groupData <- groupData[c('PATIENT_NUM','VALUE')]
+	
+	# Check if patient are uniquely divided over the groups
+	if (nrow(groupData) != length(unique(groupData$PATIENT_NUM))) stop(paste("||FRIENDLY||Patients not uniquely divided over the groups"))
+	# Check size of groupsize on average > 1 (i.e. not as many groups as patients)
+	if (nrow(groupData) == length(unique(groupData$VALUE))) stop(paste("||FRIENDLY||Size of groups too small (as many groups as patients)"))
+
+	groupColumnNames <- c("PATIENT_NUM",output.column.group)
+	colnames(groupData) <- groupColumnNames
+
+	###################################	
+	#We need MASS to dump the matrix to a file.
+	require(MASS)
+	
+	#Write the group data file.
+	write.table(groupData,output.clinicalFile,sep = "\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
+
+	print("-------------------")
+}
