06aabe8dc9555398d24f11a902941e628e42186e
Peter Rice <p.rice@imperial.ac.uk>
2013-09-06 16:21:39 +0100

    max number of rows option

--- web-app/Rscripts/Heatmap/KMeansHeatmap.R
+++ web-app/Rscripts/Heatmap/KMeansHeatmap.R
@@ -25,9 +25,14 @@ clusters.number = 2,
 probes.aggregate = false,
 imageWidth = 1200,
 imageHeight = 800,
-pointsize = 15
+pointsize = 15,
+maxDrawNumber = 50
 )
 {
+	print("-------------------")
+	print("KMeansHeatmap.R")
+	print("CREATING K-MEANS HEATMAP")
+
 	library(Cairo)
 	library(reshape2)
 	library(gplots)
@@ -59,39 +64,62 @@ pointsize = 15
 	colnames(castedData) <- sub("^X","",colnames(castedData))	
 	
 	#Convert data to an integer matrix.
+	print("Convert data to an integer matrix")
 	matrixData <- data.matrix(subset(castedData, select = -c(GROUP)))
-	
+
+# by Serge and Wei to filter a sub set and reorder markers
+
+        matrixData <- matrixData[!apply(is.na(matrixData),1,any), ]				# remove rows with NA
+
+        num_markers<-dim(matrixData)[1]                                                         # number of markers in the dataset
+        if (num_markers > maxDrawNumber) {                                                      # if more markers in the dataset, apply filter
+                sd_rows_matrix<-apply (matrixData,1,sd,na.rm=T)
+                matrixData<-matrixData[!is.na(sd_rows_matrix),]                                 # remove markers where sd is NA
+                sd_rows_matrix<-sd_rows_matrix[!is.na(sd_rows_matrix)]
+                cutoff_sd<- sd_rows_matrix[order(sd_rows_matrix,decreasing = T)][maxDrawNumber+1] # filter by SD, draw only the top maxDrawNumber
+                matrixData<-matrixData[sd_rows_matrix>cutoff_sd,]
+        }
+
 	#Transpose the matrix to put the sample column into a row.
+	print("Transpose the matrix to put the sample column into a row")
 	transposedMatrixData <- t(matrixData)
 	
 	#Make sure the number of clusters is applicable.
 	if(clusters.number >= nrow(transposedMatrixData)) stop(paste("||FRIENDLY||The number of clusters must fall between 1 and the number of subjects in your data. Your data only has ",as.character(nrow(transposedMatrixData))," subjects"))
 	
 	#Create the kmeans object. We cluster by columns.
+	print("Create the kmeans object. We cluster by columns")
 	kMeansObject <- kmeans(transposedMatrixData,centers=clusters.number)
 	
 	#We want to merge the cluster names back into our data set.
+	print("Merge the cluster names back into our data set")
 	dataWithCluster <- data.frame(transposedMatrixData,cluster=kMeansObject$cluster)
 	
 	#Order the data on the cluster column.
+	print("Order the data on the cluster column")
 	dataWithCluster <- dataWithCluster[order(dataWithCluster$cluster),]
 	
 	#Create a function that will help us decide which color to draw above the heatmap.
+	print("Create a function that will help us decide which color to draw above the heatmap")
 	color.map <- function(clusterNumber) { if (clusterNumber %% 2 == 0 ) "#8B8989" else "#5C3317" }
 	
 	#Use the function to create a list with a color for each patient.
+	print("Use the function to create a list with a color for each patient")
 	patientcolors <- unlist(lapply(dataWithCluster$cluster, color.map))
 	
 	#Remove the cluster column.
+	print("Remove the cluster column")
 	dataWithCluster <- subset(dataWithCluster, select = -c(cluster))	
 	
 	#Transpose the data back.
+	print("Transpose the data back")
 	dataWithCluster <- t(dataWithCluster)
 	
 	#If we didn't aggregate the probes the rownames are probe IDs, which are usually numeric. Strip the X from them here.
 	rownames(dataWithCluster) <- sub("^X","",rownames(dataWithCluster))	
 	
 	#Push the data back to a matrix so we can plot it.
+	print("Push the data back to a matrix so we can plot it")
 	matrixData <- data.matrix(dataWithCluster)
 	
 	#We can't draw a heatmap for a matrix with only 1 row.
@@ -101,9 +129,10 @@ pointsize = 15
 	#Prepare the package to capture the image file.
 	CairoPNG(file=paste(output.file,".png",sep=""),width=as.numeric(imageWidth),height=as.numeric(imageHeight),pointsize=as.numeric(pointsize))	
 	
-	colorPanelList <- colorpanel(100,low="green",mid="black",high="red")
-	
+ 	colorPanelList <- colorpanel(100,low="green",mid="black",high="red")
+
 	#Store the heatmap in a temp variable.
+	print("Create the heatmap")
 	tmp <- heatmap(	matrixData,
 					Rowv=NA,
 					Colv=NA,col=colorPanelList,
@@ -113,6 +142,7 @@ pointsize = 15
 					cexCol=1.5)		
 	
 	#Print the heatmap to an image
+	print("Print the heatmap to an image")
 	print (tmp)		
 	
 	dev.off()
