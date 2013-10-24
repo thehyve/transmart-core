e94cac928ede966d39fe44d6ab8cfcbf88e476ce
Harm Aarts <harmaarts@gmail.com>
2013-10-24 14:34:49 +0200

    this is still no good. the script has implicit knowledge of the TSV file and the table structure

--- samples/oracle/load_annotation.sh
+++ samples/oracle/load_annotation.sh
@@ -2,6 +2,6 @@
 
 groovy -cp $JDBC_DRIVER InsertGplInfo.groovy -p $PLATFORM -t $TITLE -o $ORGANISM
 
-groovy -cp $JDBC_DRIVER load_tsv_file.groovy -f some_file -t some_table
+groovy -cp $JDBC_DRIVER LoadTsvFile.groovy -f $DATA_LOCATION/$ANNOTATIONS_FILE -t tm_lz.lt_src_deapp_annot
 
 groovy -cp $JDBC_DRIVER RunStoredProcedure.groovy
