43de90a3fc66b2f6e98c812d098dfdd17027b470
Ward Weistra <ward@thehyve.nl>
2013-10-30 16:32:11 +0100

    properly run the stored procedure, I dont know if the return value is sane

--- samples/oracle/RunStoredProcedure.groovy
+++ samples/oracle/RunStoredProcedure.groovy
@@ -3,7 +3,7 @@ import DatabaseConnection
 
 def runStoredProcedure() {
   sql = DatabaseConnection.setupDatabaseConnection()
-  return_value = sql.execute("SELECT TM_CZ.i2b2_load_annotation_deapp()")
+  return_value = sql.execute("BEGIN TM_CZ.i2b2_load_annotation_deapp(); END;")
   if (return_value != 1) {
     println return_value
     println "Call to load function failed; run showdblog target"
