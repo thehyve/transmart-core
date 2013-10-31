3b67e26334cadfe3bb3f9bfe690933ea6a19a2cc
Harm Aarts <harmaarts@gmail.com>
2013-10-31 13:39:52 +0100

    assume the last entry in the audit table is the entry associated with this job/procedure

--- samples/oracle/RunStoredProcedure.groovy
+++ samples/oracle/RunStoredProcedure.groovy
@@ -3,14 +3,17 @@ import DatabaseConnection
 
 def runStoredProcedure() {
   sql = DatabaseConnection.setupDatabaseConnection()
-  try {
-    rows_changed = sql.call("call TM_CZ.i2b2_load_annotation_deapp()")
-  } catch(SQLException e) {
-    println e.getMessage()
+  sql.call("call TM_CZ.i2b2_load_annotation_deapp()")
+  job = sql.firstRow("select * from TM_CZ.cz_job_master where job_id = (select max(job_id) FROM TM_CZ.cz_job_master)")
+  if (job.job_status != "SUCCESS") {
     println "Call to load function failed; run showdblog target"
+    println """
+    Please note that this failure might very well be a false positive
+    as the script _assumes_ the latest entry in the audit table is the one from
+    this job.
+    """
     System.exit 1
   }
-  println "Rows changed ${rows_changed}"
   sql.close()
 }
 
