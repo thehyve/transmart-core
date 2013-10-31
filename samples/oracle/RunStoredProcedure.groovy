15c1f6b6fef517bf737377c54c99505259e89b45
Harm Aarts <harmaarts@gmail.com>
2013-10-31 10:59:38 +0100

    prefix with "call" The stored procedure never returns anything useful to determine success. We catch hard SQL exceptions only.

--- samples/oracle/RunStoredProcedure.groovy
+++ samples/oracle/RunStoredProcedure.groovy
@@ -4,7 +4,7 @@ import DatabaseConnection
 def runStoredProcedure() {
   sql = DatabaseConnection.setupDatabaseConnection()
   try {
-    rows_changed = sql.call("TM_CZ.i2b2_load_annotation_deapp()")
+    rows_changed = sql.call("call TM_CZ.i2b2_load_annotation_deapp()")
   } catch(SQLException e) {
     println e.getMessage()
     println "Call to load function failed; run showdblog target"
