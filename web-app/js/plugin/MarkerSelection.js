5e35baf4cf96b20cb09dcdda42fbf518446af0b8
Riza Nugraha <rnugraha@gmail.com>
2013-11-15 12:25:32 +0100

    Refactored to use FormValidator.js to reduce redundancies of similar validations.

--- web-app/js/plugin/MarkerSelection.js
+++ web-app/js/plugin/MarkerSelection.js
@@ -1,117 +1,108 @@
-/*************************************************************************   
-* Copyright 2008-2012 Janssen Research & Development, LLC.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-******************************************************************/
-
-function submitMarkerSelectionJob(form){
-	
-	var independentVariableConceptCode = "";	
-	
-	independentVariableConceptCode = readConceptVariables("divIndependentVariable");
-
-	var variablesConceptCode = independentVariableConceptCode;
-	
-	//----------------------------------
-	//Validation
-	//----------------------------------
-	//This is the independent variable.
-	var independentVariableEle = Ext.get("divIndependentVariable");	
-
-	//Get the types of nodes from the input box.
-	var independentNodeList = createNodeTypeArrayFromDiv(independentVariableEle,"setnodetype")
-	
-	//Validate to make sure a concept was dragged in.
-	if(independentVariableConceptCode == '')
-	{
-		Ext.Msg.alert('Missing input', 'Please drag at least one concept into the Marker Variable box.');
-		return;
-	}	
-	
-	if((independentNodeList[0] == 'valueicon' || independentNodeList[0] == 'hleaficon') && (independentVariableConceptCode.indexOf("|") != -1))
-	{
-		Ext.Msg.alert('Wrong input', 'For continuous and high dimensional data, you may only drag one node into the input boxes. The Marker variable input box has multiple nodes.');
-		return;		
-	}				
-
-	if(document.getElementById("txtNumberOfMarkers").value == '')
-	{
-		Ext.Msg.alert('Wrong input', 'Please enter the number of markers to display into the "Number of markers" text box.');
-		return;			
-	}	
-	
-	if(!isNumber(document.getElementById("txtNumberOfMarkers").value))
-	{
-		Ext.Msg.alert('Wrong input', 'Please enter a valid integer into the "Number of markers" text box.');
-		return;			
-	}
-	
-	if(document.getElementById("txtNumberOfMarkers").value < 1)
-	{
-		Ext.Msg.alert('Wrong input', 'Please enter a valid integer greater than 0 into the "Number of markers" text box.');
-		return;			
-	}	
-		
-	//----------------------------------	
-	
-	var formParams = {
-			independentVariable:		independentVariableConceptCode,
-			variablesConceptPaths:		variablesConceptCode,			
-			jobType:			'MarkerSelection',
-			txtNumberOfMarkers:		document.getElementById("txtNumberOfMarkers").value
-	};
-	
-	//Use a common function to load the High Dimensional Data params.
-	loadCommonHighDimFormObjects(formParams,"divIndependentVariable")	
-	loadCommonHeatmapImageAttributes(formParams);
-	
-	if(!validateCommonHeatmapImageAttributes(formParams))
-	{
-		return false;
-	}
-	
-	//------------------------------------
-	//More Validation
-	//------------------------------------		
-	if(independentNodeList[0] == 'hleaficon' && formParams["divIndependentVariableType"] == "CLINICAL")
-	{
-		Ext.Msg.alert('Wrong input', 'You dragged a High Dimensional Data node into the Marker variable box but did not select any filters. Please click the "High Dimensional Data" button and select filters. Apply the filters by clicking "Apply Selections".');
-		return;			
-	}
-	
-	//For the time being if the user is trying to run anything but GEX, stop them.
-	if(formParams["divIndependentVariableType"] != "MRNA")
-	{
-		Ext.Msg.alert("Invalid selection", "The Marker Selection only supports GEX data at this time. Please drag a Gene Expression node into the Marker variable and click the 'High Dimensional Data' button.")
-		return false;
-	}	
-	//Make sure both subsets are selected
-	if((GLOBAL.CurrentSubsetIDs[1] == null) || (GLOBAL.CurrentSubsetIDs[2]== null)){
-		Ext.Msg.alert("Missing Input", "The Marker Selection requires 2 subsets of cohorts to be selected. Please use the Comparison Tab and select the cohorts")
-		return false;
-	}
-	//------------------------------------
-	
-	submitJob(formParams);
-}
-
 /**
  * Register drag and drop.
  * Clear out all gobal variables and reset them to blank.
  */
 function loadMarkerSelectionView(){
-	registerMarkerSelectionDragAndDrop();
-	clearHighDimDataSelections('divIndependentVariable');
+    registerMarkerSelectionDragAndDrop();
+    clearHighDimDataSelections('divIndependentVariable');
+}
+
+/**
+ *  Submitting the heatmap job
+ * @param form
+ */
+function submitMarkerSelectionJob (frm) {
+
+    // get formParams
+    var formParams = getFormParameters();
+
+    if (formParams) { // if formParams is not null
+        submitJob(formParams);
+    }
+}
+
+/**
+ * get form parameters
+ * @param frm
+ */
+function getFormParameters () {
+
+    var formParameters = {}; // init
+
+    //Use a common function to load the High Dimensional Data params.
+    loadCommonHighDimFormObjects(formParameters, "divIndependentVariable");
+
+    // instantiate input elements object with their corresponding validations
+    var inputArray = [
+        {
+            "label" : "High Dimensional Data",
+            "el" : Ext.get("divIndependentVariable"),
+            "validations" : [
+                {type:"REQUIRED"},
+                {
+                    type:"HIGH_DIMENSIONAL",
+                    platform:formParameters["divIndependentVariableType"],
+                    pathway:formParameters["divIndependentVariablePathway"]
+                }
+            ]
+        },
+        {
+            "label" : "Number of Markers",
+            "el" : document.getElementById("txtNumberOfMarkers"),
+            "validations" : [{type:"REQUIRED"}, {type:"INTEGER", min:1}]
+        },
+        {
+            "label" : "Image Width",
+            "el" : document.getElementById("txtImageWidth"),
+            "validations" : [{type:"REQUIRED"}, {type:"INTEGER", min:1, max:9000}]
+        },
+        {
+            "label" : "Image Height",
+            "el" : document.getElementById("txtImageHeight"),
+            "validations" : [{type:"REQUIRED"}, {type:"INTEGER", min:1, max:9000}]
+        },
+        {
+            "label" : "Image Point Size",
+            "el" : document.getElementById("txtImagePointsize"),
+            "validations" : [{type:"REQUIRED"}, {type:"INTEGER", min:1, max:100}]
+        },
+        {
+            "label" : "Subsets",
+            "el" : null,
+            "value" : [GLOBAL.CurrentSubsetIDs[1], GLOBAL.CurrentSubsetIDs[2]],
+            "validations" : [{type:"MIN_TWO_SUBSETS"}]
+        }
+    ]
+
+    // define the validator for this form
+    var formValidator = new FormValidator(inputArray);
+
+    if (formValidator.validateInputForm()) { // if input files satisfy the validations
+
+        // get values
+        var inputConceptPathVar = readConceptVariables("divIndependentVariable");
+        var numOfMarkers = inputArray[1].el.value;
+        var imageWidth = inputArray[2].el.value;
+        var imageHeight = inputArray[3].el.value;
+        var imagePointSize = inputArray[4].el.value;
+
+        // assign values to form parameters
+        formParameters['jobType'] = 'MarkerSelection';
+        formParameters['independentVariable'] = inputConceptPathVar;
+        formParameters['variablesConceptPaths'] = inputConceptPathVar;
+        formParameters['txtNumberOfMarkers'] = numOfMarkers;
+        formParameters['txtImageWidth'] = imageWidth;
+        formParameters['txtImageHeight'] = imageHeight;
+        formParameters['txtImagePointsize'] = imagePointSize;
+
+    } else { // something is not correct in the validation
+        // empty form parameters
+        formParameters = null;
+        // display the error message
+        formValidator.display_errors();
+    }
+
+    return formParameters;
 }
 
 function loadMarkerSelectionOutput()
