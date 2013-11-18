5532e631a970813b6ca74e0e14a2423c1f38af3c
Riza Nugraha <rnugraha@gmail.com>
2013-11-18 19:32:21 +0100

    Refactored to use generic rmodules view js

--- web-app/js/plugin/Heatmap.js
+++ web-app/js/plugin/Heatmap.js
@@ -1,89 +1,45 @@
-/*************************************************************************   
-* Copyright 2008-2012 Janssen Research & Development, LLC.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-******************************************************************/
-
 /**
  * Where everything starts
  * - Register drag and drop.
  * - Clear out all gobal variables and reset them to blank.
  */
 function loadHeatmapView(){
-    registerHeatmapDragAndDrop();
-    clearHighDimDataSelections('divIndependentVariable');
+    heatMapView.clear_high_dimensional_input('divIndependentVariable');
+    heatMapView.register_drag_drop();
 }
 
-/**
- *  Submitting the heatmap job
- * @param form
- */
-function submitHeatmapJob (frm) {
+// constructor
+var HeatMapView = function () {
+    RmodulesView.call(this);
+}
+
+// inherit RmodulesView
+HeatMapView.prototype = new RmodulesView();
+
+// correct the pointer
+HeatMapView.prototype.constructor = HeatMapView;
+
+// submit analysis job
+HeatMapView.prototype.submit_job = function () {
 
     // get formParams
-    var formParams = getFormParameters();
+    var formParams = this.get_form_params();
 
     if (formParams) { // if formParams is not null
         submitJob(formParams);
     }
+
 }
 
-/**
- * get form parameters
- * @param frm
- */
-function getFormParameters () {
-
+// get form params
+HeatMapView.prototype.get_form_params = function () {
     var formParameters = {}; // init
 
     //Use a common function to load the High Dimensional Data params.
     loadCommonHighDimFormObjects(formParameters, "divIndependentVariable");
 
     // instantiate input elements object with their corresponding validations
-    var inputArray = [
-        {
-            "label" : "High Dimensional Data",
-            "el" : Ext.get("divIndependentVariable"),
-            "validations" : [
-                {type:"REQUIRED"},
-                {
-                    type:"HIGH_DIMENSIONAL",
-                    platform:formParameters["divIndependentVariableType"],
-                    pathway:formParameters["divIndependentVariablePathway"]
-                }
-            ]
-        },
-       {
-            "label" : "Max Row to Display",
-            "el" : document.getElementById("txtMaxDrawNumber"),
-            "validations" : [{type:"REQUIRED"}, {type:"INTEGER", min:1}]
-        },
-        {
-            "label" : "Image Width",
-            "el" : document.getElementById("txtImageWidth"),
-            "validations" : [{type:"REQUIRED"}, {type:"INTEGER", min:1, max:9000}]
-        },
-        {
-            "label" : "Image Height",
-            "el" : document.getElementById("txtImageHeight"),
-            "validations" : [{type:"REQUIRED"}, {type:"INTEGER", min:1, max:9000}]
-        },
-        {
-            "label" : "Image Point Size",
-            "el" : document.getElementById("txtImagePointsize"),
-            "validations" : [{type:"REQUIRED"}, {type:"INTEGER", min:1, max:100}]
-        }
-    ]
+    var inputArray = this.get_inputs(formParameters);
 
     // define the validator for this form
     var formValidator = new FormValidator(inputArray);
@@ -116,34 +72,44 @@ function getFormParameters () {
     return formParameters;
 }
 
-/**
- * Clear the variable selection box
- * Clear all selection stored in global variables
- * Clear the selection display
- * @param divName
- */
-function clearGroupHeatmap(divName)
-{
-    //Clear the drag and drop div.
-    var qc = Ext.get(divName);
-    
-    for(var i=qc.dom.childNodes.length-1;i>=0;i--)
-    {
-        var child=qc.dom.childNodes[i];
-        qc.dom.removeChild(child);
-    }    
-    clearHighDimDataSelections(divName);
-    clearSummaryDisplay(divName);
+HeatMapView.prototype.get_inputs = function (form_params) {
+   return  [
+        {
+            "label" : "High Dimensional Data",
+            "el" : Ext.get("divIndependentVariable"),
+            "validations" : [
+                {type:"REQUIRED"},
+                {
+                    type:"HIGH_DIMENSIONAL",
+                    platform:form_params["divIndependentVariableType"],
+                    pathway:form_params["divIndependentVariablePathway"]
+                }
+            ]
+        },
+        {
+            "label" : "Max Row to Display",
+            "el" : document.getElementById("txtMaxDrawNumber"),
+            "validations" : [{type:"REQUIRED"}, {type:"INTEGER", min:1}]
+        },
+        {
+            "label" : "Image Width",
+            "el" : document.getElementById("txtImageWidth"),
+            "validations" : [{type:"REQUIRED"}, {type:"INTEGER", min:1, max:9000}]
+        },
+        {
+            "label" : "Image Height",
+            "el" : document.getElementById("txtImageHeight"),
+            "validations" : [{type:"REQUIRED"}, {type:"INTEGER", min:1, max:9000}]
+        },
+        {
+            "label" : "Image Point Size",
+            "el" : document.getElementById("txtImagePointsize"),
+            "validations" : [{type:"REQUIRED"}, {type:"INTEGER", min:1, max:100}]
+        }
+    ];
 }
 
-function registerHeatmapDragAndDrop()
-{
-    //Set up drag and drop for Dependent and Independent variables on the data association tab.
-    //Get the Independent DIV
-    var independentDiv = Ext.get("divIndependentVariable");
-    
-    dtgI = new Ext.dd.DropTarget(independentDiv,{ddGroup : 'makeQuery'});
-    dtgI.notifyDrop =  dropOntoCategorySelection;
-}
+// init heat map view instance
+var heatMapView = new HeatMapView();
 
 
