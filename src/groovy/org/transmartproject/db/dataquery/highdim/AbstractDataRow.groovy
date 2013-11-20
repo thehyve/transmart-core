d38d656437f4f3faa3cacdbe49f93ee646a95dc0
Gustavo Lopes <gustavo@thehyve.nl>
2013-11-20 10:30:30 +0100

    Extract partial DataRow impl into abstract class

---
+++ src/groovy/org/transmartproject/db/dataquery/highdim/AbstractDataRow.groovy
@@ -0,0 +1,21 @@
+package org.transmartproject.db.dataquery.highdim
+
+import org.transmartproject.core.dataquery.DataRow
+import org.transmartproject.core.dataquery.highdim.AssayColumn
+
+abstract class AbstractDataRow implements DataRow<AssayColumn, Object> {
+
+    Map<AssayColumn, Integer> assayIndexMap
+
+    List<Object> data
+
+    @Override
+    Object getAt(int index) {
+        data[index]
+    }
+
+    @Override
+    Object getAt(AssayColumn column) {
+        data[assayIndexMap[column]]
+    }
+}
--- src/groovy/org/transmartproject/db/dataquery/highdim/mrna/ProbeRow.groovy
+++ src/groovy/org/transmartproject/db/dataquery/highdim/mrna/ProbeRow.groovy
@@ -1,11 +1,10 @@
 package org.transmartproject.db.dataquery.highdim.mrna
 
 import groovy.transform.ToString
-import org.transmartproject.core.dataquery.DataRow
-import org.transmartproject.core.dataquery.highdim.AssayColumn
+import org.transmartproject.db.dataquery.highdim.AbstractDataRow
 
 @ToString(excludes = [ 'assayIndexMap' ])
-class ProbeRow implements DataRow<AssayColumn, Object> {
+class ProbeRow extends AbstractDataRow {
 
     String probe
 
@@ -13,22 +12,8 @@ class ProbeRow implements DataRow<AssayColumn, Object> {
 
     String organism
 
-    Map<AssayColumn, Integer> assayIndexMap
-
-    List<Object> data
-
     @Override
     String getLabel() {
         probe
-    }
-
-    @Override
-    Object getAt(int index) {
-        data[index]
-    }
-
-    @Override
-    Object getAt(AssayColumn column) {
-        data[assayIndexMap[column]]
     }
 }
