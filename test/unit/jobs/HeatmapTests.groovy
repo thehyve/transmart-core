08d715e5f7324a00294de21b0caa4c7571ea970d
Harm Aarts <harmaarts@gmail.com>
2013-11-22 10:36:31 +0100

    Tests if the correct dirs are made

--- test/unit/jobs/HeatmapTests.groovy
+++ test/unit/jobs/HeatmapTests.groovy
@@ -4,18 +4,28 @@ import grails.test.mixin.TestMixin
 import grails.test.mixin.support.GrailsUnitTestMixin
 import org.gmock.WithGMock
 import org.hamcrest.Matchers
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.quartz.JobExecutionContext
+import org.transmartproject.core.dataquery.TabularResult
 
 @TestMixin(GrailsUnitTestMixin)
 @WithGMock
 class HeatmapTests {
 
-    Heatmap testee
+    Heatmap heatmap
+
 
     @Before
     void setUp() {
-        testee = new Heatmap()
+        heatmap = new Heatmap()
+        heatmap.temporaryDirectory = "/tmp/unit_tests"
+    }
+
+    @After
+    void tearDown() {
+        new File(heatmap.temporaryDirectory).deleteDir()
     }
 
     @Test
@@ -30,9 +40,30 @@ class HeatmapTests {
         }
     }
 
-    /*
-    void writesParameterFileTest() {}
+    @Test
+    void setupTemporaryDirectoryTest() {
+        heatmap.metaClass.writeParametersFile = { }
+        heatmap.metaClass.fetchResults = { }
+        heatmap.metaClass.writeData = { TabularResult a, File b -> null }
+        heatmap.metaClass.runAnalysis = { }
 
+        JobExecutionContext context = mock(JobExecutionContext)
+        def jobDetailMap = mock()
+        jobDetailMap.jobDataMap.returns([jobName: 'foo_bar_baz']).stub()
+        context.jobDetail.returns(jobDetailMap).stub()
+
+        play {
+            heatmap.execute(context)
+            assert new File(new File(heatmap.temporaryDirectory, 'foo_bar_baz'), 'workingDirectory').exists()
+        }
+    }
+
+    @Test
+    void writesParameterFileTest() {
+
+    }
+
+    /*
     void fetchesResultsTest() {}
 
     void writesResultsTest() {}
