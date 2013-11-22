ab59457e345112b525a77ce775a03557fe13f2e6
Harm Aarts <harmaarts@gmail.com>
2013-11-22 13:14:44 +0100

    Adds running of the R commands

--- src/groovy/jobs/Heatmap.groovy
+++ src/groovy/jobs/Heatmap.groovy
@@ -1,5 +1,6 @@
 package jobs
 
+import com.recomdata.transmart.util.RUtil
 import org.quartz.Job
 import org.quartz.JobDataMap
 import org.quartz.JobDetail
@@ -7,6 +8,9 @@ import org.quartz.JobExecutionContext
 import org.quartz.JobExecutionException
 import grails.util.Holders
 import org.quartz.SimpleTrigger
+import org.rosuda.REngine.REXP
+import org.rosuda.REngine.Rserve.RConnection
+import org.rosuda.REngine.Rserve.RserveException
 import org.transmartproject.core.dataquery.TabularResult
 import org.transmartproject.core.dataquery.highdim.HighDimensionDataTypeResource
 import org.transmartproject.core.dataquery.highdim.HighDimensionResource
@@ -21,20 +25,19 @@ class Heatmap implements Job {
     Map jobDataMap
     String name
 
-    String temporaryDirectory = Holders.config.RModules.tempFolderDirectory
+    File temporaryDirectory
 
     @Override
     void execute(JobExecutionContext context) throws JobExecutionException {
         name = context.jobDetail.jobDataMap["jobName"]
         jobDataMap = context.jobDetail.jobDataMap
 
-        File dir = setupTemporaryDirectory()
+        setupTemporaryDirectory()
 
-        writeParametersFile(dir)
+        writeParametersFile()
         TabularResult results = fetchResults()
-        writeData(results, dir)
+        writeData(results)
         runAnalysis()
-
     }
 
     static void scheduleJob(Map params) {
@@ -46,11 +49,91 @@ class Heatmap implements Job {
 
     private def runAnalysis() {
         updateStatus('Running Analysis')
+
+        Closure<GString> source = {
+            "source('$pluginDirectory/Heatmap/HeatmapLoader.R')"
+        }
+
+        Closure<GString> createHeatmap = {
+            """
+            Heatmap.loader(
+                            input.filename = 'outputfile',
+                            imageWidth     = as.integer('$txtImageWidth'),
+                            imageHeight    = as.integer('$txtImageHeight'),
+                            pointsize      = as.integer('$txtImagePointsize'),
+                            maxDrawNumber  = as.integer('$txtMaxDrawNumber'))
+            """
+        }
+
+        runRCommandList([source, createHeatmap])
     }
 
-    private def writeData(TabularResult results, File destinationDirectory) {
+    private static Object escapingWrapper(Map<String, String> vars) {
+        Object escapingWrapper = new Object()
+        escapingWrapper.metaClass.getProperty { name ->
+            if (name == null) {
+                ''
+            } else {
+                RUtil.escapeRStringContent(vars[name])
+            }
+        }
+        return escapingWrapper
+    }
+
+    private void runRCommandList(List<Closure<GString>> stepList) {
+        String study = i2b2ExportHelperService.findStudyAccessions([jobDataMap.result_instance_id1])
+
+        //Establish a connection to R Server.
+        RConnection rConnection = new RConnection();
+
+        //Run the R command to set the working directory to our temp directory.
+        rConnection.eval("setwd('$temporaryDirectory')");
+
+        //For each R step there is a list of commands.
+        stepList.each { Closure<GString> currentCommand ->
+
+            /**
+             * Please make sure that any and all variables you add to the map here are placed _after_ the putAll
+             * as otherwise you create a potential security vulnerability
+             */
+            Map vars = [:]
+            vars.putAll jobDataMap
+            vars.pluginDirectory = Holders.config.RModules.pluginScriptDirectory
+            vars.temporaryDirectory = new File(temporaryDirectory, "subset1_" + study).absolutePath
+
+            def finalCommand = Heatmap.escapingWrapper(vars).with {
+                currentCommand.call().toString()
+            }
+
+            REXP rObject = rConnection.parseAndEval("try($finalCommand, silent=TRUE)")
+
+            if (rObject.inherits("try-error")) {
+                handleError(rObject)
+            }
+        }
+    }
+
+    private void handleError(REXP rObject) throws RserveException {
+        //Grab the error R gave us.
+        String rError = rObject.asString()
+
+        //This is the error we will eventually throw.
+        RserveException newError = null
+
+        //If it is a friendly error, use that, otherwise throw the default message.
+        if (rError ==~ /.*\|\|FRIENDLY\|\|.*/) {
+            rError = rError.replaceFirst(/.*\|\|FRIENDLY\|\|/, "")
+            newError = new RserveException(rConnection, rError)
+        } else {
+            newError = new RserveException(rConnection, "There was an error running the R script for your job. Please contact an administrator.")
+        }
+
+        throw newError
+    }
+
+    private def writeData(TabularResult results) {
         try {
-            File output = new File(destinationDirectory, 'outputfile')
+            File output = new File(temporaryDirectory, 'outputfile')
             output.createNewFile()
             output.withWriter {
                 CSVWriter writer = new CSVWriter(it, '\t' as char)
@@ -84,16 +167,13 @@ class Heatmap implements Job {
     }
 
     private File setupTemporaryDirectory() {
-        File jobWorkingDirectory = new File(new File(temporaryDirectory, name), 'workingDirectory')
-
-        //TODO: This is stupid of course, taking the 'name' from the client. What if the name is '../../'?
-        jobWorkingDirectory.mkdirs()
-
-        jobWorkingDirectory
+        //FIXME: This is stupid of course, taking the 'name' from the client. What if the name is '../../'?
+        temporaryDirectory = new File(new File(Holders.config.RModules.tempFolderDirectory, name), 'workingDirectory')
+        temporaryDirectory.mkdirs()
     }
 
-    private void writeParametersFile(File jobWorkingDirectory) {
-        File jobInfoFile = new File(jobWorkingDirectory, 'jobInfo.txt')
+    private void writeParametersFile() {
+        File jobInfoFile = new File(temporaryDirectory, 'jobInfo.txt')
 
         jobInfoFile.withWriter { BufferedWriter it ->
             it.writeLine 'Parameters'
@@ -118,6 +198,10 @@ class Heatmap implements Job {
 
     private def getConceptsResourceService() {
         jobDataMap.grailsApplication.mainContext.getBean ConceptsResource
+    }
+
+    private def getI2b2ExportHelperService() {
+        jobDataMap.grailsApplication.mainContext.i2b2ExportHelperService
     }
 
     private def getAsyncJobService() {
