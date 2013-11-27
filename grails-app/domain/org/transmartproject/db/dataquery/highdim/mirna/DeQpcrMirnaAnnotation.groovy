ee3315386e3792bf2a21d7be7137d18c1381dd08
Harm Aarts <harmaarts@gmail.com>
2013-11-27 13:19:11 +0100

    Replaces tabs with spaces

--- grails-app/domain/org/transmartproject/db/dataquery/highdim/mirna/DeQpcrMirnaAnnotation.groovy
+++ grails-app/domain/org/transmartproject/db/dataquery/highdim/mirna/DeQpcrMirnaAnnotation.groovy
@@ -2,7 +2,7 @@ package org.transmartproject.db.dataquery.highdim.mirna
 
 class DeQpcrMirnaAnnotation implements Serializable {
 
-	String mirnaId
+    String mirnaId
     String detector
 
     // unused or irrelevant:
@@ -11,20 +11,20 @@ class DeQpcrMirnaAnnotation implements Serializable {
 
     //String organism
 
-	static mapping = {
+    static mapping = {
         table    schema: 'deapp'
         id       column: 'probeset_id', generator: 'assigned'
         detector column: 'mirna_symbol' // column name is scheduled to be changed
         version  false
-	}
+    }
 
-	static constraints = {
-        mirnaId     nullable: true, maxSize: 100
-        detector    nullable: true, maxSize: 100
+    static constraints = {
+        mirnaId  nullable: true, maxSize: 100
+        detector nullable: true, maxSize: 100
 
         // unused or irrelevant:
         //idRef       nullable: true, maxSize: 100
         //probeId     nullable: true, maxSize: 100
         //organism    nullable: true, maxSize: 2000
-	}
+    }
 }
--- grails-app/domain/org/transmartproject/db/dataquery/highdim/mirna/DeSubjectMirnaData.groovy
+++ grails-app/domain/org/transmartproject/db/dataquery/highdim/mirna/DeSubjectMirnaData.groovy
@@ -4,9 +4,9 @@ import org.transmartproject.db.dataquery.highdim.DeSubjectSampleMapping
 
 class DeSubjectMirnaData implements Serializable {
 
-	BigDecimal rawIntensity
-	BigDecimal logIntensity
-	BigDecimal zscore
+    BigDecimal rawIntensity
+    BigDecimal logIntensity
+    BigDecimal zscore
 
     DeQpcrMirnaAnnotation jProbe //see comment on mapping
 
@@ -21,9 +21,9 @@ class DeSubjectMirnaData implements Serializable {
     ]
 
 
-	static mapping = {
-        table schema:    'deapp'
-        id    composite: [ 'assay', 'probe' ]
+    static mapping = {
+        table schema: 'deapp'
+        id    composite: ['assay', 'probe']
 
         assay   column: 'assay_id'
         probe   column: 'probeset_id'
@@ -31,18 +31,18 @@ class DeSubjectMirnaData implements Serializable {
 
         // this is needed due to a Criteria bug.
         // see https://forum.hibernate.org/viewtopic.php?f=1&t=1012372
-        jProbe  column: 'probeset_id', insertable: false, updateable: false
+        jProbe column: 'probeset_id', insertable: false, updateable: false
 
         version false
-	}
+    }
 
-	static constraints = {
+    static constraints = {
         assay        nullable: true
-        rawIntensity nullable: true, scale:   4
-        logIntensity nullable: true, scale:   4
-        zscore       nullable: true, scale:   4
+        rawIntensity nullable: true, scale: 4
+        logIntensity nullable: true, scale: 4
+        zscore       nullable: true, scale: 4
         //trialSource  nullable: true, maxSize: 200
         //trialName    nullable: true, maxSize: 50
         //patient      nullable: true
-	}
+    }
 }
