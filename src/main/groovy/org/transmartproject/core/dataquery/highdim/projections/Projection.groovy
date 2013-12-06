d7e48b0f2c01dc63e8930d640098648ac84efdde
Gustavo Lopes <gustavo@thehyve.nl>
2013-12-06 15:46:41 +0100

    Move constants for well-known projections here
    Better docs for Projection as well.

--- src/main/groovy/org/transmartproject/core/dataquery/highdim/projections/Projection.groovy
+++ src/main/groovy/org/transmartproject/core/dataquery/highdim/projections/Projection.groovy
@@ -1,11 +1,40 @@
 package org.transmartproject.core.dataquery.highdim.projections
 
 /**
+ * A projection determines that which data will show up in the result set's
+ * cells.
  *
- * @param < CELL > The type of the cells in the {@link DataQueryResult}.
+ * @param < CELL > The type of the cells in the
+ * {@link org.transmartproject.core.dataquery.TabularResult}.
  */
 public interface Projection<CELL> {
 
+    /**
+     * The data type default projection for getting real data series from the
+     * API. If several real values are available, the most representative or
+     * or least processed value should be returned.
+     *
+     * Data type implementations are strongly encouraged to support this
+     * projection, if possible.
+     */
+    public static final DEFAULT_REAL_PROJECTION = 'default_real_projection'
+
+    /**
+     * Projection for getting transformed data points representing the standard
+     * score of each original data point.
+     *
+     * It's implementation-defined whether this score is calculated on the
+     * filtered data set (with rows or assays excluded) or on the complete
+     * data set.
+     */
+    public static final ZSCORE_PROJECTION = 'zscore'
+
+    /**
+     * This method takes an implementation-defined object and returns the final
+     * cell value.
+     * @param object an implementation-defined object
+     * @return the final cell value
+     */
     CELL doWithResult(Object object)
 
 }
