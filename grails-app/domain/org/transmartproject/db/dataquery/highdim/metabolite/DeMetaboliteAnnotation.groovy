c369982fae241d30fde5e9709d170aa493b2f738
Harm Aarts <harmaarts@gmail.com>
2014-01-20 16:39:36 +0100

    Adds domain models for metabolites.
    Generated with 'grails -classpath lib/ db-reverse-engineer' after
    copying the Oracle JDBC to the lib dir. Then tweaked by hand, took as an
    example the proteomics domain models.

---
+++ grails-app/domain/org/transmartproject/db/dataquery/highdim/metabolite/DeMetaboliteAnnotation.groovy
@@ -0,0 +1,20 @@
+package org.transmartproject.db.dataquery.highdim.metabolite
+
+import org.transmartproject.db.dataquery.highdim.DeGplInfo
+
+class DeMetaboliteAnnotation {
+
+    static belongsTo = [ platform: DeGplInfo ]
+
+    static hasMany = [ dataRows: DeSubjectMetabolomicsData ]
+
+    static mappedBy = [ dataRows: 'annotation' ]
+
+    static mapping = {
+        table    schema:    'deapp'
+        id       generator: 'assigned'
+        platform column:    'gpl_id'
+
+        version   false
+    }
+}
---
+++ grails-app/domain/org/transmartproject/db/dataquery/highdim/metabolite/DeSubjectMetabolomicsData.groovy
@@ -0,0 +1,29 @@
+package org.transmartproject.db.dataquery.highdim.metabolite
+
+import org.apache.commons.lang.builder.EqualsBuilder
+import org.apache.commons.lang.builder.HashCodeBuilder
+import org.transmartproject.db.dataquery.highdim.DeSubjectSampleMapping
+
+class DeSubjectMetabolomicsData implements Serializable {
+
+    BigDecimal zscore
+    DeMetaboliteAnnotation jAnnotation
+
+    static belongsTo = [
+            assay:      DeSubjectSampleMapping,
+            annotation: DeMetaboliteAnnotation,
+    ]
+
+    static mapping = {
+        table schema:    'deapp'
+        id    composite: ['assay', 'annotation']
+
+        assay      column: 'assay_id'
+        annotation column: 'metabolite_annotation_id'
+
+        // this is needed due to a Criteria bug.
+        // see https://forum.hibernate.org/viewtopic.php?f=1&t=1012372
+        jAnnotation column: 'metabolite_annotation_id', updateable: false, insertable: false
+        version false
+    }
+}
