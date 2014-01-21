dda610c30c766083173b78100527c247a730a53b
Harm Aarts <harmaarts@gmail.com>
2014-01-21 15:55:00 +0100

    Adds test for retrieving data

--- test/integration/org/transmartproject/db/dataquery/highdim/metabolite/MetaboliteEndToEndRetrievalTest.groovy
+++ test/integration/org/transmartproject/db/dataquery/highdim/metabolite/MetaboliteEndToEndRetrievalTest.groovy
@@ -1,13 +1,54 @@
 package org.transmartproject.db.dataquery.highdim.metabolite
 
+import com.google.common.collect.Lists
+import org.hibernate.criterion.Projection
+import org.junit.Before
+import org.junit.After
 import org.junit.Test
+import org.transmartproject.core.dataquery.TabularResult
+import org.transmartproject.core.dataquery.highdim.AssayColumn
+import org.transmartproject.core.dataquery.highdim.HighDimensionDataTypeResource
+import org.transmartproject.core.dataquery.highdim.HighDimensionResource
+import org.transmartproject.core.dataquery.highdim.assayconstraints.AssayConstraint
 
 class MetaboliteEndToEndRetrievalTest {
+
+    HighDimensionResource highDimensionResourceService
+
+    HighDimensionDataTypeResource metaboliteResource
+
+    AssayConstraint trialConstraint
+
+    Projection projection
+
+    TabularResult<AssayColumn, MetaboliteDataRow> result
+
     MetaboliteTestData testData = new MetaboliteTestData()
+
+    @Before
+    void setup() {
+        testData.saveAll()
+        metaboliteResource = highDimensionResourceService.getSubResourceForType('metabolite')
+    }
+
+    @After
+    void teardown() {
+        result?.close()
+    }
 
     @Test
     void dataPersistenceTest() {
-        testData.saveAll()
+        def trialConstraint = metaboliteResource.createAssayConstraint(
+                AssayConstraint.TRIAL_NAME_CONSTRAINT,
+                name: MetaboliteTestData.TRIAL_NAME)
 
+        def projection = metaboliteResource.createProjection([:],
+                org.transmartproject.core.dataquery.highdim.projections.Projection.ZSCORE_PROJECTION)
+
+        result = metaboliteResource.retrieveData([trialConstraint], [], projection)
+
+        List rows = Lists.newArrayList result.rows
+
+        println(rows)
     }
 }
