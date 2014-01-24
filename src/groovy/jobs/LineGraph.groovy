2a7ab672ed17e5f6714b06866639b01a45da681f
temoedjin <temoedjin@gmail.com>
2014-01-24 17:45:29 +0100

    changed name for pathway search item to math with the ui

--- src/groovy/jobs/LineGraph.groovy
+++ src/groovy/jobs/LineGraph.groovy
@@ -49,7 +49,7 @@ class LineGraph extends AbstractAnalysisJob {
 
         dependentVariableConfigurator.keyForConceptPath     = "dependentVariable"
         dependentVariableConfigurator.keyForDataType        = "divDependentVariableType"
-        dependentVariableConfigurator.keyForSearchKeywordId = "divDependentSearchKeyword"
+        dependentVariableConfigurator.keyForSearchKeywordId = "gexpathway"
 
         groupByColumnConfigurator.columnHeader = 'GROUP_VAR'
         groupByColumnConfigurator.keyForConceptPaths = 'groupByVariable'
