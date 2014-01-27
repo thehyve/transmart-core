a1d07283ce4c0555c721323d7cc82e25246c6904
Gustavo Lopes <gustavo@thehyve.nl>
2014-01-27 12:17:27 +0100

    Refactor code into HighDimensionColumnConfigurator

--- src/groovy/jobs/steps/helpers/NumericColumnConfigurator.groovy
+++ src/groovy/jobs/steps/helpers/NumericColumnConfigurator.groovy
@@ -1,20 +1,11 @@
 package jobs.steps.helpers
 
 import jobs.table.Column
-import jobs.table.columns.HighDimensionMultipleRowsResultColumn
-import jobs.table.columns.HighDimensionSingleRowResultColumn
 import jobs.table.columns.SimpleConceptVariableColumn
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Component
 import org.transmartproject.core.dataquery.clinical.ClinicalVariable
-import org.transmartproject.core.dataquery.highdim.HighDimensionDataTypeResource
-import org.transmartproject.core.dataquery.highdim.HighDimensionResource
-import org.transmartproject.core.dataquery.highdim.assayconstraints.AssayConstraint
-import org.transmartproject.core.dataquery.highdim.dataconstraints.DataConstraint
-import org.transmartproject.core.exceptions.InvalidArgumentsException
-
-import static jobs.steps.OpenHighDimensionalDataStep.createConceptKeyFrom
 
 @Component
 @Scope('prototype')
@@ -24,24 +15,24 @@ class NumericColumnConfigurator extends ColumnConfigurator {
 
     String columnHeader
 
-    String projection
+    String projection             /* only applicable for high dim data */
 
     String keyForConceptPath,
            keyForDataType,        /* CLINICAL for clinical data */
            keyForSearchKeywordId  /* only applicable for high dim data */
 
-    boolean multiRow = false
+    boolean multiRow = false      /* only applicable for high dim data */
 
     boolean alwaysClinical = false
-
-    @Autowired
-    private HighDimensionResource highDimensionResource
 
     @Autowired
     private ClinicalDataRetriever clinicalDataRetriever
 
     @Autowired
     private ResultInstanceIdsHolder resultInstanceIdsHolder
+
+    @Autowired
+    private HighDimensionColumnConfigurator highDimensionColumnConfigurator
 
     @Override
     protected void doAddColumn(Closure<Column> columnDecorator) {
@@ -57,50 +48,11 @@ class NumericColumnConfigurator extends ColumnConfigurator {
     }
 
     private void addColumnHighDim(Closure<Column> decorateColumn) {
-        String dataType = getStringParam(keyForDataType)
-        HighDimensionDataTypeResource subResource =
-                highDimensionResource.getSubResourceForType dataType
-
-        def projection = subResource.createProjection [:], projection
-        def assayConstraints = []
-
-        assayConstraints << subResource.createAssayConstraint(
-                AssayConstraint.DISJUNCTION_CONSTRAINT,
-                subconstraints: [
-                        (AssayConstraint.PATIENT_SET_CONSTRAINT):
-                                resultInstanceIdsHolder.resultInstanceIds.collect {
-                                    [result_instance_id: it]
-                                }])
-        assayConstraints << subResource.createAssayConstraint(
-                AssayConstraint.ONTOLOGY_TERM_CONSTRAINT,
-                concept_key: createConceptKeyFrom(getStringParam(keyForConceptPath)))
-
-        def searchKeyword = getStringParam(keyForSearchKeywordId)
-        if (!searchKeyword.isLong()) {
-            throw new InvalidArgumentsException("Illegal search keyword id: $searchKeyword")
+        ['columnHeader', 'projection', 'keyForConceptPath', 'keyForDataType',
+                'keyForSearchKeywordId', 'multiRow'].each { prop ->
+            highDimensionColumnConfigurator."$prop" = this."$prop"
         }
-        def dataConstraint = subResource.createDataConstraint(
-                DataConstraint.SEARCH_KEYWORD_IDS_CONSTRAINT,
-                keyword_ids: [searchKeyword])
-
-        def tabularResult = subResource.retrieveData(
-                assayConstraints, [dataConstraint], projection)
-
-        String dataSourceName = columnHeader + '_highdim'
-
-        table.addDataSource dataSourceName, tabularResult
-
-        def highDimColumn
-        if (!multiRow) {
-            highDimColumn = new HighDimensionSingleRowResultColumn(
-                    header: columnHeader)
-        } else {
-            highDimColumn = new HighDimensionMultipleRowsResultColumn(
-                    header: columnHeader)
-        }
-        table.addColumn(
-                decorateColumn.call(highDimColumn),
-                [dataSourceName] as Set)
+        highDimensionColumnConfigurator.addColumn decorateColumn
     }
 
     private void addColumnClinical(Closure<Column> decorateColumn) {
