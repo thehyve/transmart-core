ed13729a12eef9aac93ec512f2d3895f1428875f
Gustavo Lopes <gustavo@thehyve.nl>
2014-01-28 10:45:20 +0100

    Simply equals() and hashCode() for AssayColumnImpl

--- src/groovy/org/transmartproject/db/dataquery/highdim/AssayColumnImpl.groovy
+++ src/groovy/org/transmartproject/db/dataquery/highdim/AssayColumnImpl.groovy
@@ -1,8 +1,10 @@
 package org.transmartproject.db.dataquery.highdim
 
+import groovy.transform.ToString
 import org.transmartproject.core.dataquery.assay.Assay
 import org.transmartproject.core.dataquery.highdim.AssayColumn
 
+@ToString(includes = ['assay', 'label'])
 class AssayColumnImpl implements AssayColumn {
 
     @Delegate
@@ -15,13 +17,14 @@ class AssayColumnImpl implements AssayColumn {
         this.label = assay.sampleCode
     }
 
-
+    @Override
+    boolean equals(Object other) {
+        other instanceof AssayColumnImpl &&
+                this.assay.id == other.assay.id
+    }
 
     @Override
-    public java.lang.String toString() {
-        return "AssayColumnImpl{" +
-                "assay=" + assay +
-                ", label='" + label + '\'' +
-                '}';
+    int hashCode() {
+        this.assay.id.hashCode()
     }
 }
