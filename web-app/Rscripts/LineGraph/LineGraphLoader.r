5f979a0a24eae63d723267b22248ca32c8972a8a
Tim Dorscheidt <tim@thehyve.nl>
2014-01-30 11:12:55 +0100

    Manual merge of of implemented features from other branch in LineGraph r-script.
    This should prevent merge conflicts in the R file for branch owner later on.

--- web-app/Rscripts/LineGraph/LineGraphLoader.r
+++ web-app/Rscripts/LineGraph/LineGraphLoader.r
@@ -19,7 +19,7 @@ LineGraph.loader <- function(
 	library(ggplot2)
 	library(Cairo)
 	######################################################
-	
+
 	######################################################
 	#Read the line graph data.
 	line.data <- read.delim(input.filename,header=T)
@@ -31,17 +31,45 @@ LineGraph.loader <- function(
 	}
 	# assign the X-axis position to each row
 	line.data$TIME_VALUE <- sapply(line.data$GROUP,FUN = function(groupValue) { scaling.data$VALUE[which(groupValue==scaling.data$GROUP)] })
-	
-	#We need to convert the value column from a factor to a numeric.
-	#finalData$VALUE <- as.numeric(levels(finalData$VALUE))[as.integer(finalData$VALUE)]
 
+    # Either plot a single LineGraph (if there are no plot_group values)
+    # or, for each group-value, retrieve rows for that value and plot LineGraph
+    plotGroupValues <- unique(line.data$PLOT_GROUP)
+    if (is.null(plotGroupValues)) {
+      p <- LineGraph.plotter(line.data, graphType, plot.individuals, HDD.data.type)
+      imageFileName <- paste(output.file,".png",sep="")
+      CairoPNG(file=imageFileName, width=1200, height=600,units = "px")
+      print(p)
+      dev.off()
+    } else {
+      fileIter <- 1
+      for (plotGroup in plotGroupValues) {
+        groupData <- line.data[which(line.data$PLOT_GROUP==plotGroup),]
+        p <- LineGraph.plotter(groupData, graphType, plot.individuals, HDD.data.type)
+        p <- p + labs(title = as.character(plotGroup))
+        imageFileName <- paste(output.file,fileIter,".png",sep="")
+        fileIter <- fileIter + 1
+        CairoPNG(file=imageFileName, width=1200, height=600,units = "px")
+        print(p)
+        dev.off()
+      }
+    }
+}
+
+LineGraph.plotter <- function(
+  data.to.plot,
+  graphType,
+  plot.individuals,
+  HDD.data.type
+)
+{
 	if (plot.individuals) {
 	  #Change column order to match internal standard and adjust the column names.
-	  dataOutput <- line.data[,c("PATIENT_NUM","GROUP","TIME_VALUE","GROUP_VAR","VALUE")]
+	  dataOutput <- data.to.plot[,c("PATIENT_NUM","GROUP","TIME_VALUE","GROUP_VAR","VALUE")]
 	  colnames(dataOutput) <- c('PATIENT_NUM','TIMEPOINT','TIME_VALUE','GROUP','VALUE')
 	} else {
 	  #Aggregate the data to get rid of patient numbers. We add a standard error column so we can use it in the error bars.
-	  dataOutput <- ddply(line.data, .(GROUP,TIME_VALUE,GROUP_VAR),
+	  dataOutput <- ddply(data.to.plot, .(GROUP,TIME_VALUE,GROUP_VAR),
 	                      summarise,
 	                      MEAN   	= mean(VALUE),
 	                      SD 		= sd(VALUE),
@@ -54,11 +82,15 @@ LineGraph.loader <- function(
 
 	#Use a regular expression trim out the timepoint from the concept.
 	#dataOutput$TIMEPOINT <- str_extract(dataOutput$TIMEPOINT,"Week [0-9]+")
-	dataOutput$TIMEPOINT <- str_extract(dataOutput$TIMEPOINT,"(\\\\.+\\\\.+\\\\)+?$")
-	
+    dataOutput$TIMEPOINT <- as.character(dataOutput$TIMEPOINT)
+    TIMEPOINT_reducedConceptPath <- str_extract(dataOutput$TIMEPOINT,"(\\\\.+\\\\.+\\\\)+?$")
+    validReplacements <- which(!is.na(TIMEPOINT_reducedConceptPath))
+    dataOutput$TIMEPOINT[validReplacements] <- TIMEPOINT_reducedConceptPath[validReplacements]
+    dataOutput$TIMEPOINT <- as.factor(dataOutput$TIMEPOINT)
+
 	#Convert the timepoint field to a factor.
 	dataOutput$TIMEPOINT <- factor(dataOutput$TIMEPOINT)
-		
+
 	#Convert the group field to a factor.
 	dataOutput$GROUP <- factor(dataOutput$GROUP)
 	######################################################
@@ -87,20 +119,20 @@ LineGraph.loader <- function(
 	  yLabel <- paste(yLabel,"(median + se)")
 	}
 	p <- ggplot(data=dataOutput,layerData) + ylab(yLabel)
-	
-	p <- p + geom_line(size=1.5) + scale_colour_brewer() 
+
+	p <- p + geom_line(size=1.5) + scale_colour_brewer()
 	if (!plot.individuals) p <- p + geom_errorbar(limits,width=0.2)
-  
+
 	#Defines a continuous x-axis with proper break-locations, labels, and axis-name
 	p <- p + scale_x_continuous(name = "TIMEPOINT", breaks = dataOutput$TIME_VALUE, labels = dataOutput$TIMEPOINT)
-  
+
 	#This sets the color theme of the background/grid.
 	p <- p + theme_bw();
 	
 	#Set the text options for the axis.
 	p <- p + theme(axis.text.x = theme_text(size = 17,face="bold",angle=5));
 	p <- p + theme(axis.text.y = theme_text(size = 17,face="bold"));
-	
+
 	#Set the text options for the title.
 	p <- p + theme(axis.title.x = theme_text(vjust = -.5,size = 20,face="bold"));
 	p <- p + theme(axis.title.y = theme_text(vjust = .35,size = 20,face="bold",angle=90));
@@ -111,17 +143,6 @@ LineGraph.loader <- function(
 	p <- p + theme(legend.title=theme_blank())
 
 	p <- p + geom_point(size=4);
-	
-	#This is the name of the output image file.
-	imageFileName <- paste(output.file,".png",sep="")
-	
-	#This initializes our image capture object.
-	CairoPNG(file=imageFileName, width=1200, height=600,units = "px")	
-	
-	#Printing actually puts the plot in the image.
-	print(p)
-	
-	#Turn of the graphics device to save the image.
-	dev.off()
-	######################################################
+
+    p
 }
