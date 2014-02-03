b4401cafa69e56d822aeb7501dacb6a2ccecf4d9
Gustavo Lopes <gustavo@thehyve.nl>
2014-02-03 12:39:27 +0100

    Attempt to fix travis build

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -49,6 +49,12 @@ grails.project.dependency.resolution = {
             transitive = false /* don't bring groovy-all */
             export     = false
         }
+
+        /* for reasons I don't want to guess (we'll move away from ivy soon
+         * anyway), javassist is not being included in the test classpath
+         * when running test-app in Travis even though the hibernate plugin
+         * depends on it */
+        test('org.javassist:javassist:3.16.1-GA')
     }
 
     plugins {
