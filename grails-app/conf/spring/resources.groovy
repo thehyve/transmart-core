731cd49d4d61904408586f585de00c1e15f00dd7
temoedjin <temoedjin@gmail.com>
2014-02-05 14:18:58 +0100

    added marshaller for subject

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,13 +1,15 @@
 import org.transmartproject.webservices.TransmartMarshallerRegistrar
 import org.transmartproject.webservices.StudyJsonMarshaller
 import org.transmartproject.webservices.ObservationJsonMarshaller
+import org.transmartproject.webservices.SubjectJsonMarshaller
 
 // Place your Spring DSL code here
 beans = {
 	transmartMarshallerRegistrar(TransmartMarshallerRegistrar) {
 		marshallers = [
 			new StudyJsonMarshaller(),
-			new ObservationJsonMarshaller()
+			new ObservationJsonMarshaller(),
+			new SubjectJsonMarshaller(),
 		]
 	}
 }
