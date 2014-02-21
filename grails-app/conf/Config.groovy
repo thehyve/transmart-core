982a6fb42de6c67ddb2f05ad792f7f3f6d841f3d
Gustavo Lopes <gustavo@thehyve.nl>
2014-02-21 15:39:58 +0100

    Increase log verbosity on transmart-core-db-tests

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -12,13 +12,6 @@ grails.databinding.trimStrings = false
 //*/
 
 log4j = {
-    // Example of changing the log pattern for the default console
-    // appender:
-    //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
-
     info  'org.codehaus.groovy.grails.web.servlet',  //  controllers
            'org.codehaus.groovy.grails.web.pages', //  GSP
            'org.codehaus.groovy.grails.web.sitemesh', //  layouts
--- transmart-core-db-tests/grails-app/conf/Config.groovy
+++ transmart-core-db-tests/grails-app/conf/Config.groovy
@@ -11,22 +11,7 @@ grails.databinding.convertEmptyStringsToNull = false
 grails.databinding.trimStrings = false
 
 log4j = {
-    // Example of changing the log pattern for the default console
-    // appender:
-    //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
-
-    error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
-           'org.codehaus.groovy.grails.web.pages', //  GSP
-           'org.codehaus.groovy.grails.web.sitemesh', //  layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping', // URL mapping
-           'org.codehaus.groovy.grails.commons', // core / classloading
-           'org.codehaus.groovy.grails.plugins', // plugins
-           'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
-           'org.springframework',
-           'org.hibernate',
-           'net.sf.ehcache.hibernate'
+    root {
+        info('stdout')
+    }
 }
