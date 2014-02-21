beb25b4f90d165ca0f54fb71da5417956d0badb8
Ruslan Forostianov <ruslan@thehyve.nl>
2014-02-21 12:31:57 +0100

    [CHERKASY-69] Prepare db for tests
    Create db schemas right after test db creation

--- transmart-core-db-tests/grails-app/conf/DataSource.groovy
+++ transmart-core-db-tests/grails-app/conf/DataSource.groovy
@@ -22,7 +22,7 @@ environments {
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;INIT=RUNSCRIPT FROM './h2_init.sql'"
         }
     }
     production {
