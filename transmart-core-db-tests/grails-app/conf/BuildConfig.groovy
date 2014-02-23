1fb6550e6b39b47fc25dd838bee3421fb71c2b92
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2014-02-23 13:51:08 +0100

    Update to Grails 2.3.3 - lots of stuff broken

--- transmart-core-db-tests/grails-app/conf/BuildConfig.groovy
+++ transmart-core-db-tests/grails-app/conf/BuildConfig.groovy
@@ -2,6 +2,21 @@ grails.project.repos.default = 'repo.thehyve.nl-snapshots'
 grails.project.repos."${grails.project.repos.default}".url = 'https://repo.thehyve.nl/content/repositories/snapshots/'
 grails.plugin.location.'transmart-core-db' = '../.'
 
+def defaultVMSettings = [
+        maxMemory: 768,
+        minMemory: 64,
+        debug:     false,
+        maxPerm:   256
+]
+
+grails.project.fork = [
+        test:    [*: defaultVMSettings, daemon:      true],
+        run:     [*: defaultVMSettings, forkReserve: false],
+        war:     [*: defaultVMSettings, forkReserve: false],
+        console: defaultVMSettings
+]
+
+grails.project.dependency.resolver = 'maven'
 grails.project.dependency.resolution = {
     log "warn"
 
@@ -15,6 +30,7 @@ grails.project.dependency.resolution = {
     dependencies {
         compile('org.hamcrest:hamcrest-library:1.3',
                 'org.hamcrest:hamcrest-core:1.3')
+        compile 'com.h2database:h2:1.3.175'
 
         test('junit:junit:4.11') {
             transitive = false /* don't bring hamcrest */
@@ -36,9 +52,11 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(":release:2.2.1",
-              ":rest-client-builder:1.0.3") {
+        build ':tomcat:7.0.47'
+        build ':release:3.0.1', ':rest-client-builder:2.0.1', {
             export = false
         }
+
+        runtime ':hibernate:3.6.10.4'
     }
 }
