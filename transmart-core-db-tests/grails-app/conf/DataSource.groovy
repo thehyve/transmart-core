5ff049ba422e6ee6565f85bd272848655fa3afe1
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2014-02-23 13:52:44 +0100

    Improve datasource config for integration tests

--- transmart-core-db-tests/grails-app/conf/DataSource.groovy
+++ transmart-core-db-tests/grails-app/conf/DataSource.groovy
@@ -1,9 +1,3 @@
-dataSource {
-    pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
-}
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
@@ -13,16 +7,22 @@ hibernate {
 
 // environment specific settings
 environments {
-    development {
-        dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-        }
-    }
     test {
         dataSource {
-            dbCreate = "update"
+            driverClassName = "org.h2.Driver"
             url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;INIT=RUNSCRIPT FROM './h2_init.sql'"
+            username = "sa"
+            password = ""
+
+            dbCreate = "update"
+            pooled = true
+
+            properties {
+                // these small values make it easier to find leaks
+                maxActive = 3
+                minIdle = 1
+                maxIdle = 1
+            }
         }
     }
 }
