bdba6c972da20bedc6144102a252bfc84ada7f9d
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2014-02-24 02:16:50 +0100

    Reduce some logging verbosity

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -12,19 +12,9 @@ grails.databinding.trimStrings = false
 //*/
 
 log4j = {
-    info  'org.codehaus.groovy.grails.web.servlet',  //  controllers
-           'org.codehaus.groovy.grails.web.pages', //  GSP
-           'org.codehaus.groovy.grails.web.sitemesh', //  layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping', // URL mapping
-           'org.codehaus.groovy.grails.commons', // core / classloading
-           'org.codehaus.groovy.grails.plugins', // plugins
-           'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
-           'org.springframework',
-           'org.hibernate',
-           'net.sf.ehcache.hibernate'
 
-    info   'org.mortbay.log'
+    warn 'org.codehaus.groovy.grails.commons.spring'
+    warn 'org.codehaus.groovy.grails.domain.GrailsDomainClassCleaner'
 
     root {
         info('stdout')
