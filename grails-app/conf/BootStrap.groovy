d39355d60c8807802fd5fba14c4c6e615fb0c079
Ruslan Forostianov <ruslan@thehyve.nl>
2014-03-03 11:54:17 +0100

    Force initialization of marshallers registrar from plug-in

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,23 +5,12 @@ import org.springframework.web.context.support.WebApplicationContextUtils
 @Log4j
 class BootStrap {
 
-    def grailsApplication
-
     def init = {servletContext ->
-        // Get spring
-        def springContext = WebApplicationContextUtils.getWebApplicationContext(servletContext)
-
-        // Force the bean being initialized
-        springContext.getBean 'marshallersRegistrar'
-
         Environment.executeForEnvironment(Environment.TEST, {
             def testData = Class.
                     forName('org.transmartproject.db.ontology.StudyTestData').newInstance()
             log.info 'About to save test data'
             testData.saveAll()
         })
-    }
-
-    def destroy = {
     }
 }
