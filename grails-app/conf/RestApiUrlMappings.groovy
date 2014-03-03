2682bfb73fbe1505a93e0fff5f2d175841f84f4f
Carlos Silva <carlos@thehyve.nl>
2014-03-03 12:04:02 +0100

    CHERKASY-58 CHERKASY-59 -implementation of /studies/<studyId>/concepts -implementation of /studies/<studyId>/concepts/<conceptId> -url mappings with <conceptId> adjusted to they work with concept paths as conceptIds

--- grails-app/conf/RestApiUrlMappings.groovy
+++ grails-app/conf/RestApiUrlMappings.groovy
@@ -6,6 +6,20 @@ class RestApiUrlMappings {
         '/studies'(resources: 'study', method: 'GET') {
             '/subjects'(controller: 'subject', resources: 'subject', includes: ['index', 'show'])
         }
+
+        "/studies/$studyId/concepts/$id**"(
+                controller: 'concept', action: 'show'
+        )
+
+        "/studies/$studyId/concepts/$conceptId**/subjects"(
+                controller: 'subject', action: 'indexByConcept'
+        )
+
+        "/studies/$studyId/concepts/$conceptId**/observations"(
+                controller: 'observation', action: 'indexByConcept'
+        )
+
+        /*
         '/studies'(resources: 'study', method: 'GET') {
             '/concepts'(resources: 'concept', method: 'GET') {
                 '/subjects'(controller: 'subject', action: 'indexByConcept')
@@ -17,13 +31,14 @@ class RestApiUrlMappings {
         }
 
         '/studies'(resources: 'study', method: 'GET') {
-            '/observations'(controller: 'observation', resources: 'observation', includes: ['index'])
-        }
-
-        '/studies'(resources: 'study', method: 'GET') {
             '/concepts'(resources: 'concept', method: 'GET') {
                 '/observations'(controller: 'observation', action: 'indexByConcept')
             }
+        }
+        */
+
+        '/studies'(resources: 'study', method: 'GET') {
+            '/observations'(controller: 'observation', resources: 'observation', includes: ['index'])
         }
 
         '/studies'(resources: 'study', method: 'GET') {
--- grails-app/controllers/org/transmartproject/rest/ConceptController.groovy
+++ grails-app/controllers/org/transmartproject/rest/ConceptController.groovy
@@ -2,17 +2,22 @@ package org.transmartproject.rest
 
 import grails.converters.JSON
 import org.transmartproject.core.ontology.ConceptsResource
+import org.transmartproject.core.ontology.Study
+import org.transmartproject.db.concept.ConceptKey
+import org.transmartproject.db.ontology.ConceptsResourceService
 
 class ConceptController {
 
+    static responseFormats = ['json', 'hal']
+
     StudyLoadingService studyLoadingServiceProxy
+    ConceptsResourceService conceptsResourceService
 
     /** GET request on /studies/XXX/concepts/
      *  This will return the list of concepts, where each concept will be rendered in its short format
     */
     def index() {
-        //TODO
-        render 'todo' as JSON
+        respond studyLoadingServiceProxy.study.ontologyTerm.allDescendants
     }
 
     /** GET request on /studies/XXX/concepts/${id}
@@ -20,9 +25,16 @@ class ConceptController {
      *
      *  @param id The id for which to return study information.
      */
-    def show(Integer id) {
-        //TODO
-        render 'todo' as JSON
+    def show(String id) {
+        String studyKey = studyLoadingServiceProxy.study.ontologyTerm.key
+        String key = studyKey + getConceptPath(id)
+
+        respond conceptsResourceService.getByKey(key)
+    }
+
+
+    private static String getConceptPath(String id) {
+        id.replace("/", "\\")
     }
 
 }
