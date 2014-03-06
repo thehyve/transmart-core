f323544471fd4de8ea3a94d276b07637f01ddc6f
Jan Hudecek <j.hudecek@nki.nl>
2014-03-06 12:28:07 +0100

    fixed acgh chromosome region

--- src/groovy/org/transmartproject/db/dataquery/highdim/acgh/AcghDataTypeResource.groovy
+++ src/groovy/org/transmartproject/db/dataquery/highdim/acgh/AcghDataTypeResource.groovy
@@ -4,14 +4,29 @@ import groovy.transform.InheritConstructors
 import org.hibernate.Query
 import org.transmartproject.core.dataquery.highdim.acgh.ChromosomalSegment
 import org.transmartproject.core.dataquery.highdim.assayconstraints.AssayConstraint
+import org.transmartproject.core.dataquery.highdim.dataconstraints.DataConstraint
 import org.transmartproject.core.exceptions.EmptySetException
+import org.transmartproject.core.exceptions.UnsupportedByDataTypeException
 import org.transmartproject.db.dataquery.highdim.AssayQuery
 import org.transmartproject.db.dataquery.highdim.HighDimensionDataTypeResourceImpl
+import org.transmartproject.db.dataquery.highdim.chromoregion.ChromosomeSegmentConstraint
+
 /**
  * Created by glopes on 11/22/13.
  */
 @InheritConstructors
 class AcghDataTypeResource extends HighDimensionDataTypeResourceImpl {
+    @Override
+    DataConstraint createDataConstraint(Map<String, Object> params, String name) throws UnsupportedByDataTypeException {
+        def ret = super.createDataConstraint(params, name)
+        if (ret instanceof ChromosomeSegmentConstraint) {
+            (ret as ChromosomeSegmentConstraint).regionPrefix='region.'
+            (ret as ChromosomeSegmentConstraint).regionStartColumn='start'
+            (ret as ChromosomeSegmentConstraint).regionEndColumn='end'
+            (ret as ChromosomeSegmentConstraint).regionChromosomeColumn='chromosome'
+        }
+        return ret;
+    }
 
     List<ChromosomalSegment> retrieveChromosomalSegments(
             List<AssayConstraint> assayConstraints) {
