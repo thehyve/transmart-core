8a2d1f83a4cc71860bf30d99c1b543deec4d7fd4
Ruslan Forostianov <ruslan@thehyve.nl>
2014-03-07 17:14:02 +0100

    [CHERKASY] Add new api method for obsrvations

--- src/main/groovy/org/transmartproject/core/dataquery/clinical/ClinicalDataResource.groovy
+++ src/main/groovy/org/transmartproject/core/dataquery/clinical/ClinicalDataResource.groovy
@@ -1,8 +1,11 @@
 package org.transmartproject.core.dataquery.clinical
 
+import org.transmartproject.core.dataquery.Patient
 import org.transmartproject.core.dataquery.TabularResult
 import org.transmartproject.core.doc.Experimental
 import org.transmartproject.core.exceptions.InvalidArgumentsException
+import org.transmartproject.core.ontology.OntologyTerm
+import org.transmartproject.core.ontology.Study
 import org.transmartproject.core.querytool.QueryResult
 
 @Experimental
@@ -15,6 +18,9 @@ public interface ClinicalDataResource {
     @Experimental
     TabularResult<ClinicalVariableColumn, PatientRow> retrieveData(List<QueryResult> patientSets,
                                                                    List<ClinicalVariable> variables)
+
+    @Experimental
+    TabularResult<ClinicalVariableColumn, PatientRow> retrieveData(Study study, List<Patient> patients, List<OntologyTerm> ontologyTerms)
 
     @Experimental
     ClinicalVariable createClinicalVariable(Map<String, Object> parameters,
