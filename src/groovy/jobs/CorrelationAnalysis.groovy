3723938b687bcea23ce23d0c52e3071a18abe042
Riza Nugraha <rnugraha@gmail.com>
2014-03-07 11:38:21 +0100

    job class for Correlation Analysis

---
+++ src/groovy/jobs/CorrelationAnalysis.groovy
@@ -0,0 +1,85 @@
+package jobs
+
+import jobs.steps.BuildTableResultStep
+import jobs.steps.CorrelationAnalysisDumpDataStep
+import jobs.steps.ParametersFileStep
+import jobs.steps.RCommandsStep
+import jobs.steps.Step
+import jobs.steps.helpers.GroupNamesHolder
+import jobs.steps.helpers.MultiNumericClinicalVariableColumnConfigurator
+import jobs.steps.helpers.SimpleAddColumnConfigurator
+import jobs.table.Table
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Scope
+import org.springframework.stereotype.Component
+
+import javax.annotation.PostConstruct
+
+
+@Component
+@Scope('job')
+class CorrelationAnalysis extends AbstractAnalysisJob {
+    @Autowired
+    SimpleAddColumnConfigurator primaryKeyColumnConfigurator
+
+    @Autowired
+    MultiNumericClinicalVariableColumnConfigurator columnConfigurator
+
+    @Autowired
+    Table table
+
+    GroupNamesHolder holder = new GroupNamesHolder()
+
+    @PostConstruct
+    void init() {
+
+        columnConfigurator.header = 'VALUE'
+        columnConfigurator.keyForConceptPaths = 'variablesConceptPaths'
+        columnConfigurator.groupNamesHolder = holder
+
+    }
+
+    @Override
+    protected List<Step> prepareSteps() {
+
+        List<Step> steps = []
+
+        steps << new ParametersFileStep(
+                temporaryDirectory: temporaryDirectory,
+                params: params)
+
+        steps << new BuildTableResultStep(
+                table:         table,
+                configurators: [columnConfigurator])
+
+        steps << new CorrelationAnalysisDumpDataStep(
+                table:              table,
+                temporaryDirectory: temporaryDirectory,
+                groupNamesHolder:   holder)
+
+        steps << new RCommandsStep(
+                temporaryDirectory: temporaryDirectory,
+                scriptsDirectory: scriptsDirectory,
+                rStatements: RStatements,
+                studyName: studyName,
+                params: params)
+
+        steps
+    }
+
+    @Override
+    protected List<String> getRStatements() {
+        [
+            '''source('$pluginDirectory/Correlation/CorrelationLoader.r')''',
+            '''Correlation.loader(input.filename='outputfile',
+                    correlation.by='$correlationBy',
+                    correlation.method='$correlationType')'''
+        ]
+    }
+
+    @Override
+    protected getForwardPath() {
+        "/correlationAnalysis/correlationAnalysisOutput?jobName=$name"
+    }
+
+}
