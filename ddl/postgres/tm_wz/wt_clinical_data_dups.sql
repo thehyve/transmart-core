9f5f3e36e4da958671fe17b8573576d1f0ee7bcf
cdejonge <c.de.jonge@philips.com>
2014-03-14 11:57:00 +0100

    Adjusted tables to support SUBJ_ID's of 30 chars (FT-743)
    OpenClinica has subject names of 30 character max. To support the automatic
    upload of data from OpenClinica we have to meet this.
    From i2b2 I had to adapt patient_dimension.sourcesystem_cd <study:visit:subject>

--- ddl/postgres/tm_wz/wt_clinical_data_dups.sql
+++ ddl/postgres/tm_wz/wt_clinical_data_dups.sql
@@ -3,7 +3,7 @@
 --
 CREATE TABLE wt_clinical_data_dups (
     site_id     character varying(50),
-    subject_id  character varying(20),
+    subject_id  character varying(30),
     visit_name  character varying(100),
     data_label  character varying(500),
     category_cd character varying(250),
