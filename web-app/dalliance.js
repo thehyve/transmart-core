631978e21629695d0022c8a0931bdb46d8b9b535
Peter Kok <peter@thehyve.nl>
2014-03-18 10:10:39 +0100

    Upgrades reference genome to GRCh37/hg19.

--- web-app/dalliance.js
+++ web-app/dalliance.js
@@ -63,148 +63,68 @@ var genomeBrowserPanel = new Ext.Panel(
 
         // create new instance of dalliance browser
         createGenomeBrowser: function () {
+
             this.genomeBrowser = new Browser({
-                chr: '17',
-                viewStart: 19274199,
-                viewEnd: 19278199,
-                cookieKey: 'human2',
+                chr:                 '22',
+                viewStart:           30700000,
+                viewEnd:             30900000,
+                cookieKey:           'human-grc_h37',
+
+                coordSystem: {
+                    speciesName: 'Human',
+                    taxon: 9606,
+                    auth: 'GRCh',
+                    version: '37',
+                    ucscName: 'hg19'
+                },
 
                 chains: {
-                    hg19ToHg18: new Chainset('http://www.derkholm.net:8080/das/hg19ToHg18/', 'GRCh37', 'NCBI36',
+                    hg18ToHg19: new Chainset('http://www.derkholm.net:8080/das/hg18ToHg19/', 'NCBI36', 'GRCh37',
                         {
                             speciesName: 'Human',
                             taxon: 9606,
-                            auth: 'GRCh',
-                            version: 37
+                            auth: 'NCBI',
+                            version: 36,
+                            ucscName: 'hg18'
                         })
                 },
 
-                sources: [
-//                    {
-//                        name:                 'Genome',
-//                        twoBitURI:            'http://www.biodalliance.org/datasets/hg18.2bit',
-//                        // uri:                  'http://www.derkholm.net:8080/das/hsa_54_36p/',
-//                        tier_type:            'sequence'
-//                    },
-//                    {
-//                        name: 'MeDIP-raw',
-//                        desc: 'MeDIP-seq reads from Nature Biotech. 26:779-785',
-//                        uri: 'http://www.derkholm.net:8080/das/medipseq_reads/'
-//                    },
-//                    {
-//                        name:                 'Genes',
-//                        desc:                 'Gene structures from Ensembl 54',
-//                        uri:                  'http://www.derkholm.net:8080/das/hsa_54_36p/',
-//                        collapseSuperGroups:  true,
-//                        provides_karyotype:   true,
-//                        provides_search:      true,
-//                        provides_entrypoints: true,
-//                        maxbins:              false
-//                    },
-//                    {
-//                        name:                 'Repeats',
-//                        uri:                  'http://www.derkholm.net:8080/das/hsa_54_36p/',
-//                        stylesheet_uri:       'http://www.biodalliance.org/stylesheets/repeats-L1.xml'
-//                    },
-//                    {
-//                        name:                 'CpG Density',
-//                        uri:                  'http://www.derkholm.net:8080/das/hg18comp/',
-//                        // stylesheet_uri:       'http://www.biodalliance.org/stylesheets/cpg-hist.xml',
-//                        style:                [{type: 'cpgoe',style: {glyph: 'LINEPLOT', FGCOLOR: 'green', HEIGHT: '50'}}]
-//                    }
-
-                    {name: 'Genome',
-                        twoBitURI: 'http://www.biodalliance.org/datasets/hg18.2bit',
-                        // uri:                  'http://www.derkholm.net:8080/das/hsa_54_36p/',
-                        tier_type: 'sequence'},
-//                    {name: 'e! transcripts',
-//                        uri: 'http://beta.rest.ensembl.org',
-//                        tier_type: 'ensembl',
-//                        species: 'human',
-//                        type: ['transcript', 'exon', 'cds'],
-//                        mapping: 'hg19ToHg18'
-//                    },
+                sources:          [{name:                 'Genome',
+                    twoBitURI:            'http://www.biodalliance.org/datasets/hg19.2bit',
+                    tier_type: 'sequence'},
                     {name: 'Genes',
-                        desc: 'Gene structures from Ensembl 54',
-                        uri: 'http://www.derkholm.net:8080/das/hsa_54_36p/',
+                        desc: 'Gene structures from GENCODE 19',
+                        bwgURI: 'http://www.biodalliance.org/datasets/gencode.bb',
+                        stylesheet_uri: 'http://www.biodalliance.org/stylesheets/gencode.xml',
                         collapseSuperGroups: true,
-                        provides_karyotype: true,
-                        provides_search: true,
-                        provides_entrypoints: true,
-                        maxbins: false,
-                        featureInfoPlugin: function (f, info) {
-                            info.add('Testing', 'This is another test!');
-                        }
-                    },
+                        trixURI: 'http://www.biodalliance.org/datasets/geneIndex.ix'},
                     {name: 'Repeats',
-                        uri: 'http://www.derkholm.net:8080/das/hsa_54_36p/',
-                        stylesheet_uri: 'http://www.biodalliance.org/stylesheets/repeats-L1.xml'},
-
-                    /** GWAS Related DAS Source
-
-
-                    {name: 'MAF Cohort',
-                        uri: 'http://gwas-test.thehyve.net/transmart/das/vcf-41/',
-                        desc: 'Minor Alele Frequency in Cohort level',
-                        stylesheet_uri: 'http://gwas-test.thehyve.net/transmart/das/vcf/stylesheet'},
-                    {name: 'MAF Summary',
-                        uri: 'http://gwas-test.thehyve.net/transmart/das/smaf-'+GLOBAL.CurrentSubsetIDs[1]+'/',
-                        desc: 'Minor Alele Frequency in Summary level',
-                        stylesheet_uri: 'http://gwas-test.thehyve.net/transmart/das/smaf/stylesheet'},
-                    {name: 'Quality of Depth',
-                        uri: 'http://gwas-test.thehyve.net/transmart/das/qd/',
-                        desc: 'Quality of Depth',
-                        stylesheet_uri: 'http://gwas-test.thehyve.net/transmart/das/qd/stylesheet'},
-                    {name: 'OV Genomic Variants',
-                        uri: 'http://gwas-test.thehyve.net/transmart/das/gv/',
-                        desc: 'Genomic Variants',
-                        stylesheet_uri: 'http://gwas-test.thehyve.net/transmart/das/gv/stylesheet'},
-
-                    **/
-
-
-                    {name: 'CpG Density',
-                        uri: 'http://www.derkholm.net:8080/das/hg18comp/',
-                        // stylesheet_uri:       'http://www.biodalliance.org/stylesheets/cpg-hist.xml',
-                        quantLeapThreshold: 0.8,
-                        forceReduction: -1,
-                        style: [
-                            {type: 'cpgoe',
-                                style: {glyph: 'LINEPLOT',
-                                    FGCOLOR: 'green', HEIGHT: '50', MIN: 0, MAX: 1.2}}
-                        ]},
-                    {name: 'MeDIP-raw',
-                        desc: 'MeDIP-seq reads from Nature Biotech. 26:779-785',
-                        uri: 'http://www.derkholm.net:8080/das/medipseq_reads/'},
-                    {name: 'BWG test',
-                        bwgURI: 'http://www.biodalliance.org/datasets/spermMethylation.bw',
-                        stylesheet_uri: 'http://www.ebi.ac.uk/das-srv/genomicdas/das/batman_seq_SP/stylesheet',
-                        quantLeapThreshold: 80},
-                    /* {name:                 'BBD test',
-                     bwgURI:               'http://www.biodalliance.org/datasets/ensGene.bb',
-                     link:                 'http://ncbi36.ensembl.org/Homo_sapiens/Gene/Summary?t=$$',
-                     collapseSuperGroups:  true,
-                     disable: true} */
-                    {name: 'BWG hist',
-                        bwgURI: 'http://www.biodalliance.org/datasets/spermMethylation.bw',
-                        style: [
-                            {type: 'default',
-                                style: {glyph: 'HISTOGRAM', HEIGHT: 50, MIN: 0, MAX: 100}}
-                        ],
-                        quantLeapThreshold: 80}
-
+                        desc: 'Repeat annotation from Ensembl 59',
+                        bwgURI: 'http://www.biodalliance.org/datasets/repeats.bb',
+                        stylesheet_uri: 'http://www.biodalliance.org/stylesheets/bb-repeats.xml'}
+                    ,{name: 'Conservation',
+                        desc: 'Conservation',
+                        bwgURI: 'http://www.biodalliance.org/datasets/phastCons46way.bw',
+                        noDownsample: true}
+                    ,{name: 'GM12878 ChromHMM', desc: 'GM12878 ChromHMM Genome Segmentation',
+                        pennant: 'http://genome.ucsc.edu/images/encodeThumbnail.jpg',
+                        bwgURI: 'http://ftp.ebi.ac.uk/pub/databases/ensembl/encode/integration_data_jan2011/byDataType/segmentations/jan2011/hub/gm12878.ChromHMM.bb',
+                        style: [{type: 'bigwig', style: {glyph: 'BOX', FGCOLOR: 'black', BGCOLOR: 'blue', HEIGHT: 8, BUMP: false, LABEL: false, ZINDEX: 20, BGITEM: true, id: 'style1'}},
+                            {type: 'bb-translation', style: {glyph: 'BOX', FGCOLOR: 'black', BGITEM: true, BGCOLOR: 'red', HEIGHT: 10, BUMP: true, ZINDEX: 20, id: 'style2'}},
+                            {type: 'bb-transcript', style: {glyph: 'BOX', FGCOLOR: 'black', BGCOLOR: 'white', HEIGHT: 10, ZINDEX: 10, BUMP: true, LABEL: true, id: 'style3'}}]}
                 ],
 
                 setDocumentTitle: true,
 
-                searchEndpoint: new DASSource('http://www.derkholm.net:8080/das/hsa_54_36p/'),
-
                 browserLinks: {
-                    Ensembl: 'http://ncbi36.ensembl.org/Homo_sapiens/Location/View?r=${chr}:${start}-${end}',
-                    UCSC: 'http://genome.ucsc.edu/cgi-bin/hgTracks?db=hg18&position=chr${chr}:${start}-${end}',
-                    Sequence: 'http://www.derkholm.net:8080/das/hg18comp/sequence?segment=${chr}:${start},${end}'
-                }
+                    Ensembl: 'http://www.ensembl.org/Homo_sapiens/Location/View?r=${chr}:${start}-${end}',
+                    UCSC: 'http://genome.ucsc.edu/cgi-bin/hgTracks?db=hg19&position=chr${chr}:${start}-${end}',
+                    Sequence: 'http://www.derkholm.net:8080/das/hg19comp/sequence?segment=${chr}:${start},${end}'
+                },
+
+                hubs: ['http://ftp.ebi.ac.uk/pub/databases/ensembl/encode/integration_data_jan2011/hub.txt'],
             });
+
             var that = this;
             setTimeout(function() { that.genomeBrowser.realInit()}, 0);
         }
