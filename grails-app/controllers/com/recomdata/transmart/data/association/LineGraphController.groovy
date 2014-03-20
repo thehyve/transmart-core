78d7fde68e1ba149d544893ba4787c755f552aee
Ruslan Forostianov <ruslan@thehyve.nl>
2014-03-20 12:49:33 +0100

    [UAT-213] Broken link to download R data in Linegraph

--- grails-app/controllers/com/recomdata/transmart/data/association/LineGraphController.groovy
+++ grails-app/controllers/com/recomdata/transmart/data/association/LineGraphController.groovy
@@ -23,6 +23,6 @@ class LineGraphController {
 		//Create a directory object so we can pass it to be traversed.
 		def tempDirectoryFile = new File(tempDirectory)
 
-		render(template: "/plugin/lineGraph_out", model:[imageLocations:imageLinks,zipLocation:RModulesOutputRenderService.zipLink], contextPath:pluginContextPath)
+		render(template: "/plugin/lineGraph_out", model:[imageLocations:imageLinks,zipLink:RModulesOutputRenderService.zipLink], contextPath:pluginContextPath)
 	}
 }
