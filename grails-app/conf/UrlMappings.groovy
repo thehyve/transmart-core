b929c68a11104ddab40dfbd91a231314a7ef79e4
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2014-03-26 12:00:41 +0100

    Add empty UrlMappings file

---
+++ grails-app/conf/UrlMappings.groovy
@@ -0,0 +1,6 @@
+class UrlMappings {
+    /* This empty mappings file seems to be required when inlining core-db
+     * plugins. Otherwise we get:
+     * Class not found loading Grails application: UrlMappings */
+    static mappings = { }
+}
