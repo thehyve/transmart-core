fd99d8a8a690ae3da44101b2234e4df3d07862ff
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2014-03-27 00:20:02 +0100

    Fix for 'no Session' error in analyses
    'could not initialize proxy - no Session' when fetching clinical data.
    We need to fetch all the relevant data when we return the data to the
    client because we don't know when the client is to close the session.
    
    See also the comment added.

--- grails-app/domain/org/transmartproject/db/querytool/QtQueryResultInstance.groovy
+++ grails-app/domain/org/transmartproject/db/querytool/QtQueryResultInstance.groovy
@@ -1,5 +1,10 @@
 package org.transmartproject.db.querytool
 
+import com.google.common.collect.Sets
+import org.hibernate.ScrollMode
+import org.hibernate.ScrollableResults
+import org.hibernate.classic.Session
+import org.transmartproject.core.dataquery.Patient
 import org.transmartproject.core.querytool.QueryResult
 import org.transmartproject.core.querytool.QueryStatus
 import org.transmartproject.db.i2b2data.PatientDimension
@@ -21,7 +26,7 @@ class QtQueryResultInstance implements QueryResult {
 	static belongsTo = QtQueryInstance
 
     static hasMany = [patientSet: QtPatientSetCollection,
-                      patients:   PatientDimension]
+                      patientsA:  PatientDimension]
 
 	static mapping = {
         table          schema: 'I2B2DEMODATA'
@@ -33,7 +38,7 @@ class QtQueryResultInstance implements QueryResult {
         errorMessage   column: 'message'
         queryInstance  column: 'query_instance_id'
 
-        patients       joinTable: [name:   'qt_patient_set_collection',
+        patientsA      joinTable: [name:   'qt_patient_set_collection',
                                    key:    'result_instance_id',
                                    column: 'patient_num']
 
@@ -53,5 +58,42 @@ class QtQueryResultInstance implements QueryResult {
     @Override
     QueryStatus getStatus() {
         QueryStatus.forId(statusTypeId)
+    }
+
+    Set<Patient> getPatients() {
+        /**
+         * The many-to-many mapping in patients_ does not serve us because
+         * it only fetches the ids of the patients from the db.
+         * If there is a way to fetch the properties of the patients when
+         * calling qtQueryResultInstance.patientsA, I have not yet found it.
+         * (Not the problem is not about fetching the association eagerly;
+         * that can be done with fetchMode; we do not need that. We just
+         * need that when the association is fetched a join with the
+         * patients table be made and the patient properties be fetched).
+         */
+        def res = Sets.newTreeSet({ PatientDimension p1, PatientDimension p2 ->
+            p1.id <=> p2.id
+        } as Comparator)
+
+        PatientDimension.withSession { Session session ->
+            def query = session.createQuery '''
+                 FROM PatientDimension p
+                 WHERE
+                     p.id IN (
+                         SELECT pset.patient.id
+                         FROM QtPatientSetCollection pset
+                         WHERE pset.resultInstance = :queryResult)
+            '''
+            query.cacheable = false
+            query.readOnly = true
+            query.setParameterList 'queryResult', owner
+
+            ScrollableResults results = query.scroll ScrollMode.FORWARD_ONLY
+            while (results.next()) {
+                res << results.get()[0]
+            }
+        }
+
+        res
     }
 }
