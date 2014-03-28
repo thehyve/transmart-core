e25f3030082516426f33c37c75e03903b10a02a7
Wim van der Linden <wim.van.der.linden@philips.com>
2014-03-28 19:21:06 +0100

    Change de_gpl_info.release_nbr to string (FT-749 + FT-751)

--- grails-app/domain/org/transmartproject/db/dataquery/highdim/DeGplInfo.groovy
+++ grails-app/domain/org/transmartproject/db/dataquery/highdim/DeGplInfo.groovy
@@ -10,7 +10,7 @@ class DeGplInfo implements Platform {
     String  organism
     Date    annotationDate
     String  markerType
-    Integer releaseNumber
+    String  releaseNumber
 
     static mapping = {
         table         schema: 'deapp'
