1d1decf21a5d425b7227cce1af81bf74b2b40dd7
Carlos Silva <carlos@thehyve.nl>
2014-03-31 14:07:44 +0200

    CHERKASY-102 removing forgotten TestData singleton

--- transmart-core-db-tests/src/groovy/org/transmartproject/db/TestData.groovy
+++ transmart-core-db-tests/src/groovy/org/transmartproject/db/TestData.groovy
@@ -4,7 +4,6 @@ import org.transmartproject.db.dataquery.clinical.ClinicalTestData
 import org.transmartproject.db.dataquery.highdim.mrna.MrnaTestData
 import org.transmartproject.db.i2b2data.I2b2Data
 import org.transmartproject.db.ontology.ConceptTestData
-import org.transmartproject.db.ontology.I2b2
 
 class TestData {
 
@@ -13,28 +12,20 @@ class TestData {
     ClinicalTestData clinicalData
     MrnaTestData mrnaData
 
-    private static TestData defaultInstance
-
     static TestData createDefault() {
         def conceptData = ConceptTestData.createDefault()
         def i2b2Data = I2b2Data.createDefault()
         def clinicalData = ClinicalTestData.createDefault(conceptData.i2b2List, i2b2Data.patients)
 
-        def mrnaData = new MrnaTestData("2")
+        def mrnaData = new MrnaTestData('2') //concept code '2'
 
-        defaultInstance = new TestData(
+        new TestData(
                 conceptData: conceptData,
                 i2b2Data: i2b2Data,
                 clinicalData: clinicalData,
                 mrnaData:  mrnaData,
         )
-        defaultInstance
     }
-
-    static getDefaultInstance() {
-        defaultInstance
-    }
-
     void saveAll() {
         conceptData?.saveAll()
         i2b2Data?.saveAll()
