3190b8cbb21717755f0c3b21989ce4b2da282730
Carlos Silva <carlos@thehyve.nl>
2014-03-31 13:02:58 +0200

    CHERKASY-102 adding mrna data to the default TestData instance

--- transmart-core-db-tests/src/groovy/org/transmartproject/db/TestData.groovy
+++ transmart-core-db-tests/src/groovy/org/transmartproject/db/TestData.groovy
@@ -1,27 +1,46 @@
 package org.transmartproject.db
 
 import org.transmartproject.db.dataquery.clinical.ClinicalTestData
+import org.transmartproject.db.dataquery.highdim.mrna.MrnaTestData
 import org.transmartproject.db.i2b2data.I2b2Data
 import org.transmartproject.db.ontology.ConceptTestData
+import org.transmartproject.db.ontology.I2b2
 
 class TestData {
 
     ConceptTestData conceptData
     I2b2Data i2b2Data
     ClinicalTestData clinicalData
+    MrnaTestData mrnaData
+
+    private static TestData defaultInstance
 
     static TestData createDefault() {
-        def concept = ConceptTestData.createDefault()
-        def i2b2 = I2b2Data.createDefault()
-        def clinical = ClinicalTestData.createDefault(concept.i2b2List, i2b2.patients)
+        def conceptData = ConceptTestData.createDefault()
+        def i2b2Data = I2b2Data.createDefault()
+        def clinicalData = ClinicalTestData.createDefault(conceptData.i2b2List, i2b2Data.patients)
 
-        new TestData(conceptData: concept, i2b2Data: i2b2, clinicalData: clinical)
+        def mrnaData = new MrnaTestData("2")
+
+        defaultInstance = new TestData(
+                conceptData: conceptData,
+                i2b2Data: i2b2Data,
+                clinicalData: clinicalData,
+                mrnaData:  mrnaData,
+        )
+        defaultInstance
+    }
+
+    static getDefaultInstance() {
+        defaultInstance
     }
 
     void saveAll() {
         conceptData?.saveAll()
         i2b2Data?.saveAll()
         clinicalData?.saveAll()
+        mrnaData?.saveAll()
+        mrnaData?.updateDoubleScaledValues()
     }
 
 }
