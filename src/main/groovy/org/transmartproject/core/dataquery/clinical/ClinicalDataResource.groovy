c14e25ae60e701ad5b441a464c91db3ac8177be3
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2014-04-02 21:54:17 +0200

    ClinicalDataResource: always take ClinicalVariable
    instead of OntologyTerm on the Set<Patient> overload of retrieveData()

--- src/main/groovy/org/transmartproject/core/dataquery/clinical/ClinicalDataResource.groovy
+++ src/main/groovy/org/transmartproject/core/dataquery/clinical/ClinicalDataResource.groovy
@@ -4,7 +4,6 @@ import org.transmartproject.core.dataquery.Patient
 import org.transmartproject.core.dataquery.TabularResult
 import org.transmartproject.core.doc.Experimental
 import org.transmartproject.core.exceptions.InvalidArgumentsException
-import org.transmartproject.core.ontology.OntologyTerm
 import org.transmartproject.core.querytool.QueryResult
 
 @Experimental
@@ -19,7 +18,8 @@ public interface ClinicalDataResource {
                                                                    List<ClinicalVariable> variables)
 
     @Experimental
-    TabularResult<ClinicalVariableColumn, PatientRow> retrieveData(Set<Patient> patients, Set<OntologyTerm> ontologyTerms)
+    TabularResult<ClinicalVariableColumn, PatientRow> retrieveData(Set<Patient> patients,
+                                                                   List<ClinicalVariable> ontologyTerms)
 
     @Experimental
     ClinicalVariable createClinicalVariable(Map<String, Object> parameters,
