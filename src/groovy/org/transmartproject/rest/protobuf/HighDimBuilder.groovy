7e754af6d5231c9ddc081fc594289678970f2daf
Jan Kanis <jan.code@jankanis.nl>
2014-04-15 10:53:30 +0200

    Adjust to changed core API and enable CompileStatic
    Use the exposed types from core API to select to generate Double columns or string columns.
    Enable CompileStatic and trim a lot of now superfluous code, as the new serialization is quite a bit simpler.

--- src/groovy/org/transmartproject/rest/protobuf/HighDimBuilder.groovy
+++ src/groovy/org/transmartproject/rest/protobuf/HighDimBuilder.groovy
@@ -1,7 +1,6 @@
 package org.transmartproject.rest.protobuf
 
-import com.google.common.collect.Iterators
-import com.google.common.collect.PeekingIterator
+import groovy.transform.CompileStatic
 import org.transmartproject.core.dataquery.DataRow
 import org.transmartproject.core.dataquery.TabularResult
 import org.transmartproject.core.dataquery.highdim.AssayColumn
@@ -18,35 +17,17 @@ import org.transmartproject.rest.protobuf.HighDimProtos.Row
  * Helper class to build HighDimTable for highdim results
  * For performance reasons, this builder has state and cannot be reused, so needs a new instance for every TabularResult
  */
-//@groovy.transform.CompileStatic
+@CompileStatic
 class HighDimBuilder {
-
-    static enum DataRowType {
-        BIO_MARKER,
-        REGION,
-
-        static DataRowType forRow(DataRow<?,?> row) {
-            //assumes all non biomarker is region
-            return (row instanceof BioMarkerDataRow) ? BIO_MARKER : REGION
-        }
-    }
 
     /**
      * The projection used (mandatory)
      */
     Projection projection
 
-    /**
-     * The projection used (mandatory)
-     */
-    DataRowType dataRowType
-
     List<AssayColumn> assayColumns
 
     OutputStream out
-
-    //no bioMarkerClosure for DataRowType.REGION
-    @Lazy private Closure<String> bioMarkerClosure =  (DataRowType.REGION == dataRowType) ? null : this.&getBioMarkerLabel
 
     @Lazy private Map<String, ? extends Class> dataColumns = { getDataProperties(projection) }()
 
@@ -57,27 +38,24 @@ class HighDimBuilder {
                                  TabularResult<AssayColumn, DataRow<AssayColumn,? extends Object>> tabularResult,
                                  OutputStream out) {
 
-        PeekingIterator<DataRow<AssayColumn,?>> it = Iterators.peekingIterator(tabularResult.rows)
-        if (!it.hasNext()) {
+        Iterator<DataRow<AssayColumn,?>> rows = tabularResult.getRows()
+        if (!rows.hasNext()) {
             throw new IllegalArgumentException("No results") //TODO: improve this
         }
 
         List<AssayColumn> cols = tabularResult.indicesList
 
-        DataRow<AssayColumn,? extends Object> sampleRow = it.peek() //peeks the 1st row, for inspecting the types
-        DataRowType dataRowType = DataRowType.forRow(sampleRow)
-
         HighDimBuilder builder = new HighDimBuilder(
                 projection: projection,
-                dataRowType: dataRowType,
                 assayColumns: cols,
                 out: out,
         )
 
         builder.createHeader().writeDelimitedTo(out)
 
-        for (DataRow<AssayColumn, ? extends Object> dataRow: it) {
-            builder.createRow(dataRow).writeDelimitedTo(out)
+        // Normal groovy iterator syntax doesn't seem to work in this case with CompileStatic
+        while(rows.hasNext()) {
+            builder.createRow(rows.next()).writeDelimitedTo(out)
         }
     }
 
@@ -101,7 +79,7 @@ class HighDimBuilder {
     }
 
     public static ColumnType typeForClass(Class clazz) {
-        clazz == Double ? ColumnType.DOUBLE : ColumnType.STRING
+        clazz in [Double, BigDecimal] ? ColumnType.DOUBLE : ColumnType.STRING
     }
 
     public static Map<String, Class> getDataProperties(Projection projection) {
@@ -116,23 +94,22 @@ class HighDimBuilder {
     private Row createRow(DataRow<AssayColumn, Object> inputRow) {
         rowBuilder.clear()
         rowBuilder.setLabel(inputRow.label)
-        if (bioMarkerClosure) {
-            rowBuilder.setBioMarker(safeString(bioMarkerClosure(inputRow)))
+        if (inputRow instanceof BioMarkerDataRow) {
+            rowBuilder.setBioMarker(safeString(((BioMarkerDataRow) inputRow).bioMarker))
         }
 
         if (isMultiValuedProjection()) {
-            // Multi column projection
-            Map<String, HighDimProtos.ColumnValue.Builder> cols = dataColumns.collectEntries {
-                [(it.key): HighDimProtos.ColumnValue.newBuilder()]
+            Map<String, HighDimProtos.ColumnValue.Builder> cols = (Map) dataColumns.collectEntries { col, type ->
+                [(col): HighDimProtos.ColumnValue.newBuilder()]
             }
 
             // transpose the data row
             for (AssayColumn a : assayColumns) {
-                def obj = inputRow[a] //can be a map or some bean
+                def obj = inputRow.getAt(a) //can be a map or some bean
 
-                dataColumns.each { col, type ->
-                    Object value = obj."$col"
-                    if (type == Double) {
+                dataColumns.each { String col, Class type ->
+                    def value = obj.getAt(col)
+                    if(typeForClass(type) == ColumnType.DOUBLE) {
                         cols[col].addDoubleValue(value as Double) //we must convert explicitly to Double
                     } else {
                         cols[col].addStringValue(safeString(value))
@@ -146,7 +123,7 @@ class HighDimBuilder {
         } else {
             // Single column projection
             columnBuilder.clear()
-            columnBuilder.addAllDoubleValue(assayColumns.collect { AssayColumn col -> inputRow[col] as Double})
+            columnBuilder.addAllDoubleValue(assayColumns.collect { AssayColumn col -> inputRow.getAt(col) as Double})
             rowBuilder.addValue(columnBuilder)
         }
 
@@ -159,16 +136,17 @@ class HighDimBuilder {
         builder.patientId = safeString(col.patientInTrialId)
 
         Map optionalValues = [
-                platform: col.platform?.id,
-                sampleCode: col.sampleCode,
-                sampleTypeName: col.sampleType?.label,
-                timepointName: col.timepoint?.label,
-                tissueTypeName: col.tissueType?.label,
+                Platform: col.platform?.id,
+                SampleCode: col.sampleCode,
+                SampleTypeName: col.sampleType?.label,
+                TimepointName: col.timepoint?.label,
+                TissueTypeName: col.tissueType?.label,
         ]
         
         optionalValues.each { field, value ->
             if(value != null) {
-                builder."$field" = value
+                // The conversion to Object[] is not necessary, but otherwise IntelliJ shows an error
+                builder.class.getDeclaredMethod('set'+field, String).invoke(builder, [value] as Object[])
             }
         }
         builder.build()
@@ -176,10 +154,6 @@ class HighDimBuilder {
 
     private boolean isMultiValuedProjection() {
         projection instanceof MultiValueProjection
-    }
-
-    private String getBioMarkerLabel(BioMarkerDataRow<?> inputRow) {
-        inputRow.bioMarker
     }
 
     public static String safeString(Object obj) {
