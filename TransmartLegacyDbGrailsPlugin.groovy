a78176f15285d183a60243db4fff3cb98a7d4b26
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2014-04-24 10:27:45 +0100

    Restauring proper legacy db 0.3 for oracle

--- TransmartLegacyDbGrailsPlugin.groovy
+++ TransmartLegacyDbGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class TransmartLegacyDbGrailsPlugin {
     // the plugin version
-    def version = "0.3-SNAPSHOT"
+    def version = "0.3-ORACLE-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
