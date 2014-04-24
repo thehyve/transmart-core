cddc33c59c6443429af9fa90e372f1e2ada526c1
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2014-04-24 17:23:30 +0100

    Again missing bits

--- grails-app/domain/org/transmart/searchapp/AuthUser.groovy
+++ grails-app/domain/org/transmart/searchapp/AuthUser.groovy
@@ -6,24 +6,6 @@ import org.hibernate.SQLQuery
 import org.hibernate.classic.Session
 import org.hibernate.type.StandardBasicTypes
 
-/*************************************************************************
- * tranSMART - translational medicine data mart
- *
- * Copyright 2008-2012 Janssen Research & Development, LLC.
- *
- * This product includes software developed at Janssen Research & Development, LLC.
- *
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
- * as published by the Free Software  * Foundation, either version 3 of the License, or (at your option) any later version, along with the following terms:
- * 1.	You may convey a work based on this program in accordance with section 5, provided that you retain the above notices.
- * 2.	You may convey verbatim copies of this program code as you receive it, in any medium, provided that you retain the above notices.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS    * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
- ******************************************************************/
 class AuthUser extends Principal {
     static transients = ['pass', 'accountExpired', 'accountLocked', 'passwordExpired']
     static hasMany = [authorities: Role, groups: UserGroup]
@@ -94,6 +76,13 @@ class AuthUser extends Principal {
 		authorities.each { if(it.authority==Role.ADMIN_ROLE) bAdmin = true; }
 		return bAdmin;
 	}
+
+    /**
+     * is this user an Data Set Explorer Admin
+     */
+    def isDseAdmin() {
+        authorities.any { it.authority == Role.DS_EXPLORER_ROLE }
+    }
 
     /*
      * Should be called with an open session and active transaction
