12073c3bcedf3502321af71f574986448aa29572
Wim van der Linden <wim.van.der.linden@philips.com>
2014-04-25 14:18:04 +0200

    Updated reference genome build identifier tests

--- transmart-core-db-tests/test/integration/org/transmartproject/db/dataquery/highdim/DeGplInfoTests.groovy
+++ transmart-core-db-tests/test/integration/org/transmartproject/db/dataquery/highdim/DeGplInfoTests.groovy
@@ -4,6 +4,7 @@ import org.junit.Test
 
 import static org.hamcrest.MatcherAssert.assertThat
 import static org.hamcrest.Matchers.*
+import org.transmartproject.core.dataquery.highdim.GenomeBuildNumber
 
 class DeGplInfoTests {
 
