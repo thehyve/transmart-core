bab2035f0b62ceb8854fe00138bdfd48c6a42a33
Florian Guitton <f.guitton@imperial.ac.uk>
2014-04-28 17:55:19 +0100

    Attempt to fix AsynJob ID issue.

--- grails-app/domain/com/recomdata/transmart/domain/i2b2/AsyncJob.groovy
+++ grails-app/domain/com/recomdata/transmart/domain/i2b2/AsyncJob.groovy
@@ -1,10 +1,18 @@
 package com.recomdata.transmart.domain.i2b2
 
+import groovy.time.TimeCategory
+import groovy.time.TimeDuration
+
 class AsyncJob {
+
+    final Set<String> TERMINATION_STATES = ['Completed', 'Cancelled', 'Error'] as Set
+
+    Long id
     String jobName
     String jobStatus
     String runTime
     Date lastRunOn
+    Date jobStatusTime
     String viewerURL
     String altViewerURL
     String results
@@ -13,10 +21,14 @@ class AsyncJob {
     static mapping = {
         table 'I2B2DEMODATA.ASYNC_JOB'
         version false
+        id column: 'ID',
+                generator: 'increment',
+                params: [sequence: 'I2B2DEMODATA.ASYNC_JOB_SEQ']
         jobName column: 'JOB_NAME'
         jobStatus column: 'JOB_STATUS'
         runTime column: 'RUN_TIME'
         lastRunOn column: 'LAST_RUN_ON'
+        jobStatusTime column: 'JOB_STATUS_TIME'
         viewerURL column: 'VIEWER_URL'
         altViewerURL column: 'ALT_VIEWER_URL'
         results column: 'JOB_RESULTS'
@@ -31,4 +43,18 @@ class AsyncJob {
         altViewerURL(nullable: true)
         results(nullable: true)
     }
+
+    TimeDuration getRunTime() {
+        def lastTime = TERMINATION_STATES.contains(jobStatus) ? this.jobStatusTime : new Date()
+        lastRunOn && lastTime ? TimeCategory.minus(lastTime, lastRunOn) : null
+    }
+
+    void setJobStatus(String jobStatus) {
+        if (this.jobStatus == jobStatus) {
+            return
+        }
+        this.jobStatusTime = new Date()
+        this.jobStatus = jobStatus
+    }
+
 }
