d24aba7a72e210904c716e419e7065b3abed865a
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2014-04-30 16:27:04 +0200

    Fix StudyImpl::equals()/hashCode()
    For some reason, neither @EqualsAndHashCode nor @ToString would detect
    the name property. Not sure what's going on here. Replace with manual
    implementation.
    
    Fixes StudiesResourceServiceTests::testGetStudySet()

--- src/groovy/org/transmartproject/db/ontology/StudyImpl.groovy
+++ src/groovy/org/transmartproject/db/ontology/StudyImpl.groovy
@@ -1,12 +1,10 @@
 package org.transmartproject.db.ontology
 
-import groovy.transform.EqualsAndHashCode
 import org.transmartproject.core.dataquery.Patient
 import org.transmartproject.core.ontology.OntologyTerm
 import org.transmartproject.core.ontology.Study
 import org.transmartproject.db.i2b2data.PatientTrialCoreDb
 
-@EqualsAndHashCode(includes = 'name')
 class StudyImpl implements Study {
 
     OntologyTerm ontologyTerm
@@ -22,6 +20,21 @@ class StudyImpl implements Study {
          * but this is more straightforward */
         PatientTrialCoreDb.executeQuery '''
             SELECT pt.patient FROM PatientTrialCoreDb pt WHERE pt.study = :study''',
-            [study: name]
+                [study: name]
+    }
+
+    boolean equals(o) {
+        if (this.is(o)) {
+            return true
+        }
+        if (getClass() != o.class) {
+            return false
+        }
+
+        ontologyTerm?.name == o.ontologyTerm?.name
+    }
+
+    int hashCode() {
+        ontologyTerm?.name?.hashCode() ?: 0
     }
 }
