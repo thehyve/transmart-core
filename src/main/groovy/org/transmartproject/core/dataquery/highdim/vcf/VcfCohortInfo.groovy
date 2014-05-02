f21e6bf8ce14441c9fb41275e6a2a596fd6f0c98
Robert Horlings <robert@isdat.nl>
2014-05-02 13:03:38 +0200

    Added methods to retrieve reference and alternative alleles

--- src/main/groovy/org/transmartproject/core/dataquery/highdim/vcf/VcfCohortInfo.groovy
+++ src/main/groovy/org/transmartproject/core/dataquery/highdim/vcf/VcfCohortInfo.groovy
@@ -4,13 +4,18 @@ package org.transmartproject.core.dataquery.highdim.vcf
  * Exposes information about the genomic variants for a given cohort
  */
 interface VcfCohortInfo {
+    // Statistics
     List<String> getAlleles()
     int getTotalAlleleCount()
     List<Integer> getAlleleCount()
     List<Double> getAlleleFrequency()
     List<GenomicVariantType> getGenomicVariantTypes()
 
-    // Methods for MAF representation
+    // Lists of reference and alternatives
+    String getReferenceAllele()
+    List<String> getAlternativeAlleles()
+    
+    // Methods for MAF representation.
     String getMajorAllele()
     String getMinorAllele()
     Double getMinorAlleleFrequency()
