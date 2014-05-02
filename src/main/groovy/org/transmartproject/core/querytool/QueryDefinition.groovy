df55b83973024a36e62c8f10c72c318522f72464
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2014-05-02 15:42:59 +0200

    Revert "Ability to associate a user with patient queries"
    This reverts commit aa6ad66c192a1e12d2876c013e1ae07ac56a007c.

--- src/main/groovy/org/transmartproject/core/querytool/QueryDefinition.groovy
+++ src/main/groovy/org/transmartproject/core/querytool/QueryDefinition.groovy
@@ -2,7 +2,6 @@ package org.transmartproject.core.querytool
 
 import groovy.transform.EqualsAndHashCode
 import org.transmartproject.core.users.ProtectedResource
-import org.transmartproject.core.users.User
 
 /**
  * A query definition is a set of panels. The data it represents is the
@@ -23,13 +22,6 @@ final class QueryDefinition implements ProtectedResource {
      */
     final List<Panel> panels
 
-    /**
-     * The username of the user who created this query. Optional. This may
-     * be used to match the user trying to retrieve a certain query result
-     * with the user that created the query, for access control reasons.
-     */
-    final String username
-
     QueryDefinition(List<Panel> panels) {
         this.name = "tranSMART's Query at " +
                 "${new Date().format('E MMM d yyyy HH:mm:ss \'GMT\'Z')}"
@@ -39,10 +31,5 @@ final class QueryDefinition implements ProtectedResource {
     QueryDefinition(String name, List<Panel> panels) {
         this.name = name
         this.panels = panels.asImmutable()
-    }
-
-    QueryDefinition(String name, List<Panel> panels, User user) {
-        this(name, panels)
-        this.username = user.username
     }
 }
