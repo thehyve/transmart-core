751bd98d03ba0cb24b2637ede6c889478bbca9ac
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2014-05-09 16:13:43 +0100

    Changes to match to match postgres

--- grails-app/domain/com/recomdata/transmart/domain/i2b2/AsyncJob.groovy
+++ grails-app/domain/com/recomdata/transmart/domain/i2b2/AsyncJob.groovy
@@ -10,13 +10,13 @@ class AsyncJob {
     Long id
     String jobName
     String jobStatus
-//    String runTime
     Date lastRunOn
     Date jobStatusTime
     String viewerURL
     String altViewerURL
     String results
     String jobType
+    String jobInputsJson
 
     static mapping = {
         table 'I2B2DEMODATA.ASYNC_JOB'
@@ -26,28 +26,29 @@ class AsyncJob {
                 params: [sequence: 'I2B2DEMODATA.ASYNC_JOB_SEQ']
         jobName column: 'JOB_NAME'
         jobStatus column: 'JOB_STATUS'
-//        runTime column: 'RUN_TIME'
         lastRunOn column: 'LAST_RUN_ON'
         jobStatusTime column: 'JOB_STATUS_TIME'
         viewerURL column: 'VIEWER_URL'
         altViewerURL column: 'ALT_VIEWER_URL'
         results column: 'JOB_RESULTS'
         jobType column: 'JOB_TYPE'
+        jobInputsJson column: 'JOB_INPUTS_JSON'
     }
 
     static constraints = {
         jobName(nullable: true)
         jobStatus(nullable: true)
-//        runTime(nullable: true)
         viewerURL(nullable: true)
         altViewerURL(nullable: true)
         results(nullable: true)
+        jobInputsJson(nullable: true)
     }
 
-//    TimeDuration getRunTime() {
-//        def lastTime = TERMINATION_STATES.contains(jobStatus) ? this.jobStatusTime : new Date()
-//        lastRunOn && lastTime ? TimeCategory.minus(lastTime, lastRunOn) : null
-//    }
+    TimeDuration getRunTime() {
+        def lastTime = TERMINATION_STATES.contains(jobStatus) ?
+                jobStatusTime : new Date()
+        lastRunOn && lastTime ? TimeCategory.minus(lastTime, lastRunOn) : null
+    }
 
     void setJobStatus(String jobStatus) {
         if (this.jobStatus == jobStatus) {
