4e2263e893f208e1cd98b410cefb1d775e1ce919
Florian Guitton <f.guitton@imperial.ac.uk>
2014-05-14 17:28:39 +0100

    Upgrade to Grails 2.3.7

--- TransmartGwasGrailsPlugin.groovy
+++ TransmartGwasGrailsPlugin.groovy
@@ -2,7 +2,7 @@ class TransmartGwasGrailsPlugin {
     // the plugin version
     def version = "1.0-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.7 > *"
+    def grailsVersion = "2.3.7 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
