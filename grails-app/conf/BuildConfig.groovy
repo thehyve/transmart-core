45c7403a4feedca05c3b62bb4220c9f48531aa0c
Florian Guitton <f.guitton@imperial.ac.uk>
2014-05-15 17:05:10 +0100

    Updagrade resource plugin to stable release

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -31,7 +31,7 @@ grails.project.dependency.resolution = {
         // runtime 'mysql:mysql-connector-java:5.1.13'
     }
     plugins {
-		compile ":resources:1.2.RC2"
+		compile(':resources:1.2.1')
 		compile(':transmart-java:1.0-SNAPSHOT')
 		compile(':biomart-domain:1.1-SNAPSHOT')
 		compile(':search-domain:1.0-SNAPSHOT')
