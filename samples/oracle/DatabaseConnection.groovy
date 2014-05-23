f034547ab6428b7aeedefd118c2e6072fab871a8
Ruslan Forostianov <ruslan@thehyve.nl>
2014-05-23 21:37:56 +0200

    Use user/password from global settings

--- samples/oracle/DatabaseConnection.groovy
+++ samples/oracle/DatabaseConnection.groovy
@@ -3,8 +3,8 @@ import groovy.sql.Sql
 static def setupDatabaseConnection() {
   String driver = "oracle.jdbc.driver.OracleDriver"
   String jdbcUrl = "jdbc:oracle:thin:@${System.getenv('ORAHOST')}:${System.getenv('ORAPORT')}:${System.getenv('ORASID')}"
-  String username = 'tm_cz'
-  String password = 'tm_cz'
+  String username = System.getenv('ORAUSER')
+  String password = System.getenv('ORAPASSWORD')
   Sql sql = Sql.newInstance jdbcUrl, username, password, driver
   sql
 }
