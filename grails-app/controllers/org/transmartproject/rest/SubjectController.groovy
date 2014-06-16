568d4504ff93bb3f5125c57b712e3963b261fc71
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2014-06-16 16:00:59 +0200

    Copyright headers; some import reorgs

--- grails-app/controllers/org/transmartproject/rest/SubjectController.groovy
+++ grails-app/controllers/org/transmartproject/rest/SubjectController.groovy
@@ -1,17 +1,37 @@
+/*
+ * Copyright 2014 Janssen Research & Development, LLC.
+ *
+ * This file is part of REST API: transMART's plugin exposing tranSMART's
+ * data via an HTTP-accessible RESTful API.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version, along with the following terms:
+ *
+ *   1. You may convey a work based on this program in accordance with
+ *      section 5, provided that you retain the above notices.
+ *   2. You may convey verbatim copies of this program code as you receive
+ *      it, in any medium, provided that you retain the above notices.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.transmartproject.rest
 
-import grails.converters.JSON
 import grails.rest.Link
-import org.codehaus.groovy.grails.web.servlet.mvc.GrailsWebRequest
-import org.springframework.web.context.request.RequestContextHolder
 import org.transmartproject.core.dataquery.Patient
 import org.transmartproject.core.dataquery.clinical.PatientsResource
-import org.transmartproject.core.exceptions.InvalidArgumentsException
 import org.transmartproject.core.exceptions.NoSuchResourceException
 import org.transmartproject.core.ontology.OntologyTerm
 import org.transmartproject.db.ontology.ConceptsResourceService
 import org.transmartproject.rest.marshallers.CollectionResponseWrapper
-import org.transmartproject.rest.marshallers.OntologyTermSerializationHelper
 import org.transmartproject.rest.ontology.OntologyTermCategory
 
 class SubjectController {
--- test/integration/org/transmartproject/rest/HighDimDataServiceTests.groovy
+++ test/integration/org/transmartproject/rest/HighDimDataServiceTests.groovy
@@ -1,3 +1,28 @@
+/*
+ * Copyright 2014 Janssen Research & Development, LLC.
+ *
+ * This file is part of REST API: transMART's plugin exposing tranSMART's
+ * data via an HTTP-accessible RESTful API.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version, along with the following terms:
+ *
+ *   1. You may convey a work based on this program in accordance with
+ *      section 5, provided that you retain the above notices.
+ *   2. You may convey verbatim copies of this program code as you receive
+ *      it, in any medium, provided that you retain the above notices.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.transmartproject.rest
 
 import grails.test.mixin.TestMixin
@@ -19,15 +44,13 @@ import org.transmartproject.db.ontology.I2b2
 import org.transmartproject.rest.HighDimTestData.HighDimResult
 import org.transmartproject.rest.protobuf.HighDimBuilder
 import org.transmartproject.rest.protobuf.HighDimProtos
-import org.transmartproject.rest.protobuf.HighDimProtos.Row
 import org.transmartproject.rest.protobuf.HighDimProtos.ColumnSpec
 import org.transmartproject.rest.protobuf.HighDimProtos.ColumnSpec.ColumnType
+import org.transmartproject.rest.protobuf.HighDimProtos.Row
 
 import static org.hamcrest.MatcherAssert.assertThat
 import static org.hamcrest.Matchers.is
-import static org.thehyve.commons.test.FastMatchers.listOf
-import static org.thehyve.commons.test.FastMatchers.listOfWithOrder
-import static org.thehyve.commons.test.FastMatchers.propsWith
+import static org.thehyve.commons.test.FastMatchers.*
 
 @TestMixin(IntegrationTestMixin)
 class HighDimDataServiceTests {
