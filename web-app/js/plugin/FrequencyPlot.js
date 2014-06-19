2a26142e94364c3ea0cfae14baa866a970f01bfe
Riza Nugraha <rnugraha@gmail.com>
2014-06-19 16:42:45 +0200

    :fish:

--- web-app/js/plugin/FrequencyPlot.js
+++ web-app/js/plugin/FrequencyPlot.js
@@ -3,7 +3,7 @@ var frequencyPlotView;
 /**
  * Where everything starts
  */
-function loadAcghFrequencyPlotView(){
+function loadAcghFrequencyPlotView() {
     frequencyPlotView = new FrequencyPlotView();
 }
 
@@ -18,34 +18,37 @@ var FrequencyPlotInputWidget = Ext.extend(GenericAnalysisInputBar, {
     acghPanel: null,
     groupPanel: null,
 
-    constructor: function(config) {
+    constructor: function (config) {
         FrequencyPlotInputWidget.superclass.constructor.apply(this, arguments);
         this.init();
     },
 
-    init: function() {
+    init: function () {
 
         // define child panel configs
-        var childPanelConfig = [{
-            title: 'Array CGH',
-            id: 'fp-input-acgh',
-            isDroppable: true,
-            notifyFunc: dropOntoCategorySelection,
-            toolTipTitle: 'Tip: array CGH',
-            toolTipTxt: 'Drag and drop aCGH data here.',
-            columnWidth:.5
-        },{
-            title: 'Group',
-            id: 'fp-input-group',
-            isDroppable: true,
-            notifyFunc: dropOntoCategorySelection,
-            toolTipTitle: 'Tip: Group',
-            toolTipTxt: 'Drag and drop clinical variables to define multiple groups. '+
-                'Please keep in mind that only one variable can be compared, '+
-                'e.g. gender (female) with gender (male); '+
-                'not gender (female) with age (>60)!',
-            columnWidth:.5
-        }];
+        var childPanelConfig = [
+            {
+                title: 'Array CGH',
+                id: 'fp-input-acgh',
+                isDroppable: true,
+                notifyFunc: dropOntoCategorySelection,
+                toolTipTitle: 'Tip: array CGH',
+                toolTipTxt: 'Drag and drop aCGH data here.',
+                columnWidth: .5
+            },
+            {
+                title: 'Group',
+                id: 'fp-input-group',
+                isDroppable: true,
+                notifyFunc: dropOntoCategorySelection,
+                toolTipTitle: 'Tip: Group',
+                toolTipTxt: 'Drag and drop clinical variables to define multiple groups. ' +
+                    'Please keep in mind that only one variable can be compared, ' +
+                    'e.g. gender (female) with gender (male); ' +
+                    'not gender (female) with age (>60)!',
+                columnWidth: .5
+            }
+        ];
 
         // create child panels
         this.acghPanel = this.createChildPanel(childPanelConfig[0]);
@@ -63,20 +66,20 @@ var FrequencyPlotInputWidget = Ext.extend(GenericAnalysisInputBar, {
 var FrequencyPlotView = Ext.extend(GenericAnalysisView, {
 
     // input panel
-    inputBar : null,
+    inputBar: null,
 
     // job type
     jobType: 'acghFrequencyPlot',
 
     // job info
-    jobInfo : null,
+    jobInfo: null,
 
     // constructor
-    constructor: function() {
+    constructor: function () {
         this.init();
     },
 
-    init: function() {
+    init: function () {
 
         // first of all, let's reset all major components
         this.resetAll();
@@ -96,14 +99,14 @@ var FrequencyPlotView = Ext.extend(GenericAnalysisView, {
         Ext.destroy(this.inputBar);
     },
 
-    createInputToolBar: function() {
+    createInputToolBar: function () {
         var _this = this;
 
         /**
          * Buttons for Input Panel
          * @type {Array}
          */
-        var _fpInputBarBtnList = ['->',{  // '->' making it right aligned
+        var _fpInputBarBtnList = ['->', {  // '->' making it right aligned
             xtype: 'button',
             text: 'Run Analysis',
             scale: 'medium',
@@ -128,19 +131,19 @@ var FrequencyPlotView = Ext.extend(GenericAnalysisView, {
 
         // get image path
         Ext.Ajax.request({
-            url: pageInfo.basePath+"/AcghFrequencyPlot/imagePath",
+            url: pageInfo.basePath + "/AcghFrequencyPlot/imagePath",
             method: 'POST',
-            success: function(result, request){
+            success: function (result, request) {
 
                 imagePath = result.responseText;
 
                 _this.resultPanel = new GenericPlotPanel({
                     id: 'plotResultCurve',
                     renderTo: 'freq_plot_wrapper',
-                    width:'100%',
-                    frame:true,
-                    height:600,
-                    defaults: {autoScroll:true}
+                    width: '100%',
+                    frame: true,
+                    height: 600,
+                    defaults: {autoScroll: true}
                 });
 
                 // Getting the template as blue print for survival curve plot.
@@ -167,9 +170,9 @@ var FrequencyPlotView = Ext.extend(GenericAnalysisView, {
                 frequencyPlotTpl.overwrite(Ext.get('freq_plot_wrapper'), region);
 
                 // generate download button
-                var exportBtn = new Ext.Button ({
-                    text : 'Download Result',
-                    iconCls : 'downloadbutton',
+                var exportBtn = new Ext.Button({
+                    text: 'Download Result',
+                    iconCls: 'downloadbutton',
                     renderTo: 'downloadBtn',
                     handler: function () {
                         _this.downloadFrequencyPlotResult(jobName);
@@ -189,30 +192,31 @@ var FrequencyPlotView = Ext.extend(GenericAnalysisView, {
         try {
             Ext.destroy(Ext.get('downloadIframe'));
         }
-        catch(e) {}
+        catch (e) {
+        }
 
         // get the file
         Ext.DomHelper.append(document.body, {
             tag: 'iframe',
-            id:'downloadIframe',
+            id: 'downloadIframe',
             frameBorder: 0,
             width: 0,
             height: 0,
             css: 'display:none;visibility:hidden;height:0px;',
-            src: pageInfo.basePath+"/AcghFrequencyPlot/zipFile?jobName=" + jobName
+            src: pageInfo.basePath + "/AcghFrequencyPlot/zipFile?jobName=" + jobName
         });
     },
 
-    onJobFinish: function(jobName, view) {
+    onJobFinish: function (jobName, view) {
         this.renderResults(jobName, view);
     },
 
     getJobByName: function (jobName, view) {
         var _this = this;
 
-        Ext.Ajax.request ({
+        Ext.Ajax.request({
             // retrieve information about the job (status, inputs, run-time, etc)
-            url: pageInfo.basePath+"/asyncJob/getjobbyname",
+            url: pageInfo.basePath + "/asyncJob/getjobbyname",
             method: 'GET',
             success: function (result, request) {
                 var resultJSON = JSON.parse(result.responseText);
@@ -228,7 +232,7 @@ var FrequencyPlotView = Ext.extend(GenericAnalysisView, {
 
     },
 
-    renderResults: function(jobName, view) {
+    renderResults: function (jobName, view) {
         this.getJobByName(jobName, view);
         this.createResultPlotPanel(jobName, view);
     },
@@ -251,22 +255,24 @@ var FrequencyPlotView = Ext.extend(GenericAnalysisView, {
             // create a string of all the concepts we need for the i2b2 data.
             var variablesConceptCode = acghVal;
             variablesConceptCode += groupVals != '' ? "|" + groupVals : "";
- 
+
             // compose params
             var formParams = {
                 regionVariable: acghVal,
                 groupVariable: groupVals,
                 variablesConceptPaths: variablesConceptCode,
                 analysisConstraints: JSON.stringify({
-                    "job_type":  _this.jobType,
+                    "job_type": _this.jobType,
                     "data_type": "acgh",
                     "assayConstraints": {
                         "patient_set": [GLOBAL.CurrentSubsetIDs[1], GLOBAL.CurrentSubsetIDs[2]],
                         "assay_id_list": null,
-                        "ontology_term": [{
-                            'term': acghVal,
-                            'options': {'type': "default"}
-                        }],
+                        "ontology_term": [
+                            {
+                                'term': acghVal,
+                                'options': {'type': "default"}
+                            }
+                        ],
                         "trial_name": null
                     },
                     "dataConstraints": {
@@ -291,18 +297,18 @@ var FrequencyPlotView = Ext.extend(GenericAnalysisView, {
 
         return  [
             {
-                "label" : "Array CGH Data",
-                "el" : this.inputBar.acghPanel.getInputEl(),
-                "validations" : [
-                    {type:"REQUIRED"},
-                    {type:"HIGH_DIMENSIONAL_ACGH"}
+                "label": "Array CGH Data",
+                "el": this.inputBar.acghPanel.getInputEl(),
+                "validations": [
+                    {type: "REQUIRED"},
+                    {type: "HIGH_DIMENSIONAL_ACGH"}
                 ]
             }
-            /**
-             *  Group doesn't need to be defined for a frequency plot nor need to consist of at least two elements
-             *  If no group is defined, a single frequency plot of all subjects in the cohort will be created
-             *  If one group is defined, a single frequency plot of that group within the cohort will be created
-             * {
+        /**
+         *  Group doesn't need to be defined for a frequency plot nor need to consist of at least two elements
+         *  If no group is defined, a single frequency plot of all subjects in the cohort will be created
+         *  If one group is defined, a single frequency plot of that group within the cohort will be created
+         * {
              *    "label" : "Group",
              *    "el" : this.inputBar.groupPanel.getInputEl(),
              *    "validations" : [
@@ -310,7 +316,7 @@ var FrequencyPlotView = Ext.extend(GenericAnalysisView, {
              *       {type:"GROUP_VARIABLE"}
              *    ]
              * }
-             */
+         */
         ];
     }
 
