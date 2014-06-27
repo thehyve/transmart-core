e28dcfbf7512a7c557f79bcd45b4c84fccc72a25
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2014-06-27 18:12:06 +0200

    ddl/oracle: fix grants

--- ddl/oracle/grants.php
+++ ddl/oracle/grants.php
@@ -12,11 +12,15 @@ $spec = [
         ['I2B2METADATA', '*VIEW',                     'READ', ''],
         ['SEARCHAPP',    '*TABLE',                    'WRITE', ''],
         ['SEARCHAPP',    '*VIEW',                     'READ', ''],
+        ['SEARCHAPP',    '*SEQUENCE',                 'READ', ''],
+        ['FMAPP',        '*TABLE',                    'WRITE', ''],
+        ['AMAPP',        '*TABLE',                    'READ', ''],
         ['I2B2DEMODATA', 'QT_QUERY_MASTER',           'WRITE', ''],
         ['I2B2DEMODATA', 'QT_QUERY_INSTANCE',         'WRITE', ''],
         ['I2B2DEMODATA', 'QT_QUERY_RESULT_INSTANCE',  'WRITE', ''],
         ['I2B2DEMODATA', 'QT_PATIENT_SET_COLLECTION', 'WRITE', ''],
         ['I2B2DEMODATA', 'ASYNC_JOB',                 'WRITE', ''],
+        ['I2B2DEMODATA', '*SEQUENCE',                 'READ', ''],
         ['DEAPP',        'DE_SAVED_COMPARISON',       'WRITE', ''],
         ['BIOMART',      'BIO_ASSAY_FEATURE_GROUP',   'WRITE', ''],
         ['BIOMART',      'BIO_MARKER',                'WRITE', ''],
@@ -43,7 +47,23 @@ $spec = [
     ],
     'SEARCHAPP' => [
         ['BIOMART', 'BIO_ASSAY_DATA_ANNOTATION', 'READ', 'GRANT_OPTION'],
-	    ['BIOMART', 'BIO_MARKER_CORREL_MV', 'READ', 'GRANT_OPTION']
+        ['BIOMART', 'BIO_MARKER_CORREL_MV',      'READ', 'GRANT_OPTION']
+    ],
+    'BIOMART' => [
+        ['DEAPP', 'DE_METABOLITE_SUPER_PATHWAYS', 'READ', 'GRANT_OPTION'],
+        ['DEAPP', 'DE_METABOLITE_SUB_PATHWAYS',   'READ', 'GRANT_OPTION'],
+        ['DEAPP', 'DE_METABOLITE_SUB_PWAY_METAB', 'READ', 'GRANT_OPTION'],
+        ['DEAPP', 'DE_METABOLITE_ANNOTATION',     'READ', 'GRANT_OPTION'],
+        ['DEAPP', 'DE_MRNA_ANNOTATION',           'READ', 'GRANT_OPTION'],
+    ],
+    'AMAPP' => [
+        ['BIOMART', 'BIO_DATA_UID',       'READ', 'GRANT_OPTION'],
+        ['BIOMART', 'BIO_CONCEPT_CODE',   'READ', 'GRANT_OPTION'],
+        ['BIOMART', 'BIO_DISEASE',        'READ', 'GRANT_OPTION'],
+        ['BIOMART', 'BIO_ASSAY_PLATFORM', 'READ', 'GRANT_OPTION'],
+        ['BIOMART', 'BIO_COMPOUND',       'READ', 'GRANT_OPTION'],
+        ['BIOMART', 'BIO_MARKER',         'READ', 'GRANT_OPTION'],
+        ['BIOMART', 'BIO_OBSERVATION',    'READ', 'GRANT_OPTION'],
     ],
 ];
 
