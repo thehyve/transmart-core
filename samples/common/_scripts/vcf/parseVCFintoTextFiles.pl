88329d5a6eb5b77a3d9cbd1d14988532b14af393
Robert Horlings <robert@isdat.nl>
2014-07-01 12:46:26 +0200

    Added missing semicolon

--- samples/common/_scripts/vcf/parseVCFintoTextFiles.pl
+++ samples/common/_scripts/vcf/parseVCFintoTextFiles.pl
@@ -527,7 +527,7 @@ close OUT;
 close POPULATION_INFO;
 close POPULATION_DATA;
 
-print "\n"
+print "\n";
 print "----------------------------------------------------\n";
 while ( my ($var, $count) = each(%counts) ) {
 	if( $count > 0 ) {
