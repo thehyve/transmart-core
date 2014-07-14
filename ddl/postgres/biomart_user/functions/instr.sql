eb3c6d6eb52b424c6fdd12ad4fde7a66360a3433
Terry-Weymouth <weymouth@umich.edu>
2014-07-14 09:48:40 -0400

    Updated instr.sql from similar version in tm_cz

--- ddl/postgres/biomart_user/functions/instr.sql
+++ ddl/postgres/biomart_user/functions/instr.sql
@@ -1,120 +1,18 @@
-2--
--- Name: instr(character varying, character varying); Type: FUNCTION; Schema: biomart_user; Owner: -
+-- instr functions that mimic Oracle's counterpart
+-- Syntax: instr(string1, string2, [n], [m]) where [] denotes optional, integer, parameters.
+-- 
+-- Searches string1 beginning at the nth character for the mth occurrence
+-- of string2.  If n is negative, search backwards.  If m is not passed,
+-- assume 1 (search starts at first character).
 --
-CREATE FUNCTION instr(character varying, character varying) RETURNS integer
-    LANGUAGE plpgsql
-    AS $_$
+-- Name: instr(character varying, character varying, integer, integer); 
+-- Type: FUNCTION; Schema: biomart_user; Owner: biomart_user
+--
+CREATE FUNCTION instr(string character varying, string_to_search character varying, 
+beg_index integer DEFAULT 1, occur_index integer DEFAULT 1) RETURNS integer
+    LANGUAGE plpgsql IMMUTABLE STRICT
+    AS $$
 DECLARE
-    pos integer;
-BEGIN
-    pos:= instr($1, $2, 1);
-    RETURN pos;
-END;
-$_$;
-
---
--- Name: instr(character varying, character varying, character varying); Type: FUNCTION; Schema: biomart_user; Owner: -
---
-CREATE FUNCTION instr(character varying, character varying, character varying) RETURNS integer
-    LANGUAGE plpgsql
-    AS $_$
-DECLARE
-    string ALIAS FOR $1;
-    string_to_search ALIAS FOR $2;
-    beg_index ALIAS FOR $3;
-    pos integer NOT NULL DEFAULT 0;
-    temp_str varchar;
-    beg integer;
-    length integer;
-    ss_length integer;
-BEGIN
-    IF beg_index > 0 THEN
-        temp_str := substring(string FROM beg_index);
-        pos := position(string_to_search IN temp_str);
-
-        IF pos = 0 THEN
-            RETURN 0;
-        ELSE
-            RETURN pos + beg_index - 1;
-        END IF;
-    ELSE
-        ss_length := char_length(string_to_search);
-        length := char_length(string);
-        beg := length + beg_index - ss_length + 2;
-
-        WHILE beg > 0 LOOP
-            temp_str := substring(string FROM beg FOR ss_length);
-            pos := position(string_to_search IN temp_str);
-
-            IF pos > 0 THEN
-                RETURN beg;
-            END IF;
-
-            beg := beg - 1;
-        END LOOP;
-
-        RETURN 0;
-    END IF;
-END;
-$_$;
-
---
--- Name: instr(character varying, character varying, integer); Type: FUNCTION; Schema: biomart_user; Owner: -
---
-CREATE FUNCTION instr(character varying, character varying, integer) RETURNS integer
-    LANGUAGE plpgsql
-    AS $_$
-DECLARE
-    string ALIAS FOR $1;
-    string_to_search ALIAS FOR $2;
-    beg_index ALIAS FOR $3;
-    pos integer NOT NULL DEFAULT 0;
-    temp_str varchar;
-    beg integer;
-    length integer;
-    ss_length integer;
-BEGIN
-    IF beg_index > 0 THEN
-        temp_str := substring(string FROM beg_index);
-
-        IF pos = 0 THEN
-            RETURN 0;
-        ELSE
-            RETURN pos + beg_index - 1;
-        END IF;
-    ELSE
-        ss_length := char_length(string_to_search);
-        length := char_length(string);
-        beg := length + beg_index - ss_length + 2;
-
-        WHILE beg > 0 LOOP
-            temp_str := substring(string FROM beg FOR ss_length);
-            pos := position(string_to_search IN temp_str);
-
-            IF pos > 0 THEN
-	            RETURN beg;
-                END IF;
-            END IF;
-
-            beg := beg - 1;
-        END LOOP;
-
-        RETURN 0;
-    END IF;
-END;
-$_$;
-
---
--- Name: instr(character varying, character varying, integer, integer); Type: FUNCTION; Schema: biomart_user; Owner: -
---
-CREATE FUNCTION instr(character varying, character varying, integer, integer) RETURNS integer
-    LANGUAGE plpgsql
-    AS $_$
-DECLARE
-    string ALIAS FOR $1;
-    string_to_search ALIAS FOR $2;
-    beg_index ALIAS FOR $3;
-    occur_index ALIAS FOR $4;
     pos integer NOT NULL DEFAULT 0;
     occur_number integer NOT NULL DEFAULT 0;
     temp_str varchar;
@@ -126,7 +24,7 @@ BEGIN
     IF beg_index > 0 THEN
         beg := beg_index;
         temp_str := substring(string FROM beg_index);
-
+        
         FOR i IN 1..occur_index LOOP
             pos := position(string_to_search IN temp_str);
 
@@ -167,5 +65,23 @@ BEGIN
         RETURN 0;
     END IF;
 END;
-$_$;
+$$;
 
+--
+-- Name: instr(character varying, character varying, character varying); Type: FUNCTION; Schema: biomart_user; Owner: -
+--
+-- Note: this varient, while not strictly correct was found in the code and fixed here
+-- (rather then fixing the code!) - so, until the problem is (re-)located in the code and 
+-- fixed there, this varient will be necessary.
+-- 
+CREATE FUNCTION instr(character varying, character varying, character varying) RETURNS integer
+    LANGUAGE plpgsql
+    AS $_$
+DECLARE
+    string ALIAS FOR $1;
+    string_to_search ALIAS FOR $2;
+    beg_index ALIAS FOR $3;
+BEGIN	
+	return biomart_user.instr($1,$2,CAST($3 as int4));
+END;
+$_$;
