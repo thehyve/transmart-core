2a8e329fc779614781a86e5c671c56b84748cb2e
Peter Rice <p.rice@imperial.ac.uk>
2014-07-15 15:07:05 +0100

    updates from test server

--- ddl/postgres/biomart/bio_data_observation.sql
+++ ddl/postgres/biomart/bio_data_observation.sql
@@ -2,8 +2,8 @@
 -- Name: bio_data_observation; Type: TABLE; Schema: biomart; Owner: -
 --
 CREATE TABLE bio_data_observation (
-    bio_data_id bigint,
-    bio_observation_id bigint,
+    bio_data_id bigint NOT NULL,
+    bio_observation_id bigint NOT NULL,
     etl_source character varying(100)
 );
 
