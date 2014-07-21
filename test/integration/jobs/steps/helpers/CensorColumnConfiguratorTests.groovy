a5f345a9ec29dda592ed8d87e35f8c87d150b41c
Peter Kok <peter@thehyve.nl>
2014-07-21 14:24:12 +0200

    Update censoring test, add case for unspecified censoring

--- test/integration/jobs/steps/helpers/CensorColumnConfiguratorTests.groovy
+++ test/integration/jobs/steps/helpers/CensorColumnConfiguratorTests.groovy
@@ -3,8 +3,10 @@ package jobs.steps.helpers
 import com.google.common.collect.Lists
 import grails.test.mixin.TestMixin
 import jobs.UserParameters
+import jobs.table.Column
 import jobs.table.Table
 import jobs.table.columns.CensorColumn
+import jobs.table.columns.PrimaryKeyColumn
 import org.junit.Before
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -39,52 +41,76 @@ class CensorColumnConfiguratorTests {
     void setUp() {
         initializeAsBean configuratorTestsHelper
 
-        testee.header             = 'CENSOR'
+        testee.header = 'CENSOR'
         testee.keyForConceptPaths = 'censoringVariable'
     }
 
     @Test
     void testCensoring() {
         params.@map.putAll([
-                censoringVariable  : BUNDLE_OF_CLINICAL_CONCEPT_PATH[0],
-                result_instance_id1: RESULT_INSTANCE_ID1,
-                result_instance_id2: RESULT_INSTANCE_ID2,
-        ])
-
-        List<ClinicalVariableColumn> columns =
-                createClinicalVariableColumns([BUNDLE_OF_CLINICAL_CONCEPT_PATH[0]])
-        setupClinicalResult(1, columns, ['var 1'])
-
-        configuratorTestsHelper.play {
-            testee.addColumn()
-            table.buildTable()
-
-            assertThat table.result, contains(contains(CensorColumn.CENSORING_TRUE))
-
-        }
-    }
-
-    @Test
-    void testCensoringMultiple() {
-        params.@map.putAll([
-                censoringVariable  : BUNDLE_OF_CLINICAL_CONCEPT_PATH.join('|'),
+                censoringVariable: BUNDLE_OF_CLINICAL_CONCEPT_PATH.join('|'),
                 result_instance_id1: RESULT_INSTANCE_ID1,
                 result_instance_id2: RESULT_INSTANCE_ID2,
         ])
 
         List<ClinicalVariableColumn> columns =
                 createClinicalVariableColumns(BUNDLE_OF_CLINICAL_CONCEPT_PATH)
-        setupClinicalResult(columns, [['var 1', null, 'var 3'],
-                                      [null, null, null]])
+        setupClinicalResult(2, columns, ['var 1', null, 'var 3',
+                null, null, null])
 
         configuratorTestsHelper.play {
+            table.addColumn(new PrimaryKeyColumn(header: 'PK'), [] as Set)
             testee.addColumn()
             table.buildTable()
 
             def res = Lists.newArrayList table.result
             assertThat res,
-                    containsInAnyOrder(contains(CensorColumn.CENSORING_TRUE),
-                                       contains(CensorColumn.CENSORING_FALSE))
+                    containsInAnyOrder(
+                            contains(
+                                    'subject id #1',
+                                    CensorColumn.CENSORING_TRUE
+                            ),
+                            contains(
+                                    'subject id #2',
+                                    CensorColumn.CENSORING_FALSE
+                            )
+                    )
+
+        }
+    }
+
+    @Test
+    void testCensoringUnspecified() {
+        params.@map.putAll([
+                censoringVariable: '',
+                result_instance_id1: RESULT_INSTANCE_ID1,
+                result_instance_id2: RESULT_INSTANCE_ID2,
+        ])
+
+        configuratorTestsHelper.play {
+            table.addColumn(new PrimaryKeyColumn(header: 'PK'), [] as Set)
+
+            Column stubColumn = new StubColumn(header: 'STUB',
+                    data: ['subject id #1': 23, 'subject id #2': 42])
+            table.addColumn stubColumn, Collections.emptySet()
+
+            testee.addColumn()
+            table.buildTable()
+
+            def res = Lists.newArrayList table.result
+            assertThat res,
+                    containsInAnyOrder(
+                            contains(
+                                    'subject id #1',
+                                    23,
+                                    CensorColumn.CENSORING_TRUE
+                            ),
+                            contains(
+                                    'subject id #2',
+                                    42,
+                                    CensorColumn.CENSORING_TRUE
+                            )
+                    )
 
         }
     }
