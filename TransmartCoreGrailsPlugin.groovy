21c7fa5a3f4eccc6df7ae6f770d4126a48943e87
Terry Weymouth <weymouth@umich.edu>
2014-07-30 17:46:03 -0400

    updated version to 1.2.0 in preperation for release

--- TransmartCoreGrailsPlugin.groovy
+++ TransmartCoreGrailsPlugin.groovy
@@ -26,7 +26,7 @@ import org.transmartproject.db.support.DatabasePortabilityService
 
 class TransmartCoreGrailsPlugin {
     // the plugin version
-    def version = "1.1-LH-SNAPSHOT"
+    def version = "1.2.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
