3870839ea59abdbefb0375e8df7a40ad69bae710
Terry Weymouth <weymouth@umich.edu>
2014-07-30 17:48:47 -0400

    updated version to 1.2.0 in preperation for release

--- TransmartLegacyDbGrailsPlugin.groovy
+++ TransmartLegacyDbGrailsPlugin.groovy
@@ -1,6 +1,7 @@
 class TransmartLegacyDbGrailsPlugin {
     // the plugin version
-    def version = "0.3-ORACLE-SNAPSHOT"
+    // derived from def version = "0.3-ORACLE-SNAPSHOT"
+	def version = "1.2.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
