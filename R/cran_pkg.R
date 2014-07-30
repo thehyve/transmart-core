0a5a864aa56b2c1bf32783eb6872884caa44a54d
Ruslan Forostianov <ruslan@thehyve.nl>
2014-07-30 14:48:01 +0200

    Add R packages required by group and surv. test

--- R/cran_pkg.R
+++ R/cran_pkg.R
@@ -1,5 +1,5 @@
 required.packages <- c("reshape2", "ggplot2", "data.table", "Cairo",
-		"snowfall", "gplots", "Rserve");
+		"snowfall", "gplots", "Rserve", "foreach", "doParallel");
 new.packages <- required.packages[
 		!(required.packages %in% installed.packages()[,"Package"])];
 if (length(new.packages))
