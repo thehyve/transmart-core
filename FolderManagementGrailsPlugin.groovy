eef5716e4f4d31bee43157006f8b1d1a7c9a2bc9
Terry Weymouth <weymouth@umich.edu>
2014-07-30 17:49:16 -0400

    updated version to 1.2.0 in preperation for release

--- FolderManagementGrailsPlugin.groovy
+++ FolderManagementGrailsPlugin.groovy
@@ -1,6 +1,7 @@
 class FolderManagementGrailsPlugin {
     // the plugin version
-    def version = "1.2-SNAPSHOT"
+    // derived from def version = "1.2-SNAPSHOT"
+	def version = "1.2.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3.7 > *"
     // the other plugins this plugin depends on
