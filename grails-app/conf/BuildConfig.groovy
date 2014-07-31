b101aa243969982fbd53dae68127378a95a56de9
Terry-Weymouth <weymouth@umich.edu>
2014-07-31 16:18:21 -0400

    Modified dependencies to 1.2.0 core-api

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -62,7 +62,7 @@ grails.project.dependency.resolution = {
 
         /* serializable ImmutableMap only on guava 16 */
         compile group: 'com.google.guava', name: 'guava', version: '16.0-dev-20140115-68c8348'
-        compile 'org.transmartproject:transmart-core-api:1.0-LH-SNAPSHOT'
+        compile 'org.transmartproject:transmart-core-api:1.2.0'
 
         /* compile instead of test due to technical limitations
          * (referenced from resources.groovy) */
