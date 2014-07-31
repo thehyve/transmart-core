dd16f68a396f459162d14e7fde29c0297ad8dcc4
Terry-Weymouth <weymouth@umich.edu>
2014-07-31 17:01:38 -0400

    Modified dependencies to 1.2.0 for all transmart plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -32,11 +32,11 @@ grails.project.dependency.resolution = {
     }
     plugins {
 		compile(':resources:1.2.1')
-		compile(':transmart-java:1.0-SNAPSHOT')
-		compile(':biomart-domain:1.1-SNAPSHOT')
-		compile(':search-domain:1.0-SNAPSHOT')
-		compile(':folder-management:1.2-SNAPSHOT')
-		compile(':transmart-legacy-db:0.3-ORACLE-SNAPSHOT')
+		compile(':transmart-java:1.2.0')
+		compile(':biomart-domain:1.2.0')
+		compile(':search-domain:1.2.0')
+		compile(':folder-management:1.2.0')
+		compile(':transmart-legacy-db:1.2.0')
 		compile(':spring-security-core:2.0-RC2')
 		compile(':quartz:1.0-RC2')
         compile(':mail:1.0')
