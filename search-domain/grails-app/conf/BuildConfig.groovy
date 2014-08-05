c9b40bcf4b5d2dcdc00e8c06ec74b1099c9d3feb
Terry-Weymouth <weymouth@umich.edu>
2014-08-05 08:26:34 -0400

    Update all dependencies to 1.2.1-SNAPSHOT

--- search-domain/grails-app/conf/BuildConfig.groovy
+++ search-domain/grails-app/conf/BuildConfig.groovy
@@ -25,7 +25,7 @@ grails.project.dependency.resolution = {
 		build(":release:2.2.1",
 			":rest-client-builder:1.0.3") {
 		export = false}
-        compile ':biomart-domain:1.2.0'
-        compile ':transmart-java:1.2.0'
+        compile ':biomart-domain:1.2.1-SNAPSHOT'
+        compile ':transmart-java:1.2.1-SNAPSHOT'
     }
 }
