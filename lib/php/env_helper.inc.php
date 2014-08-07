650a90f44b30a84a6271196ed2296c16c2b79e1b
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2014-08-07 15:53:00 +0200

    Add dependencies on vars file
    when a generated file depends on the values of environment variables.

--- lib/php/env_helper.inc.php
+++ lib/php/env_helper.inc.php
@@ -8,3 +8,7 @@ $host = (empty($_ENV['PGHOST']) || $_ENV['PGHOST'][0] == '/')
 
 $biomart_user_pwd = isset($_ENV['BIOMART_USER_PWD']) ?
 	$_ENV['BIOMART_USER_PWD'] : 'biomart_user';
+
+function get_env($key, $default) {
+	return isset($_ENV[$key]) ? $_ENV[$key] : $default;
+}
