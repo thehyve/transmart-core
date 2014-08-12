cabbe65ace0f8613f089a7847b1a61161c226bc3
Peter Rice <p.rice@imperial.ac.uk>
2014-08-12 19:12:01 +0100

    create 1.2.1-SNAPSHOT

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -27,9 +27,11 @@ grails.project.dependency.resolution = {
 
     plugins {
         compile(':resources:1.2.1')
-        compile(':transmart-java:1.2.1-SNAPSHOT')
-        compile(':biomart-domain:1.2.1-SNAPSHOT')
         compile(':search-domain:1.2.1-SNAPSHOT')
+        // already included in search-domain
+        //compile(':biomart-domain:1.2.1-SNAPSHOT')
+        // already included in biomart-domain
+        //compile(':transmart-java:1.2.1-SNAPSHOT')
         compile(':transmart-legacy-db:1.2.1-SNAPSHOT')
         compile(':spring-security-core:2.0-RC2')
         //compile ':spring-security-ldap:2.0-RC2')
