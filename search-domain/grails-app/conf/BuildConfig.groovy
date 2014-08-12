4ea1e0933e1fa24126d2c0cacd7ab74e34cb79ab
Peter Rice <p.rice@imperial.ac.uk>
2014-08-12 19:04:45 +0100

    transmart-java is part of biomart-domain

--- search-domain/grails-app/conf/BuildConfig.groovy
+++ search-domain/grails-app/conf/BuildConfig.groovy
@@ -26,6 +26,7 @@ grails.project.dependency.resolution = {
 			":rest-client-builder:1.0.3") {
 		export = false}
         compile ':biomart-domain:1.2.1-SNAPSHOT'
-        compile ':transmart-java:1.2.1-SNAPSHOT'
+        //// already included in biomart-domain
+        //compile ':transmart-java:1.2.1-SNAPSHOT'
     }
 }
