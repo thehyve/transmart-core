43cd34006161ac0c398d4016fee7e0078c984e82
Peter Rice <p.rice@imperial.ac.uk>
2014-08-12 19:13:33 +0100

    remove duplicate dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -32,11 +32,15 @@ grails.project.dependency.resolution = {
     }
     plugins {
 		compile(':resources:1.2.1')
-		compile(':transmart-java:1.2.1-SNAPSHOT')
-		compile(':biomart-domain:1.2.1-SNAPSHOT')
-		compile(':search-domain:1.2.1-SNAPSHOT')
+		//// already included in biomart-domain
+                //compile(':transmart-java:1.2.1-SNAPSHOT')
+		//// already included in search-domain
+                //compile(':biomart-domain:1.2.1-SNAPSHOT')
+		//// already included in folder-management
+                //compile(':search-domain:1.2.1-SNAPSHOT')
 		compile(':folder-management:1.2.1-SNAPSHOT')
-		compile(':transmart-legacy-db:1.2.1-SNAPSHOT')
+		//// already included in folder-management
+                //compile(':transmart-legacy-db:1.2.1-SNAPSHOT')
 		compile(':spring-security-core:2.0-RC2')
 		compile(':quartz:1.0-RC2')
         compile(':mail:1.0')
