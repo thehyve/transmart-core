6e4d11438828c6cabd71e0c31a85417144e2a862
Valeria Hohlova <khoulwa.khoulwa@gmail.com>
2014-09-03 08:18:23 -0700

    Fix for Marker Selection.

--- grails-app/taglib/com/thomsonreuters/lsps/transmart/ThomsonReutersTagLib.groovy
+++ grails-app/taglib/com/thomsonreuters/lsps/transmart/ThomsonReutersTagLib.groovy
@@ -10,6 +10,12 @@ class ThomsonReutersTagLib {
 		def mode = metacoreEnrichmentService.metacoreSettingsMode()
 		def settings = metacoreEnrichmentService.getMetacoreParams()
 		
-		out << render(template:'/metacoreEnrichment/metacoreSettingsButton', model: [settingsMode: mode, settings: settings])
+		out << render(template:'/metacoreEnrichment/metacoreSettingsButton', model: [settingsMode: mode, settings: settings], plugin: "transmart-metacore-plugin")
 	}
+
+    def metacoreEnrichmentResult = {
+        attrs, body ->
+
+        out << render(template:'/metacoreEnrichment/enrichmentResult', model: [prefix: 'marker_'], plugin: "transmart-metacore-plugin")
+    }
 }
