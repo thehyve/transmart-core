920cb7063400c69c9aefe69a4479757e0527e9f1
Carlos Silva <carlos@thehyve.nl>
2014-09-04 16:01:41 +0200

    convertToStandardFormat and readControlFiles modelled as Flows. column and word mappings are now read in parallel

--- src/main/groovy/org/transmartproject/batch/JobConfiguration.groovy
+++ src/main/groovy/org/transmartproject/batch/JobConfiguration.groovy
@@ -2,13 +2,21 @@ package org.transmartproject.batch
 
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
+import org.springframework.batch.core.StepContribution
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.job.builder.FlowBuilder
+import org.springframework.batch.core.job.flow.Flow
+import org.springframework.batch.core.job.flow.support.SimpleFlow
+import org.springframework.batch.core.scope.context.ChunkContext
+import org.springframework.batch.core.step.tasklet.Tasklet
+import org.springframework.batch.repeat.RepeatStatus
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
+import org.springframework.core.task.SimpleAsyncTaskExecutor
 import org.transmartproject.batch.model.ColumnMapping
 import org.transmartproject.batch.model.WordMapping
 import org.transmartproject.batch.tasklet.ReadFileTasklet
@@ -24,21 +32,26 @@ class JobConfiguration {
     @Autowired
     private StepBuilderFactory steps
 
-    /*
     @Bean
-    SimpleFlow mainFlow() {
-        SimpleFlow splitFlow =
-                new FlowBuilder<SimpleFlow>('Split flow')
-                        .split(new SimpleAsyncTaskExecutor())
-                        .add(new FlowBuilder<SimpleFlow>().start(readColumnMappingsStep()).build())
-                        //.add(new FlowBuilder<SimpleFlow>().start(readWordMappingsStep()).build())
-                        //.add(columnMappingFlow(), wordMappingFlow())
-                        .build()
-        //new FlowBuilder<>()
-
-        splitFlow
+    SimpleFlow readControlFilesFlow() {
+        new FlowBuilder<SimpleFlow>('readControlFilesFlow')
+                .start(dummyStep()) //we always need an initial step, hence this dummy
+                .split(new SimpleAsyncTaskExecutor()) //forks execution
+                .add(wrap(readColumnMappingsStep()), wrap(readWordMappingsStep()))
+                .end()
     }
-    */
+
+    private Flow wrap(Step step) {
+        new FlowBuilder<SimpleFlow>().start(step).build()
+    }
+
+    @Bean
+    SimpleFlow convertToStandardFormatFlow() {
+        new FlowBuilder<SimpleFlow>('convertToStandardFormatFlow')
+                .start(readControlFilesFlow())
+                .build()
+    }
+
 
     @Bean
     Step readColumnMappingsStep() {
@@ -57,11 +70,20 @@ class JobConfiguration {
     @Bean
     Job job() {
         jobs.get('job')
-                //.start(mainFlow()) //@todo make this parallel
-                .start(readColumnMappingsStep())
-                .next(readWordMappingsStep())
+                .start(convertToStandardFormatFlow())
+                .end()
                 .build()
     }
 
+    @Bean
+    Step dummyStep() {
+        Tasklet tasklet = new Tasklet() {
+            @Override
+            RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                return RepeatStatus.FINISHED
+            }
+        }
+        steps.get('dummyStep').tasklet(tasklet).build()
+    }
 
 }
