6a7e630fbdc7b8c3c46a160dc227c145e4d2d7f6
Terry-Weymouth <weymouth@umich.edu>
2014-09-15 14:02:46 -0400

    Update version and dependencies to 1.2.2-SNAPSHOT

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -27,12 +27,12 @@ grails.project.dependency.resolution = {
 
     plugins {
         compile(':resources:1.2.1')
-        compile(':search-domain:1.2.1-SNAPSHOT')
+        compile(':search-domain:1.2.2-SNAPSHOT')
         // already included in search-domain
-        //compile(':biomart-domain:1.2.1-SNAPSHOT')
+        //compile(':biomart-domain:1.2.2-SNAPSHOT')
         // already included in biomart-domain
-        //compile(':transmart-java:1.2.1-SNAPSHOT')
-        compile(':transmart-legacy-db:1.2.1-SNAPSHOT')
+        //compile(':transmart-java:1.2.2-SNAPSHOT')
+        compile(':transmart-legacy-db:1.2.2-SNAPSHOT')
         compile(':spring-security-core:2.0-RC2')
         //compile ':spring-security-ldap:2.0-RC2')
         compile(':quartz:1.0-RC2')
