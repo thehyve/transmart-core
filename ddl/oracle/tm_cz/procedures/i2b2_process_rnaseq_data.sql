94af4812a8fbfcf83acc0bce3a4445e020b93fa0
Cees de Jonge <c.de.jonge@philips.com>
2014-10-21 12:54:42 +0200

    Removed some old code-lines and repaired stupid typo.

--- ddl/oracle/tm_cz/procedures/i2b2_process_rnaseq_data.sql
+++ ddl/oracle/tm_cz/procedures/i2b2_process_rnaseq_data.sql
@@ -515,17 +515,6 @@ BEGIN
 	i2b2_fill_in_tree(TrialId, r_addNodes.leaf_node, jobID);
   END LOOP;
 
-  --	set sourcesystem_cd, c_comment to null if any added upper-level nodes
-
-  update i2b2 b
-	set sourcesystem_cd=null,c_comment=null
-	where b.sourcesystem_cd = TrialId
-	  and length(b.c_fullname) < length(topNode);
-
-  stepCt := stepCt + 1;
-  cz_write_audit(jobId,databaseName,procedureName,'Set sourcesystem_cd to null for added upper level nodes',SQL%ROWCOUNT,stepCt,'Done');
-  commit;
-
   --	update concept_cd for nodes, this is done to make the next insert easier
 
   update wt_mrna_nodes t
@@ -900,7 +889,7 @@ BEGIN
   cz_write_audit(jobId,databaseName,procedureName,'Insert into DEAPP wt_subject_rnasq_region',SQL%ROWCOUNT,stepCt,'Done');
   commit;
 
-  p_exists := SQL%ROWCOUNT;
+  pExists := SQL%ROWCOUNT;
 
   if pExists = 0 then
 	raise no_probeset_recs;
