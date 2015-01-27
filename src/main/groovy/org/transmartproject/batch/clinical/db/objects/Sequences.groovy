e6ba260b6b50fedcd516083f470af85901acf52e
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2015-01-27 18:16:47 +0100

    Create secure object when loading private study
    Management of study level access in the admin panel is actually the
    management of what tranSMART calls "secure objects". These are stored in
    the search_secure_object table. The permissions for these objects are
    stored in search_auth_sec_object_access. At runtime, these two tables
    are joined to check user access to a study (together with others, for
    instance in order to handle user group membership). For that, the secure
    object with type 'BIO_CLINICAL_TRIAL' and a token equal to the same that
    is stored in i2b2_secure (typically `EXP:<trial name>`) is involved.
    
    This commit adds the necessary row in search_secure_object. A row is
    also inserted in bio_experiment for no other reason than the original
    code does that. The bio_data_id of the secure object is then filled with
    theh experiment id.
    
    Original code:
    https://github.com/transmart/transmart-data/blob/cbcdeb62a9aefe709fa11f75367fcdaeee9d07b6/ddl/postgres/tm_cz/functions/i2b2_secure_study.sql
    
    Related: JE-49

--- src/main/groovy/org/transmartproject/batch/clinical/db/objects/Sequences.groovy
+++ src/main/groovy/org/transmartproject/batch/clinical/db/objects/Sequences.groovy
@@ -6,13 +6,16 @@ package org.transmartproject.batch.clinical.db.objects
 final class Sequences {
     private Sequences() {}
 
-    static final String PATIENT = 'i2b2demodata.SEQ_PATIENT_NUM'
-    static final String CONCEPT = 'i2b2metadata.I2B2_ID_SEQ'
-    static final String I2B2_RECORDID = 'i2b2metadata.i2b2_record_id_seq'
+    public static final String PATIENT = 'i2b2demodata.seq_patient_num'
+    public static final String CONCEPT = 'i2b2metadata.i2b2_id_seq'
+    public static final String I2B2_RECORDID = 'i2b2metadata.i2b2_record_id_seq'
 
-    static final String PROBESET_ID = 'tm_cz.SEQ_PROBESET_ID'
+    public static final String BIO_DATA_ID = 'biomart.seq_bio_data_id'
 
-    static final String ASSAY_ID = 'deapp.seq_assay_id'
+    public static final String SEARCH_SEQ_DATA_ID = 'searchapp.seq_search_data_id'
 
-    static final String MRNA_PARTITION_ID = 'deapp.seq_mrna_partition_id'
+    public static final String PROBESET_ID = 'tm_cz.seq_probeset_id'
+
+    public static final String ASSAY_ID = 'deapp.seq_assay_id'
+    public static final String MRNA_PARTITION_ID = 'deapp.seq_mrna_partition_id'
 }
