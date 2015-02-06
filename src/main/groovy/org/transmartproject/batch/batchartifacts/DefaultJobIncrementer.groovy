8a0f62054f45a562a5240ee539015aee53b254c8
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2015-02-06 18:14:55 +0100

    Better job incrementer (see javadoc)

--- src/main/groovy/org/transmartproject/batch/batchartifacts/DefaultJobIncrementer.groovy
+++ src/main/groovy/org/transmartproject/batch/batchartifacts/DefaultJobIncrementer.groovy
@@ -3,24 +3,24 @@ package org.transmartproject.batch.batchartifacts
 import org.springframework.batch.core.JobParameters
 import org.springframework.batch.core.JobParametersBuilder
 import org.springframework.batch.core.JobParametersIncrementer
+import org.springframework.batch.core.launch.support.RunIdIncrementer
 
 /**
- * JobParametersIncrementer impl that makes sure we increment the run.id (resuming from last value)</br>
- * This allows us to re-run a job for the same set of parameters.
+ * JobParametersIncrementer that adds a parameter with the date.
+ * We do not use {@link RunIdIncrementer} because it depends on the jobs being
+ * consistently executed with -n. The parameters passed to getNext() are the
+ * parameter of the last created job instance. But, for instance, if the first
+ * job instance was run with -n (and therefore got run.id=1), then one is run
+ * *without* -n (and get no run.id) parameter, at this point the latest job
+ * instance will be the one without run.id. A job then started with -n, will get
+ * run.id=1 and the first run will be resumed.
  */
 class DefaultJobIncrementer implements JobParametersIncrementer {
 
     @Override
     JobParameters getNext(JobParameters parameters) {
-        if (parameters == null || parameters.isEmpty()) {
-            new JobParametersBuilder().
-                    addLong("run.id", 1L).
-                    toJobParameters()
-        } else {
-            long id = parameters.getLong("run.id", 1L) + 1
-            new JobParametersBuilder().
-                    addLong("run.id", id).
-                    toJobParameters()
-        }
+        new JobParametersBuilder().
+                addDate("run.date", new Date()).
+                toJobParameters()
     }
 }
