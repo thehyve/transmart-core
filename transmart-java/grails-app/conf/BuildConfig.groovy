b66992f678efe74ef3e176fa17fb5a73e232569a
Peter Rice <p.rice@ic.ac.uk>
2015-02-26 09:36:35 +0000

    maven dependency resolver

--- transmart-java/grails-app/conf/BuildConfig.groovy
+++ transmart-java/grails-app/conf/BuildConfig.groovy
@@ -2,6 +2,7 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
+grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -29,7 +30,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(":release:2.2.1",
+        build(":release:3.0.1",
                 ":rest-client-builder:1.0.3") {
             export = false
         }
