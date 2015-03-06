7befe97f997e8733524d398403184bb131db5cb2
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2015-03-06 12:26:27 +0100

    Fix sequence used for concept_cd
    See also:
    https://github.com/transmart/transmart-data/pull/66#issuecomment-77543941

--- src/main/groovy/org/transmartproject/batch/clinical/db/objects/Sequences.groovy
+++ src/main/groovy/org/transmartproject/batch/clinical/db/objects/Sequences.groovy
@@ -7,7 +7,7 @@ final class Sequences {
     private Sequences() {}
 
     public static final String PATIENT = 'i2b2demodata.seq_patient_num'
-    public static final String CONCEPT = 'i2b2metadata.i2b2_id_seq'
+    public static final String CONCEPT = 'i2b2demodata.concept_id'
     public static final String I2B2_RECORDID = 'i2b2metadata.i2b2_record_id_seq'
 
     public static final String BIO_DATA_ID = 'biomart.seq_bio_data_id'
