7dd7e72fbceeabc552cab313cf9ad4d0fec63dfb
Peter Rice <p.rice@imperial.ac.uk>
2015-07-01 17:08:28 +0100

    increase column width for job query details to match viewer_url

--- ddl/postgres/i2b2demodata/async_job.sql
+++ ddl/postgres/i2b2demodata/async_job.sql
@@ -9,7 +9,7 @@ CREATE TABLE async_job (
     job_status_time timestamp(6) without time zone,
     last_run_on timestamp(6) without time zone,
     viewer_url character varying(4000),
-    alt_viewer_url character varying(600),
+    alt_viewer_url character varying(4000),
     job_results text,
     job_inputs_json text,
     job_type character varying(20)
