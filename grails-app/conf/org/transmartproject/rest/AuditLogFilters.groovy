eab030e0162fe588cd819835912bb355e8696fc1
Ruslan Forostianov <ruslan@thehyve.nl>
2015-08-05 16:47:59 +0200

    Use core-api's access log

--- grails-app/conf/org/transmartproject/rest/AuditLogFilters.groovy
+++ grails-app/conf/org/transmartproject/rest/AuditLogFilters.groovy
@@ -1,20 +1,32 @@
 package org.transmartproject.rest
 
+import org.transmartproject.core.users.User
+
 class AuditLogFilters {
+
+    def accessLogService
+    User currentUserBean
 
     def filters = {
         lowDim(controller: 'observation', action:'*') {
             after = { model ->
-                log.info("${request.remoteUser} (IP: ${request.remoteAddr}) gets low dim. data" +
-                        " with ${request.forwardURI}${request.queryString ? '?' + request.queryString : ''}")
+                def fullUrl = "${request.forwardURI}${request.queryString ? '?' + request.queryString : ''}"
+
+                accessLogService.report(currentUserBean, 'REST API Data Retrieval',
+                        eventMessage:  "User got low dim. data with ${fullUrl}",
+                        requestURL: fullUrl)
             }
         }
 
         highDim(controller: 'highDim', action:'*') {
             after = { model ->
-                log.info("${request.remoteUser} (IP: ${request.remoteAddr}) gets high dim. data" +
-                        " with ${request.forwardURI}${request.queryString ? '?' + request.queryString : ''}")
+                def fullUrl = "${request.forwardURI}${request.queryString ? '?' + request.queryString : ''}"
+
+                accessLogService.report(currentUserBean, 'REST API Data Retrieval',
+                        eventMessage:  "User got high dim. data with ${fullUrl}",
+                        requestURL: fullUrl)
             }
         }
     }
+
 }
