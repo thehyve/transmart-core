f881a231fb2e210234f3d0dea299b506f9d6ffab
dverbeeck <dennyverbeeck@gmail.com>
2015-09-09 15:39:48 +0200

    Refactoring

--- src/main/groovy/org/transmartproject/core/querytool/ConstraintByOmicsValue.groovy
+++ src/main/groovy/org/transmartproject/core/querytool/ConstraintByOmicsValue.groovy
@@ -30,7 +30,7 @@ class ConstraintByOmicsValue {
      */
     ProjectionType projectionType
 
-    enum Operator {
+    static enum Operator {
 
         LOWER_THAN          ('LT'),
         LOWER_OR_EQUAL_TO   ('LE'),
@@ -51,7 +51,7 @@ class ConstraintByOmicsValue {
         }
     }
 
-    public static final enum OmicsType {
+    static enum OmicsType {
         GENE_EXPRESSION ('Gene Expression'),
         RNASEQ_RCNT ('RNASEQ_RCNT'),
         PROTEOMICS ('PROTEOMICS'),
@@ -67,75 +67,90 @@ class ConstraintByOmicsValue {
 
         static OmicsType forValue(String value) {
             values().find { value == it.value } ?:
-                    { throw new IllegalArgumentException("No operator for value $value") }()
+                    { throw new IllegalArgumentException("No OmicsType for value $value") }
         }
     }
 
     // ProjectionType names should correspond to an existing column in the table holding the omics data
-    public static final enum ProjectionType {
+    static enum ProjectionType {
         // Gene expression types
-        RAW_INTENSITY,
-        LOG_INTENSITY,
-        ZSCORE,
+        RAW_INTENSITY ("Raw Intensity"),
+        LOG_INTENSITY ("Log Intensity"),
+        ZSCORE ("Z-Score"),
 
         // RNASEQ types (Zscore already specified)
-        NORMALIZED_READCOUNT,
-        LOG_NORMALIZED_READCOUNT,
+        NORMALIZED_READCOUNT ("Normalized Readcount"),
+        LOG_NORMALIZED_READCOUNT ("Log Normalized Readcount"),
 
         // Proteomics has 'intensity'
-        INTENSITY
+        INTENSITY ("Raw Intensity"),
+
+        // ACGH and VCF will have a complex selector defining the entire filter
+        COMPLEX ("Complex")
+
+        final String value
+
+        protected ProjectionType(String value) {
+            this.value = value
+        }
+
+        static ProjectionType forValue(String value) {
+            values().find { value == it.value } ?:
+                    { throw new IllegalArgumentException("No ProjectionType for value $value")}
+        }
     }
 
-    public static final ALLOWED_PROJECTIONS = [
-            (OmicsType.GENE_EXPRESSION) : [ProjectionType.RAW_INTENSITY, ProjectionType.LOG_INTENSITY, ProjectionType.ZSCORE],
-            (OmicsType.RNASEQ_RCNT)     : [ProjectionType.NORMALIZED_READCOUNT, ProjectionType.LOG_NORMALIZED_READCOUNT, ProjectionType.ZSCORE],
-            (OmicsType.PROTEOMICS)      : [ProjectionType.INTENSITY, ProjectionType.LOG_INTENSITY, ProjectionType.ZSCORE],
-            (OmicsType.MIRNA_QPCR)      : [ProjectionType.RAW_INTENSITY, ProjectionType.LOG_INTENSITY, ProjectionType.ZSCORE]
-    ]
-
-    public static def getMarkerInfo(String markerType) {
-        if (markerType.equals('Gene Expression')) {
-            return [data_table: "deapp.de_subject_microarray_data",
-                    annotation_table: "deapp.de_mrna_annotation",
-                    id_column: "probeset_id",
-                    annotation_id_column: "probeset_id",
-                    selector_column: "gene_symbol",
-                    allowed_projections: ["raw_intensity", "log_intensity", "zscore"]]
-        }
-        else if (markerType.equals("RNASEQ_RCNT")) {
-            return [data_table: "deapp.de_subject_rnaseq_data",
-                    annotation_table: "deapp.de_chromosomal_region",
-                    id_column: "region_id",
-                    annotation_id_column: "region_id",
-                    selector_column: "gene_symbol",
-                    allowed_projections: ["normalized_readcount", "log_normalized_readcount", "zscore"]]
-        }
-        else if (markerType.equals("PROTEOMICS")) {
-            return [data_table: "deapp.de_subject_protein_data",
-                    annotation_table: "deapp.de_protein_annotation",
-                    id_column: "protein_annotation_id",
-                    annotation_id_column: "id",
-                    selector_column: "uniprot_name",
-                    allowed_projections: ["intensity", "log_intensity", "zscore"]]
-        }
-        else if (markerType.equals("Chromosomal")) {
-            return [data_table: "deapp.de_subject_protein_data",
-                    annotation_table: "deapp.de_chromosomal_region",
-                    id_column: "region_id",
-                    annotation_id_column: "region_id",
-                    selector_column: "gene_symbol",
-                    allowed_projections: ["chip","segmented","flag","probloss","probnorm","probgain","probamp"]]
-        }
-        else if (markerType.equals("MIRNA_QPCR")) {
-            return [data_table: "deapp.de_subject_mirna_data",
-                    annotation_table: "deapp.de_qpcr_mirna_annotation",
-                    id_column: "probeset_id",
-                    annotation_id_column: "probeset_id",
-                    selector_column: "mirna_id",
-                    allowed_projections: ["raw_intensity", "log_intensity", "zscore"]]
-        }
-
-        return null
+    // Different classes of omics filters
+    static enum FilterType {
+        SINGLE_NUMERIC,
+        ACGH,
+        VCF
     }
 
+    // centralized point to get metadata on the marker type
+    // allows to parameterize the dialog and queries for the simpler omics data types
+    static def markerInfo = [
+         "Gene Expression": [data_table: "deapp.de_subject_microarray_data",
+                            annotation_table: "deapp.de_mrna_annotation",
+                            id_column: "probeset_id",
+                            annotation_id_column: "probeset_id",
+                            selector_column: "gene_symbol",
+                            allowed_projections: [ProjectionType.RAW_INTENSITY, ProjectionType.LOG_INTENSITY, ProjectionType.ZSCORE],
+                            filter_type: FilterType.SINGLE_NUMERIC],
+        "RNASEQ_RCNT": [data_table: "deapp.de_subject_rnaseq_data",
+                        annotation_table: "deapp.de_chromosomal_region",
+                        id_column: "region_id",
+                        annotation_id_column: "region_id",
+                        selector_column: "gene_symbol",
+                        allowed_projections: [ProjectionType.NORMALIZED_READCOUNT, ProjectionType.LOG_NORMALIZED_READCOUNT, ProjectionType.ZSCORE],
+                        filter_type: FilterType.SINGLE_NUMERIC],
+        "PROTEOMICS":  [data_table: "deapp.de_subject_protein_data",
+                        annotation_table: "deapp.de_protein_annotation",
+                        id_column: "protein_annotation_id",
+                        annotation_id_column: "id",
+                        selector_column: "uniprot_name",
+                        allowed_projections: [ProjectionType.INTENSITY, ProjectionType.LOG_INTENSITY, ProjectionType.ZSCORE],
+                        filter_type: FilterType.SINGLE_NUMERIC],
+        "Chromosomal": [data_table: "",
+                        annotation_table: "",
+                        id_column: "",
+                        annotation_id_column: "",
+                        selector_column: "",
+                        allowed_projections: [ProjectionType.COMPLEX],
+                        filter_type: FilterType.ACGH],
+        "MIRNA_QPCR":  [data_table: "deapp.de_subject_mirna_data",
+                        annotation_table: "deapp.de_qpcr_mirna_annotation",
+                        id_column: "probeset_id",
+                        annotation_id_column: "probeset_id",
+                        selector_column: "mirna_id",
+                        allowed_projections: [ProjectionType.RAW_INTENSITY, ProjectionType.LOG_INTENSITY, ProjectionType.ZSCORE],
+                        filter_type: FilterType.SINGLE_NUMERIC],
+        "VCF":         [data_table: "",
+                        annotation_table: "",
+                        id_column: "",
+                        annotation_id_column: "",
+                        selector_column: "",
+                        allowed_projections: [ProjectionType.COMPLEX],
+                        filter_type: FilterType.VCF]
+            ]
 }
