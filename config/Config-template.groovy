907c0e5df695c6991f814083a7637c6d890dae2d
Peter Rice <p.rice@imperial.ac.uk>
2015-09-11 16:47:16 +0100

    document GWAS and Rmodules image options

--- config/Config-template.groovy
+++ config/Config-template.groovy
@@ -242,15 +242,43 @@ environments {
         // The working directory for R scripts, where the jobs get created and
         // output files get generated
         RModules.tempFolderDirectory = jobsDirectory
+
+        // Whether to copy the images from the jobs directory to another
+        // directory from which they can be served. Should be false for
+        // performance reasons. Files will be served from the
+        // tempFolderDirectory instead, which should be exposed as
+        // <context path>/analysisFiles (formerly: <context path>/tempImages)
+        RModules.transferImageFile = false
+
+        // Copy inside the exploded WAR. In actual production, we don't want this
+        // The web server should be able to serve static files from this
+        // directory via the logical name specified in the imageUrl config entry
+        // Not needed because transferImageFile is false
+        //Rmodules.temporaryImageFolder = explodedWarDir + '/images/tempImages/'
     }
     development {
         RModules.tempFolderDirectory = "/tmp"
+
+        // we have stuff in _Events.groovy to make available the contents in
+        // the tempFolderDirectory
+        RModules.transferImageFile = false
+
         /* we don't need to specify temporaryImageDirectory, because we're not copying */
     }
 
     // Used to access R jobs parent directory outside RModules (e.g. data export)
     com.recomdata.plugins.tempFolderDirectory = RModules.tempFolderDirectory
 }
+/* }}} */
+
+/* {{{ GWAS Configuration */
+
+com.recomdata.dataUpload.appTitle="Upload data to tranSMART"
+//com.recomdata.dataUpload.templates.dir="/home/transmart/templates/"
+//com.recomdata.dataUpload.uploads.dir="/home/transmart/uploads/"
+//com.recomdata.dataUpload.etl.dir="/home/transmart/ETL/"
+com.recomdata.dataUpload.stageScript="run_analysis_stage"
+
 /* }}} */
 
 /* {{{ Misc Configuration */
