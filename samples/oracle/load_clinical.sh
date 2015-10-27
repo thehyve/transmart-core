7b48f187c89f737fa8e7fa88d6c6cb03db303b54
Peter Rice <p.rice@imperial.ac.uk>
2015-10-27 14:40:18 +0000

    load_clinical.sh oracle to match postgres

--- samples/oracle/load_clinical.sh
+++ samples/oracle/load_clinical.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-set -x
+#set -x
 set -e
+
+# General optional parameters:
+#   DATA_LOCATION, STUDY_NAME, STUDY_ID
+# Specific mandatory parameters for this upload script:
+#   COLUMN_MAP_FILE, WORD_MAP_FILE, either R_JOBS_PSQL or KETTLE_JOBS_PSQL
+# Specific optional parameters for this upload script:
+#   TOP_NODE_PREFIX, SECURITY_REQUIRED, USE_R_UPLOAD
+
+# locate this shell script, and source a generic shell script to process all params related settings
+UPLOAD_SCRIPTS_DIRECTORY=$(dirname "$0")
+UPLOAD_DATA_TYPE="clinical"
+source "$UPLOAD_SCRIPTS_DIRECTORY/process_params.inc"
+
+# Check if mandatory variables are set
+if [ -z "$STUDY_ID" ] || [ -z "$COLUMN_MAP_FILE" ]; then
+	echo "Following variables need to be set:"
+	echo "    STUDY_ID=$STUDY_ID"
+	echo "    COLUMN_MAP_FILE=$COLUMN_MAP_FILE"
+	exit -1
+fi
 
 if [ -z "$KETTLE_HOME" ]; then
 	echo "KETTLE_HOME is not set"
@@ -13,23 +33,27 @@ if [ -z "$KETTLE_JOBS" ]; then
 	exit 1
 fi
 
-# $KETTLE_HOME should have been set by the caller
-export KETTLE_HOME
+    mkdir -p logs
 
-# Should define COLUMN_MAP_FILE, WORD_MAP_FILE AND RECORD_EXCLUSION_FILE
-# Optionally, SECURITY_REQUIRED
-source $1
+    CLINICAL_JOB='create_clinical_data.kjb'
+    if [ ! -z "$INC_LOAD" ]; then
+	if [ "$INC_LOAD" = 'Y' ]; then
+	    CLINICAL_JOB='increment_clinical_data.kjb'
+	fi
+    fi
 
 SECURITY_REQUIRED=${SECURITY_REQUIRED:-N}
-
-if [ $SECURITY_REQUIRED = 'Y' ]; then
-    TOP_NODE_PREFIX='Private Studies'
-else
-    TOP_NODE_PREFIX='Public Studies'
+if [ -z "$TOP_NODE_PREFIX" ]; then
+    if [ $SECURITY_REQUIRED = 'Y' ]; then
+        TOP_NODE_PREFIX='Private Studies'
+    else
+        TOP_NODE_PREFIX='Public Studies'
+    fi
 fi
+TOP_NODE="\\${TOP_NODE_PREFIX}\\${STUDY_NAME}\\"
 
 $KITCHEN -norep=Y                                      \
--file=$KETTLE_JOBS/create_clinical_data.kjb            \
+-file=$KETTLE_JOBS/$CLINICAL_JOB            \
 -log=logs/load_clinical_data_$(date +"%Y%m%d%H%M").log \
 -param:COLUMN_MAP_FILE="$COLUMN_MAP_FILE"              \
 -param:DATA_LOCATION="$DATA_LOCATION"                  \
@@ -37,7 +61,7 @@ $KITCHEN -norep=Y                                      \
 -param:SECURITY_REQUIRED="$SECURITY_REQUIRED"          \
 -param:SORT_DIR=/tmp                                   \
 -param:STUDY_ID="$STUDY_ID"                            \
--param:TOP_NODE='\'"$TOP_NODE_PREFIX"'\'$STUDY_ID'\'   \
+-param:TOP_NODE="$TOP_NODE"                            \
 -param:WORD_MAP_FILE="$WORD_MAP_FILE"                  \
 -param:RECORD_EXCLUSION_FILE="$RECORD_EXCLUSION_FILE"
 #-param:SQLLDR_PATH=/spin/pg/master/bin/psql \
