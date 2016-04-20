40b083bc929414bac732f345a26a2dbb1e264285
Jan Kanis <jan.code@jankanis.nl>
2016-04-20 13:15:56 +0200

    Document GenericWrapper

--- src/groovy/org/transmartproject/rest/marshallers/GenericWrapper.groovy
+++ src/groovy/org/transmartproject/rest/marshallers/GenericWrapper.groovy
@@ -12,6 +12,15 @@ package org.transmartproject.rest.marshallers
 import grails.rest.Link
 import groovy.transform.CompileStatic
 
+/**
+ * This wrapper can be used for generic data such as lists or maps of strings or numbers, for which no dedicated
+ * wrapper/marshaller exists. The keyword arguments in the constructor (except 'links' and 'embeddedEntities') will be
+ * rendered as a JSON object with those attributes.
+ *
+ * @param links (named argument) should be a collection of {@link Link} objects
+ * @param embeddedEntities (named argument) should be a collection of strings that indicate which other keys are
+ * embedded entities.
+ */
 @CompileStatic
 public class GenericWrapper {
 
