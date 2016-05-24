9afff2395c694e7973387338c51f979a8fda8b10
Ruslan Forostianov <ruslan@thehyve.nl>
2016-05-24 14:42:08 +0200

    Add primary keys to the miRNA tables.

--- ddl/postgres/deapp/de_subject_mirna_data.sql
+++ ddl/postgres/deapp/de_subject_mirna_data.sql
@@ -4,12 +4,18 @@
 CREATE TABLE de_subject_mirna_data (
     trial_source character varying(200),
     trial_name character varying(50),
-    assay_id numeric(18,0),
+    assay_id numeric(18,0) NOT NULL,
     patient_id numeric(18,0),
     raw_intensity numeric,
     log_intensity numeric,
-    probeset_id numeric(38,0),
+    probeset_id numeric(38,0) NOT NULL,
     zscore numeric(18,9),
     partition_id numeric
 );
+
+--
+-- Name: de_subject_mirna_data_pkey; Type: CONSTRAINT; Schema: deapp; Owner: -
+--
+ALTER TABLE ONLY de_subject_mirna_data
+    ADD CONSTRAINT de_subject_mirna_data_pkey PRIMARY KEY (assay_id, probeset_id);
 
