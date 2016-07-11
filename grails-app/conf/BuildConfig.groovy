74cf12724fb040ca2c671052c974392f970e7748
Terry Weymouth <weymouth@umich.edu>
2016-07-11 13:59:51 +0100

    changed build tag to 16.2-SNAPSHOT

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -39,7 +39,7 @@ grails.project.dependency.resolution = {
     }
 
     dependencies {
-        compile('org.transmartproject:transmart-core-api:16.1')
+        compile('org.transmartproject:transmart-core-api:16.2-SNAPSHOT')
         compile group: 'com.google.guava', name: 'guava', version: '14.0.1'
 
         runtime('org.postgresql:postgresql:9.3-1100-jdbc41') {
