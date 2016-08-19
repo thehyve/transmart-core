e4a71fb6cd3b080ebb3fa5e779da2d882e69688e
Piotr Zakrzewski <piotr@thehyve.nl>
2016-08-19 16:58:49 +0200

    Make core-db build pass

--- src/main/groovy/org/transmartproject/db/dataquery/clinical/patientconstraints/PatientConstraint.groovy
+++
@@ -1,5 +0,0 @@
-package org.transmartproject.db.dataquery.clinical.patientconstraints
-
-import org.transmartproject.db.dataquery.CriteriaConstraint
-
-interface PatientConstraint extends CriteriaConstraint {}
