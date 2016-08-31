878abdcbe13332db4ebfe4d87af540eeb3d2f991
Piotr Zakrzewski <gieksosz@gmail.com>
2016-08-31 19:55:51 +0200

    Merge pull request #52 from transmart/cohort-handling
    Update RestAPI call for listing cohorts to not include patients

--- grails-app/services/org/transmartproject/db/querytool/QueriesResourceService.groovy
+++ grails-app/services/org/transmartproject/db/querytool/QueriesResourceService.groovy
@@ -31,6 +31,8 @@ import org.transmartproject.core.exceptions.NoSuchResourceException
 import org.transmartproject.core.querytool.QueriesResource
 import org.transmartproject.core.querytool.QueryDefinition
 import org.transmartproject.core.querytool.QueryResult
+import org.transmartproject.core.querytool.QueryResultSummary
+import org.transmartproject.core.querytool.QueryResultSummaryImplementation
 import org.transmartproject.core.querytool.QueryStatus
 import org.transmartproject.db.user.User
 
@@ -208,13 +210,13 @@ class QueriesResourceService implements QueriesResource {
     }
 
     @Override
-    List<QueryResult> getQueryResultsByUsername(String username) {
+    List<QueryResultSummary> getQueryResultsSummaryByUsername(String username) {
         def query = QtQueryResultInstance.where {
             queryInstance.userId == username
             deleteFlag == 'N'
         }
-        query.findAll()
-    }
+        query.collect { new QueryResultSummaryImplementation(it)}
+}
 
     @Override
     List<QueryResult> getQueryResultsByUsername(String username) {
