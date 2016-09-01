a7979a2ee5d4e1dc6014ad52cd14f74d3de60c3a
Ruslan Forostianov <ruslan@thehyve.nl>
2016-09-01 16:10:05 +0200

    New way to fill not-nullable i2b2 fields.

--- transmart-core-db-tests/src/main/groovy/org/transmartproject/db/TestDataHelper.groovy
+++ transmart-core-db-tests/src/main/groovy/org/transmartproject/db/TestDataHelper.groovy
@@ -28,39 +28,6 @@ import static org.hamcrest.Matchers.isA
  */
 class TestDataHelper {
 
-    /**
-     * Fills the object with dummy values for all the fields that are mandatory (nullable = false) and have no value
-     * @param clazz
-     * @param obj
-     */
-    static <T> void completeObject(Class<T> clazz, T obj) {
-        List<MetaProperty> fields = getMandatoryProps(clazz).findAll { it.getProperty(obj) == null } //all without value
-        for (MetaProperty f: fields) {
-            f.setProperty(obj, getDummyObject(f.type)) //set a dummy value
-        }
-    }
-
-    static Object getDummyObject(Class type) {
-
-        switch (type) {
-            case String:
-                return ''
-            case Character:
-                return ''
-            case Integer:
-                return 0
-            case Date:
-                return new Date()
-            default:
-                throw new UnsupportedOperationException("Not supported: $type.name. Care to add it?")
-        }
-    }
-
-    private static List<MetaProperty> getMandatoryProps(Class clazz) {
-        def mandatory = clazz.constraints?.findAll { it.value.nullable == false } //get all not nullable properties
-        clazz.metaClass.properties.findAll { mandatory.containsKey(it.name) }
-    }
-
     static List<String> getMissingValueFields(Object obj, Collection<String> fields) {
         def props = obj.class.metaClass.properties.findAll { fields.contains(it.name) }
         props.findAll({ !it.getProperty(obj) }).collect({ it.name })
