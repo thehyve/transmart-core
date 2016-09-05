b983024606dd902b23a9f82e72a549cc9cc9e601
Piotr Zakrzewski <piotr@thehyve.nl>
2016-09-05 16:12:18 +0200

    Add resources folder

---
+++ transmartApp/src/main/resources/js/fixconsole.js
@@ -0,0 +1,21 @@
+/**
+ * Protect window.console method calls, e.g. console is not defined on IE
+ * unless dev tools are open, and IE doesn't define console.debug
+ */
+(function() {
+    if (!window.console) {
+        window.console = {};
+    }
+    // union of Chrome, FF, IE, and Safari console methods
+    var m = [
+        "log", "info", "warn", "error", "debug", "trace", "dir", "group",
+        "groupCollapsed", "groupEnd", "time", "timeEnd", "profile", "profileEnd",
+        "dirxml", "assert", "count", "markTimeline", "timeStamp", "clear"
+    ];
+    // define undefined methods as noops to prevent errors
+    for (var i = 0; i < m.length; i++) {
+        if (!window.console[m[i]]) {
+            window.console[m[i]] = function() {};
+        }
+    }
+})();
