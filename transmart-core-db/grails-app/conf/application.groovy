b7297864b20f03d64728776f2bbf1f1471fb6758
ewelinagr <ewelina@thehyve.nl>
2016-09-06 08:54:16 +0200

    update core-db config

--- transmart-core-db/grails-app/conf/application.groovy
+++ transmart-core-db/grails-app/conf/application.groovy
@@ -28,7 +28,7 @@ grails.views.gsp.encoding="UTF-8"
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+    cache.region.factory_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory'
 }
 
 // environment specific settings
