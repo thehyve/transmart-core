408d5fdb8d707d61291781b1d09191c5e6e95c9c
ewelinagr <ewelina@thehyve.nl>
2016-09-14 11:31:20 +0200

    Fix for biomart-domain plugin

--- biomart-domain/grails-app/conf/application.groovy
+++ biomart-domain/grails-app/conf/application.groovy
@@ -1,3 +1,35 @@
+dataSource {
+    pooled = true
+    driverClassName = "org.hsqldb.jdbcDriver"
+    username = "sa"
+    password = ""
+}
+hibernate {
+    cache.use_second_level_cache = true
+    cache.use_query_cache = true
+    cache.provider_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory'
+}
+// environment specific settings
+environments {
+    development {
+        dataSource {
+            dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+            url = "jdbc:hsqldb:mem:devDB"
+        }
+    }
+    test {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:hsqldb:mem:testDb"
+        }
+    }
+    production {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+        }
+    }
+}
 // configuration for plugin testing - will not be included in the plugin zip
  
 log4j = {
