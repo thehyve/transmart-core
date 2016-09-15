a15dbe469db42d0b1e9de9f1b5d0ffe93f4fedc8
Olaf Meuwese <olafmeuwese@gmail.com>
2016-09-15 16:58:38 +0200

    First start of the sampleExplorer

---
+++ transmartApp/grails-app/assets/javascripts/sampleTab.js
@@ -0,0 +1,12 @@
+
+// require jquery.js
+//= require jquery-ui.js
+//= require jquery-plugins.js
+//= require extjs.js
+//= require session_timeout.js
+
+//= require myJobs.js
+//= require sampleExplorer/sampleExplorerMenu.js
+//= require sampleExplorer/sampleExplorer.js
+//= require sampleExplorer/sampleExplorer.utilities.js
+//= require utilitiesMenu.js
