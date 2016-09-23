d57a6b57e3c8902f2880a46da21aad74f654d35f
ewelinagr <ewelina@thehyve.nl>
2016-09-23 09:10:32 +0200

    Resolve conflicts to marge with master

--- transmartApp/grails-app/controllers/org/transmartproject/interceptors/ChartInterceptor.groovy
+++ transmartApp/grails-app/controllers/org/transmartproject/interceptors/ChartInterceptor.groovy
@@ -1,9 +1,40 @@
 package org.transmartproject.interceptors
 
+import org.transmartproject.core.users.User
 
 class ChartInterceptor {
 
-    boolean before() { true }
+    def auditLogService
+    def studyIdService
+    User currentUserBean
+
+    ChartInterceptor(){
+        matchAll().excludes(controller:'chart', action:~/(clearGrid|displayChart|childConceptPatientCounts)/)
+    }
+
+    boolean before() {
+        if (!auditLogService.enabled) return true
+        def result_instance_id1 = params.result_instance_id1 ?: ''
+        def result_instance_id2 = params.result_instance_id2 ?: ''
+        def studies = ''
+        if (params.concept_key) {
+            studies = studyIdService.getStudyIdForConceptKey(params.concept_key) ?: ''
+        }
+        if (studies.empty) {
+            studies = studyIdService.getStudyIdsForQueries([result_instance_id1, result_instance_id2])
+        }
+        def task = "Summary Statistics"
+        if (actionName == "childConceptPatientCounts") {
+            task = "Clinical Data Access"
+        }
+        auditLogService.report(task, request,
+                study: studies,
+                user: currentUserBean,
+                subset1: result_instance_id1,
+                subset2: result_instance_id2
+        )
+        true
+    }
 
     boolean after() { true }
 
--- transmartApp/grails-app/controllers/org/transmartproject/interceptors/ConceptsInterceptor.groovy
+++ transmartApp/grails-app/controllers/org/transmartproject/interceptors/ConceptsInterceptor.groovy
@@ -1,9 +1,32 @@
 package org.transmartproject.interceptors
 
+import org.transmartproject.core.users.User
+
 
 class ConceptsInterceptor {
 
-    boolean before() { true }
+    def auditLogService
+    def studyIdService
+    User currentUserBean
+
+    ConceptsInterceptor(){
+        match(controller:'concepts', action: 'getChildren')
+    }
+
+    boolean before() {
+            if (!auditLogService.enabled) return
+            def studies = null
+            if (params.concept_key) {
+                studies = studyIdService.getStudyIdForConceptKey(params.concept_key, studyConceptOnly: true)
+            }
+            if (studies == null) return
+            def task = "Clinical Data Access"
+            auditLogService.report(task, request,
+                    study: studies,
+                    user: currentUserBean
+            )
+        true
+    }
 
     boolean after() { true }
 
