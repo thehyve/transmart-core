341c8f594826a3fb19396db566ec13e86c88299a
Piotr Zakrzewski <gieksosz@gmail.com>
2016-10-03 12:07:26 +0200

    Merge pull request #62 from thehyve/gb-dev-access
    Add implementation of StudyAccess

---
+++ src/groovy/org/transmartproject/db/ontology/StudyAccessImpl.groovy
@@ -0,0 +1,17 @@
+package org.transmartproject.db.ontology
+
+import org.transmartproject.core.ontology.Study
+import org.transmartproject.core.ontology.StudyAccess
+
+/**
+ * Created by jarverp on 27/09/16.
+ *
+ *
+ */
+class StudyAccessImpl implements StudyAccess{
+
+    Study study
+
+    Map accessibility
+
+}
