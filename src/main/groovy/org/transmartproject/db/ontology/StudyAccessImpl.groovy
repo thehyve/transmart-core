43a67b8cc82ec1d04a6d7010814dde07ed8b8fc3
Jarvanerp <jarno@thehyve.nl>
2016-10-14 12:27:59 +0200

    Fix typo

--- src/main/groovy/org/transmartproject/db/ontology/StudyAccessImpl.groovy
+++ src/main/groovy/org/transmartproject/db/ontology/StudyAccessImpl.groovy
@@ -3,15 +3,10 @@ package org.transmartproject.db.ontology
 import org.transmartproject.core.ontology.Study
 import org.transmartproject.core.ontology.StudyAccess
 
-/**
- * Created by jarverp on 27/09/16.
- *
- *
- */
 class StudyAccessImpl implements StudyAccess{
 
     Study study
 
-    Map accessibility
+    Map accessibleByUser
 
 }
