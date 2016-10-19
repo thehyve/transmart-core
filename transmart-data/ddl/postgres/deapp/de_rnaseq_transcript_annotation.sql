ee4332f7b07dacc0fcdb4b6d280444964ee3d504
Ruslan Forostianov <ruslan@thehyve.nl>
2016-10-19 16:08:05 +0200

    Add reference column to the platform table.

--- transmart-data/ddl/postgres/deapp/de_rnaseq_transcript_annotation.sql
+++ transmart-data/ddl/postgres/deapp/de_rnaseq_transcript_annotation.sql
@@ -3,10 +3,11 @@
 --
 CREATE TABLE de_rnaseq_transcript_annotation (
     id bigint NOT NULL,
-    gpl_id character varying(50),
-    chromosome character varying(2),
-    start_bp bigint,
-    end_bp bigint,
+    gpl_id character varying(50) NOT NULL,
+    ref_id character varying(50) NOT NULL,
+    chromosome character varying(2) NOT NULL,
+    start_bp bigint NOT NULL,
+    end_bp bigint NOT NULL,
     transcript character varying(100)
 );
 
@@ -22,3 +23,8 @@ ALTER TABLE ONLY de_rnaseq_transcript_annotation
 ALTER TABLE ONLY de_rnaseq_transcript_annotation
     ADD CONSTRAINT de_rnaseq_transcript_annotation_gpl_id_fkey FOREIGN KEY (gpl_id) REFERENCES de_gpl_info(platform);
 
+--
+-- Name: de_rnaseq_transcript_annotation_ref_id_unq; Type: CONSTRAINT; Schema: deapp; Owner: -
+--
+ALTER TABLE ONLY de_rnaseq_transcript_annotation
+    ADD CONSTRAINT de_rnaseq_transcript_annotation_ref_id_unq UNIQUE (gpl_id, ref_id);
