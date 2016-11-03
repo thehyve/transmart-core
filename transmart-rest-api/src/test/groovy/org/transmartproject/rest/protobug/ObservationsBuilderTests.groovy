335aacd702e33c29d58d288a3f97fcd25a1ce4f4
Piotr Zakrzewski <piotr@thehyve.nl>
2016-11-03 21:29:41 +0100

    Protobuf serialization WIP

--- transmart-rest-api/src/test/groovy/org/transmartproject/rest/protobug/ObservationsBuilderTests.groovy
+++ transmart-rest-api/src/test/groovy/org/transmartproject/rest/protobug/ObservationsBuilderTests.groovy
@@ -8,12 +8,12 @@ import org.transmartproject.db.clinical.MultidimensionalDataResourceService
 import org.transmartproject.db.dataquery.clinical.ClinicalTestData
 import org.transmartproject.db.dataquery2.Dimension
 import org.transmartproject.db.metadata.DimensionDescription
-import org.transmartproject.rest.protobuf.ObservationsBuilder
 import org.transmartproject.db.TestData
+import org.transmartproject.rest.protobuf.ObservationsSerializer
+
+import static org.hamcrest.MatcherAssert.assertThat
 import static org.hamcrest.core.IsNull.notNullValue
 
-import static org.springframework.test.util.MatcherAssertionErrors.assertThat
-import static org.transmartproject.db.TestData.createHypercubeDefault
 
 
 /**
@@ -23,26 +23,22 @@ import static org.transmartproject.db.TestData.createHypercubeDefault
 @Rollback
 class ObservationsBuilderTests {
 
-    ObservationsBuilder observationsBuilder
 
     TestData testData
     ClinicalTestData clinicalData
     Map<String,Dimension> dims
 
-    def prepareBuilder() {
-        observationsBuilder = new ObservationsBuilder()
-    }
 
     @Autowired
     MultidimensionalDataResourceService queryResource
 
     @Test
     public void testSerialization() throws Exception {
-        prepareBuilder()
         setupData()
-        def mockedCube= queryResource.doQuery(constraints: [study: [clinicalData.longitudinalStudy.name]])
-        def blob =  observationsBuilder.serialize(mockedCube)
-        assertThat(blob, notNullValue())
+        new ObservationsSerializer()
+        //def mockedCube= queryResource.doQuery(constraints: [study: [clinicalData.longitudinalStudy.name]])
+        //def blob =  observationsBuilder.getDimensionsDefs(mockedCube)
+        //assertThat(blob, notNullValue())
     }
 
     void setupData() {
