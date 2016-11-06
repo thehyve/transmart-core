03edcac5c9084de5131aa04bdee6184d5e1c045b
Piotr Zakrzewski <piotr@thehyve.nl>
2016-11-06 11:14:15 +0100

    Add protobuf utils for json serialization. else WIP

--- transmart-rest-api/src/main/groovy/org/transmartproject/rest/protobuf/ObservationsSerializer.groovy
+++ transmart-rest-api/src/main/groovy/org/transmartproject/rest/protobuf/ObservationsSerializer.groovy
@@ -1,9 +1,10 @@
 package org.transmartproject.rest.protobuf
 
 import com.google.common.collect.ImmutableList
-import com.google.common.collect.ImmutableSet
+import groovy.transform.CompileStatic
 import org.transmartproject.db.dataquery2.Dimension
 import org.transmartproject.db.dataquery2.Hypercube
+import org.transmartproject.db.dataquery2.HypercubeValue
 
 /**
  * Created by piotrzakrzewski on 02/11/2016.
@@ -26,20 +27,34 @@ public class ObservationsSerializer {
             if (INLINE_DIMENSIONS.contains(dimensionName)) {
                 builder.setInline(true)
             }
-            def properties = dim.getClass().properties
+            def properties = dim.properties
             def publicFacingFields = SerializableProperties.SERIALIZABLES.get(dimensionName)
             for (String publicField : publicFacingFields) {
                 def fieldDefBuilder = ObservationsProto.FieldDefinition.newBuilder()
                 fieldDefBuilder.setName(publicField)
                 Class valueType = properties.get(publicField).getClass()
                 Class targetType = HighDimBuilder.decideColumnValueType(valueType)
-                if (targetType.equals(String) ) {
+                if (targetType.equals(String)) {
                     fieldDefBuilder.setType(ObservationsProto.FieldDefinition.ColumnType.STRING)
                 }
                 builder.addFields(fieldDefBuilder)
             }
+            builder
         }
         dimensionDeclarations
+    }
+
+    @CompileStatic
+    def getCells(Hypercube cube) {
+        Iterator<HypercubeValue> it = cube.iterator
+        List<Dimension> dims = cube.dimensions
+        while (it.hasNext()) {
+            HypercubeValue value = it.next()
+            def dimElements = dims.collect() { dim ->
+                value.getDimElement(dim)
+            }
+
+        }
     }
 
 
