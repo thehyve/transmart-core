29ab321b9578a83764613ed2a6deb0c4de0cabe0
barteld klasens <barteld@thehyve.nl>
2016-11-09 12:40:01 +0100

    first set of e2e tests for REST API v2

---
+++ transmart-e2e/src/test/groovy/config/Config.groovy
@@ -0,0 +1,18 @@
+package config
+
+/**
+ * Created by barteldklasens on 10/25/16.
+ */
+class Config {
+//    public static final String BASE_URL = 'http://localhost:8080/'
+    public static final String BASE_URL = 'http://transmart-pro-test.thehyve.net/transmart/'
+    public static final String BAD_USERNAME = 'bad username'
+    public static final String BAD_PASSWORD = 'bad password'
+    public static final String GOOD_USERNAME = ''
+    public static final String GOOD_PASSWORD = ''
+    public static final String ADMIN_USERNAME = ''
+    public static final String ADMIN_PASSWORD = ''
+
+    public static final boolean OAUTHNEEDED = true
+    public static final boolean DEBUG = true
+}
