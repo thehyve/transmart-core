7a3feb6bd0b3ae67648856159dd23368cc5cca4f
Gijs Kant <gijs@thehyve.nl>
2016-11-11 09:50:22 +0100

    Update hypercube protobuf serialisation.

--- src/main/groovy/org/transmartproject/rest/protobuf/SerializableProperties.groovy
+++ src/main/groovy/org/transmartproject/rest/protobuf/SerializableProperties.groovy
@@ -11,7 +11,15 @@ class SerializableProperties {
                                                         "age", "race", "maritalStatus",
                                                         "religion", "sourcesystemCd", "sexCd"]
     public static final Set<String> CONCEPT = ["conceptPath", "conceptCode"]
-    public static final Map<String, ImmutableMap<String, Class>> SERIALIZABLES = ["ConceptDimension": CONCEPT,
-                                                                                  "PatientDimension": PATIENT]
+    public static final Set<String> STUDY = ["studyId", "bioExperimentId", "secureObjectToken"]
+    public static final Set<String> TRIAL_VISIT = ["relTimeUnit", "relTime", "relTimeLabel", "study"]
+    public static final Set<String> START_DATE = ["time"]
+    public static final Map<String, Set<String>> SERIALIZABLES = [
+            "ConceptDimension": CONCEPT,
+            "PatientDimension": PATIENT,
+            "TrialVisitDimension": TRIAL_VISIT,
+            "StartDateDimension": START_DATE,
+            "StudyDimension": STUDY,
+    ]
     // TODO: fill in the rest of the serializable fields for all other dimensions
 }
