ebca3d0e00fe40019224a5e5e171f2b7513b9430
Jan Kanis <jan.code@jankanis.nl>
2016-11-14 11:21:02 +0100

    First Hypercube interface classes, rename implementation classes.

---
+++ src/main/groovy/org/transmartproject/core/multidimensionalquery/MultiDimConstraint.groovy
@@ -0,0 +1,9 @@
+package org.transmartproject.core.multidimensionalquery
+
+/**
+ * Constraint for multidimensional data queries
+ *
+ * No methods, the constraint objects are used internally
+ */
+interface MultiDimConstraint {
+}
