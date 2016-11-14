fd1b778ce3c72b68addd0a3283508553d1b4daa9
Jan Kanis <jan.code@jankanis.nl>
2016-11-14 14:36:03 +0100

    Implement hypercube interfaces

--- src/main/groovy/org/transmartproject/core/multidimensionalquery/HypercubeValue.groovy
+++ src/main/groovy/org/transmartproject/core/multidimensionalquery/HypercubeValue.groovy
@@ -1,5 +1,17 @@
 package org.transmartproject.core.multidimensionalquery
 
+import com.google.common.collect.ImmutableSet
+
 interface HypercubeValue {
+
+    def getValue()
+
+    def getAt(Dimension dim)
+
+    int getDimElementIndex(Dimension dim)
+
+    def getDimKey(Dimension dim)
+
+    ImmutableSet<Dimension> availableDimensions()
 
 }
