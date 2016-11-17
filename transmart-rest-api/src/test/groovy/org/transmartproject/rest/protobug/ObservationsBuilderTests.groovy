0ac9f5b28e33a7fca9adca10a2a27175b9f12904
Gijs Kant <gijs@thehyve.nl>
2016-11-17 13:21:57 +0100

    Add protobuf test.

--- transmart-rest-api/src/test/groovy/org/transmartproject/rest/protobug/ObservationsBuilderTests.groovy
+++ transmart-rest-api/src/test/groovy/org/transmartproject/rest/protobug/ObservationsBuilderTests.groovy
@@ -3,7 +3,7 @@ package org.transmartproject.rest.protobug
 import grails.test.mixin.integration.Integration
 import grails.transaction.Rollback
 import groovy.json.JsonSlurper
-import org.junit.Test
+import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.transmartproject.db.clinical.MultidimensionalDataResourceService
 import org.transmartproject.db.dataquery.clinical.ClinicalTestData
@@ -12,9 +12,11 @@ import org.transmartproject.db.dataquery2.query.Constraint
 import org.transmartproject.db.dataquery2.query.StudyConstraint
 import org.transmartproject.db.metadata.DimensionDescription
 import org.transmartproject.db.TestData
+import org.transmartproject.rest.hypercubeProto.ObservationsProto
 import org.transmartproject.rest.protobuf.ObservationsSerializer
+import spock.lang.Specification
 
-import static org.hamcrest.MatcherAssert.assertThat
+import static spock.util.matcher.HamcrestSupport.that
 import static org.hamcrest.Matchers.*
 
 
@@ -23,7 +25,8 @@ import static org.hamcrest.Matchers.*
  */
 @Integration
 @Rollback
-class ObservationsBuilderTests {
+@Slf4j
+class ObservationsBuilderTests extends Specification {
 
     TestData testData
     ClinicalTestData clinicalData
@@ -32,57 +35,78 @@ class ObservationsBuilderTests {
     @Autowired
     MultidimensionalDataResourceService queryResource
 
-    @Test
-    public void testJsonSerialization() throws Exception {
+    public void testJsonSerialization() {
         setupData()
         Constraint constraint = new StudyConstraint(studyId: clinicalData.longitudinalStudy.studyId)
         def mockedCube = queryResource.retrieveData('clinical', [clinicalData.longitudinalStudy], constraint: constraint)
         def builder = new ObservationsSerializer(mockedCube, ObservationsSerializer.Format.JSON)
 
+        when:
         def out = new ByteArrayOutputStream()
         builder.write(out)
         out.flush()
         Collection result = new JsonSlurper().parse(out.toByteArray())
         def dimElementsSize = result.last()['dimension'].size()
 
-        assertThat result.size(), is(14)
-        assertThat result, contains(
+        then:
+        result.size() == 14
+        that result, everyItem(anyOf(
                 hasKey('dimensionDeclarations'),
                 hasKey('dimensionIndexes'),
-                hasKey('dimensionIndexes'),
-                hasKey('dimensionIndexes'),
-                hasKey('dimensionIndexes'),
-                hasKey('dimensionIndexes'),
-                hasKey('dimensionIndexes'),
-                hasKey('dimensionIndexes'),
-                hasKey('dimensionIndexes'),
-                hasKey('dimensionIndexes'),
-                hasKey('dimensionIndexes'),
-                hasKey('dimensionIndexes'),
-                hasKey('dimensionIndexes'),
                 hasKey('dimension')
-        )
-        assertThat result.first()['dimensionDeclarations'], hasSize(mockedCube.dimensions.size())
-        assertThat result.first()['dimensionDeclarations']['name'],
+        ))
+        that result.first()['dimensionDeclarations'], hasSize(mockedCube.dimensions.size())
+        that result.first()['dimensionDeclarations']['name'],
                 containsInAnyOrder(mockedCube.dimensions.collect{it.toString()}.toArray()
                 )
-        assertThat result['dimension'].findAll(), everyItem(hasSize(dimElementsSize))
-        assertThat result['dimensionIndexes'].findAll(), everyItem(hasSize(dimElementsSize))
+        that result['dimension'].findAll(), everyItem(hasSize(dimElementsSize))
+        that result['dimensionIndexes'].findAll(), everyItem(hasSize(dimElementsSize))
     }
 
-    @Test
-    public void testProtobufSerialization() throws Exception {
+    public void testProtobufSerialization() {
         setupData()
         Constraint constraint = new StudyConstraint(studyId: clinicalData.longitudinalStudy.studyId)
         def mockedCube = queryResource.retrieveData('clinical', [clinicalData.longitudinalStudy], constraint: constraint)
         def builder = new ObservationsSerializer(mockedCube, ObservationsSerializer.Format.PROTOBUF)
 
-        def out = new ByteArrayOutputStream()
-        builder.write(out)
-        out.flush()
+        when:
+        def s_out = new ByteArrayOutputStream()
+        builder.write(s_out)
+        s_out.flush()
+        def data = s_out.toByteArray()
+        log.info "OUTPUT: ${s_out.toString()}"
 
-        assertThat out.toByteArray(), not(empty())
-        assertThat mockedCube.dimensions.each{out.toString().contains(it.toString())}, hasSize(mockedCube.dimensions.size())
+        then:
+        data != null
+        data.length > 0
+
+        when:
+        def s_in = new ByteArrayInputStream(data)
+        log.info "Reading header..."
+        def header = ObservationsProto.Header.parseDelimitedFrom(s_in)
+        def cells = []
+        int count = 0
+        while(true) {
+            count++
+            if (count > 12) {
+                throw new Exception("Expected previous message to be marked as 'last'.")
+            }
+            log.info "Reading cell..."
+            def cell = ObservationsProto.Observation.parseDelimitedFrom(s_in)
+            cells << cell
+            if (cell.last) {
+                log.info "Last cell."
+                break
+            }
+        }
+        log.info "Reading footer..."
+        def footer = ObservationsProto.Footer.parseDelimitedFrom(s_in)
+
+        then:
+        header != null
+        header.dimensionDeclarationsList.size() == mockedCube.dimensions.size()
+        cells.size() == 12
+        footer != null
     }
 
     void setupData() {
