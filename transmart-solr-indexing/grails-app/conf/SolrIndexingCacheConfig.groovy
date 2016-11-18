2d626e10d2b64350cafb574438cdf1a16fba3faf
Piotr Zakrzewski <piotr@thehyve.nl>
2016-11-18 17:53:33 +0100

    Move in controllers, domain classes and tests into fresh grails 3 plugin

--- transmart-solr-indexing/grails-app/conf/SolrIndexingCacheConfig.groovy
+++
@@ -1,9 +0,0 @@
-grails.cache.config = {
-    cache {
-        name 'FacetsIndexCache'
-        eternal false
-        timeToLiveSeconds(15 * 60)
-        maxElementsInMemory 10
-        maxElementsOnDisk 0
-    }
-}
