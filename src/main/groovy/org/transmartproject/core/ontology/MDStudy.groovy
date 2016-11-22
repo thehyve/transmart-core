897cd62b56a0b202f749a01cf3334f60271b8dab
Jan Kanis <jan.code@jankanis.nl>
2016-11-22 18:19:23 +0100

    docs

--- src/main/groovy/org/transmartproject/core/ontology/MDStudy.groovy
+++ src/main/groovy/org/transmartproject/core/ontology/MDStudy.groovy
@@ -3,6 +3,10 @@ package org.transmartproject.core.ontology
 import org.transmartproject.core.multidimquery.Dimension
 import org.transmartproject.core.users.ProtectedResource
 
+/**
+ * This class should ideally be called just 'Study', but that name is already taken by the tabular study. 
+ *
+ */
 interface MDStudy extends ProtectedResource {
     String getName()
 
