f574accb0dac38d6093e498e58ef7a6db3d6a7fe
Riza Nugraha <rnugraha@gmail.com>
2016-11-22 17:15:47 +0100

    encode node uri

--- transmartApp/grails-app/assets/javascripts/datasetExplorer/datasetExplorer.js
+++ transmartApp/grails-app/assets/javascripts/datasetExplorer/datasetExplorer.js
@@ -1313,7 +1313,7 @@ function setupOntTree(id_in, title_in) {
     
     ontTree.on('beforecollapsenode', function (node, deep, anim) {
         Ext.Ajax.request({
-            url: removeNodeDseURL + "?node=" + node.id,
+            url: removeNodeDseURL + "?node=" + encodeURIComponent(node.id),
             method: 'POST',
             success: function (result, request) {
             },
@@ -1337,7 +1337,7 @@ function setupOntTree(id_in, title_in) {
 
         if (expand) {
             Ext.Ajax.request({
-                url: addNodeDseURL + "?node=" + node.id,
+                url: addNodeDseURL + "?node=" + encodeURIComponent(node.id),
                 method: 'POST',
                 success: function (result, request) {
                 },
