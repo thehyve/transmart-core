997e9074f4ff74ff4d9d9a11c67c2dca8d0702f5
barteld klasens <barteld@thehyve.nl>
2016-11-22 14:40:34 +0100

    added access tests for /tree_nodes

--- transmart-e2e/src/test/groovy/tests/rest/v2/GetTreeNodesSpec.groovy
+++ transmart-e2e/src/test/groovy/tests/rest/v2/GetTreeNodesSpec.groovy
@@ -1,8 +1,10 @@
 package tests.rest.v2
 
 import grails.converters.JSON
+import org.hamcrest.core.StringContains
 import spock.lang.Ignore
 import spock.lang.IgnoreIf
+import spock.lang.Requires
 
 import static config.Config.*
 import static org.hamcrest.Matchers.*
@@ -12,6 +14,63 @@ import base.RESTSpec
 @IgnoreIf({SUPPRESS_UNIMPLEMENTED})
 class GetTreeNodesSpec extends  RESTSpec{
 
+    /**
+     *  given: "Study SHARED_CONCEPTS_STUDY_C_PRIV and SHARED_CONCEPTS_A is loaded, and I have access"
+     *  when: "I try to get the tree_nodes from all studies"
+     *  then: "nodes from SHARED_CONCEPTS_STUDY_C_PRIV are included"
+     */
+    @Requires({SHARED_CONCEPTS_RESTRICTED_LOADED && SHARED_CONCEPTS_LOADED})
+    def "restricted tree_nodes are included"(){
+        given: "Study SHARED_CONCEPTS_STUDY_C_PRIV and SHARED_CONCEPTS_A is loaded, and I do not have access"
+        setUser(UNRESTRICTED_USERNAME, UNRESTRICTED_PASSWORD)
+
+        when: "I try to get the tree_nodes from all studies"
+        def responseData = get('tree_nodes', contentTypeForJSON)
+
+        then: "only nodes from SHARED_CONCEPTS_A are returned"
+        that responseData, hasEntry(is('tree_nodes'),
+                hasItem(hasEntry(is('key'), containsString('\\Private Studies\\SHARED_CONCEPTS_STUDY_C_PRIV\\')))
+        )
+    }
+
+    /**
+     *  given: "Study SHARED_CONCEPTS_STUDY_C_PRIV and SHARED_CONCEPTS_A is loaded, and I do not have access"
+     *  when: "I try to get the tree_nodes from all studies"
+     *  then: "only nodes from SHARED_CONCEPTS_A are returned"
+     */
+    @Requires({SHARED_CONCEPTS_RESTRICTED_LOADED && SHARED_CONCEPTS_LOADED})
+    def "restricted tree_nodes are excluded"(){
+        given: "Study SHARED_CONCEPTS_STUDY_C_PRIV and SHARED_CONCEPTS_A is loaded, and I do not have access"
+
+        when: "I try to get the tree_nodes from all studies"
+        def responseData = get('tree_nodes', contentTypeForJSON)
+
+        then: "only nodes from SHARED_CONCEPTS_A are returned"
+
+
+        that responseData, hasEntry(is('tree_nodes'),
+                everyItem(hasEntry(is('key'), not(containsString('\\Private Studies\\SHARED_CONCEPTS_STUDY_C_PRIV\\'))))
+        )
+    }
+
+    /**
+     *  given: "Study SHARED_CONCEPTS_RESTRICTED_LOADED is loaded, and I do not have access"
+     *  when: "I try to get the tree_nodes from that study"
+     *  then: "I get an access error"
+     */
+    @Requires({SHARED_CONCEPTS_RESTRICTED_LOADED})
+    def "restricted tree_nodes"() {
+        given: "Study SHARED_CONCEPTS_RESTRICTED_LOADED is loaded, and I do not have access"
+
+        when: "I try to get the tree_nodes from that study"
+        def queryMap = [parent_key : '\\Private Studies\\SHARED_CONCEPTS_STUDY_C_PRIV\\', depth : 2]
+        def responseData = get('tree_nodes', contentTypeForJSON)
+
+        then: "I get an access error"
+        assert responseData.httpStatus == 403
+        assert responseData.type == 'AccessDeniedException'
+        assert responseData.message == "Access denied to study: ${SHARED_CONCEPTS_RESTRICTED_ID}"
+    }
 
     def "get tree_nodes as json"(){
         when: "when I do a GET '/tree_nodes' with header Accept: ${contentTypeForJSON}"
