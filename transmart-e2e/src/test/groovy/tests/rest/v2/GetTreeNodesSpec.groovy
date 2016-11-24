6675b5453d91b49e8255470af054ca87e94c5a89
barteld klasens <barteld@thehyve.nl>
2016-11-24 22:35:23 +0100

    tree node tests

--- transmart-e2e/src/test/groovy/tests/rest/v2/GetTreeNodesSpec.groovy
+++ transmart-e2e/src/test/groovy/tests/rest/v2/GetTreeNodesSpec.groovy
@@ -11,13 +11,12 @@ import static org.hamcrest.Matchers.*
 import static spock.util.matcher.HamcrestSupport.that
 import base.RESTSpec
 
-@IgnoreIf({SUPPRESS_UNIMPLEMENTED})
 class GetTreeNodesSpec extends  RESTSpec{
 
     /**
      *  given: "Study SHARED_CONCEPTS_STUDY_C_PRIV and SHARED_CONCEPTS_A is loaded, and I have access"
      *  when: "I try to get the tree_nodes from all studies"
-     *  then: "nodes from SHARED_CONCEPTS_STUDY_C_PRIV are included"
+     *  then: "nodes from SHARED_CONCEPTS_STUDY_C_PRIV and SHARED_CONCEPTS_A are included"
      */
     @Requires({SHARED_CONCEPTS_RESTRICTED_LOADED && SHARED_CONCEPTS_LOADED})
     def "restricted tree_nodes are included"(){
@@ -25,13 +24,17 @@ class GetTreeNodesSpec extends  RESTSpec{
         setUser(UNRESTRICTED_USERNAME, UNRESTRICTED_PASSWORD)
 
         when: "I try to get the tree_nodes from all studies"
-        def responseData = get('tree_nodes', contentTypeForJSON)
+        def responseData = get(PATH_TREE_NODES, contentTypeForJSON)
 
         then: "only nodes from SHARED_CONCEPTS_A are returned"
-        that responseData, hasEntry(is('tree_nodes'),
-                hasItem(hasEntry(is('key'), containsString('\\Private Studies\\SHARED_CONCEPTS_STUDY_C_PRIV\\')))
-        )
+        def studyA = getRootNodeByName(responseData, SHARED_CONCEPTS_A_ID)
+        def studyC = getRootNodeByName(responseData, SHARED_CONCEPTS_RESTRICTED_ID)
+
+        assert getChildren(studyA, 'name').containsAll([SHARED_CONCEPTS_A_ID, 'Vital Signs', 'Heart Rate', 'Demography', 'Age'])
+        assert getChildren(studyC, 'name').containsAll([SHARED_CONCEPTS_RESTRICTED_ID, 'Vital Signs', 'Heart Rate', 'Demography', 'Age'])
     }
+
+
 
     /**
      *  given: "Study SHARED_CONCEPTS_STUDY_C_PRIV and SHARED_CONCEPTS_A is loaded, and I do not have access"
@@ -43,14 +46,28 @@ class GetTreeNodesSpec extends  RESTSpec{
         given: "Study SHARED_CONCEPTS_STUDY_C_PRIV and SHARED_CONCEPTS_A is loaded, and I do not have access"
 
         when: "I try to get the tree_nodes from all studies"
-        def responseData = get('tree_nodes', contentTypeForJSON)
+        def responseData = get(PATH_TREE_NODES, contentTypeForJSON)
 
         then: "only nodes from SHARED_CONCEPTS_A are returned"
+        def studyA = getRootNodeByName(responseData, SHARED_CONCEPTS_A_ID)
+        def studyC = getRootNodeByName(responseData, SHARED_CONCEPTS_RESTRICTED_ID)
 
+        assert getChildren(studyA, 'name').containsAll([SHARED_CONCEPTS_A_ID, 'Vital Signs', 'Heart Rate', 'Demography', 'Age'])
+        assert studyC == null
+    }
 
-        that responseData, hasEntry(is('tree_nodes'),
-                everyItem(hasEntry(is('key'), not(containsString('\\Private Studies\\SHARED_CONCEPTS_STUDY_C_PRIV\\'))))
-        )
+    /**
+     *  given: "Study SHARED_CONCEPTS is loaded"
+     *  when: "I get the tree_nodes with a subNode and depth"
+     *  then: "only de subNodes of that node are returned"
+     */
+    @Requires({SHARED_CONCEPTS_LOADED})
+    def "params limit nodes returned"(){
+        given: "Study SHARED_CONCEPTS is loaded"
+
+        when: "I get the tree_nodes with a subNode and depth"
+
+        then: "only de subNodes of that node are returned"
     }
 
     /**
@@ -63,8 +80,8 @@ class GetTreeNodesSpec extends  RESTSpec{
         given: "Study SHARED_CONCEPTS_RESTRICTED_LOADED is loaded, and I do not have access"
 
         when: "I try to get the tree_nodes from that study"
-        def queryMap = [parent_key : '\\Private Studies\\SHARED_CONCEPTS_STUDY_C_PRIV\\', depth : 2]
-        def responseData = get('tree_nodes', contentTypeForJSON)
+        def queryMap = [parent_key : "\\Public Studies\\CLINICAL_TRIAL\\", depth : 2]
+        def responseData = get(PATH_TREE_NODES, contentTypeForJSON, queryMap)
 
         then: "I get an access error"
         assert responseData.httpStatus == 403
@@ -72,136 +89,23 @@ class GetTreeNodesSpec extends  RESTSpec{
         assert responseData.message == "Access denied to study: ${SHARED_CONCEPTS_RESTRICTED_ID}"
     }
 
-    def "get tree_nodes as json"(){
-        when: "when I do a GET '/tree_nodes' with header Accept: ${contentTypeForJSON}"
-        def responseData = get('tree_nodes', contentTypeForJSON)
-
-        then: 'then I get all studies in a json format'
-        that responseData, hasEntry(is('tree_nodes'), hasItems(
-                allOf(
-                        hasEntry('key', "\\\\i2b2\\Studies\\GSE8581\\Subjects\\"),
-                        hasEntry('parent_key', "\\\\i2b2\\Studies\\GSE8581\\"),
-                        hasEntry('name', "Subjects"),
-                        hasEntry(is('visual_attributes'), contains("FOLDER")),
-                        hasEntry('patient_count', 15)
-                ),
-                allOf(
-                        hasEntry('key', "\\\\i2b2\\Studies\\GSE8581\\Subjects\\Age\\"),
-                        hasEntry('parent_key', "\\\\i2b2\\Studies\\GSE8581\\Subjects\\"),
-                        hasEntry('name', "Age"),
-                        hasEntry(is('visual_attributes'), contains("NUMERIC")),
-                        hasEntry('patient_count', 15)
-                )
-        ))
+    def getRootNodeByName(list, name){
+        def root
+        list.tree_nodes.each{
+            if (it.name == name){
+                root = it
+            }
+        }
+        return root
     }
 
-    def "get tree_nodes as json with query"(){
-        def queryMap = [parent_key : "\\\\i2b2\\Studies\\GSE8581\\", depth : 2]
-
-        when: "when I do a GET '/tree_nodes' with header Accept: ${contentTypeForJSON}"
-        def responseData = get('tree_nodes', contentTypeForJSON, queryMap)
-
-        then: 'then I get all studies in a json format'
-        that responseData, hasEntry(is('tree_nodes'), hasItems(
-                allOf(
-                        hasEntry('key', "\\\\i2b2\\Studies\\GSE8581\\Subjects\\"),
-                        hasEntry('parent_key', "\\\\i2b2\\Studies\\GSE8581\\"),
-                        hasEntry('name', "Subjects"),
-                        hasEntry(is('visual_attributes'), contains("FOLDER")),
-                        hasEntry('patient_count', 15)
-                ),
-                allOf(
-                        hasEntry('key', "\\\\i2b2\\Studies\\GSE8581\\Subjects\\Age\\"),
-                        hasEntry('parent_key', "\\\\i2b2\\Studies\\GSE8581\\Subjects\\"),
-                        hasEntry('name', "Age"),
-                        hasEntry(is('visual_attributes'), contains("NUMERIC")),
-                        hasEntry('patient_count', 15)
-                )
-        ))
-    }
-
-    def "get tree_nodes as Hal+json with query"(){
-        def queryMap = [parent_key : "\\\\i2b2\\Studies\\GSE8581\\", depth : 2]
-
-        when: "when I do a GET '/tree_nodes' with header Accept: ${contentTypeForHAL}"
-        def responseData = get('tree_nodes', contentTypeForHAL, queryMap)
-
-        then: 'then I get all studies in a Hal+json format'
-        that responseData, halIndexResponse('/tree_nodes?parent_key=%5C%5Ci2b2%5CStudies%5CGSE8581%5C&depth=2', ['tree_nodes':
-                                                                 hasItems(
-                                                                         allOf(
-                                                                                 hasEntry('key', "\\\\i2b2\\Studies\\GSE8581\\Subjects\\"),
-                                                                                 hasEntry('parent_key', "\\\\i2b2\\Studies\\GSE8581\\"),
-                                                                                 hasEntry('name', "Subjects"),
-                                                                                 hasEntry(is('visual_attributes'), contains("FOLDER")),
-                                                                                 hasEntry('patient_count', 15),
-                                                                                 hasChildrenLink("/tree_nodes?parent_key=%5C%5Ci2b2%5CStudies%5CGSE8581%5CSubjects%5C&depth=1")
-                                                                         ),
-                                                                         allOf(
-                                                                                 hasEntry('key', "\\\\i2b2\\Studies\\GSE8581\\Subjects\\Age\\"),
-                                                                                 hasEntry('parent_key', "\\\\i2b2\\Studies\\GSE8581\\Subjects\\"),
-                                                                                 hasEntry('name', "Age"),
-                                                                                 hasEntry(is('visual_attributes'), contains("NUMERIC")),
-                                                                                 hasEntry('patient_count', 15),
-                                                                                 hasEntry(is('_links'), emptyIterable())
-                                                                         ),
-                                                                 )
-        ])
-    }
-
-    def jsonTestData(){
-        return JSON.parse("{\n" +
-                "  \"tree_nodes\": \n" +
-                "  [\n" +
-                "     {\n" +
-                "        \"key\": \"\\\\\\\\i2b2\\\\Studies\\\\GSE8581\\\\Subjects\\\\\",\n" +
-                "        \"parent_key\": \"\\\\\\\\i2b2\\\\Studies\\\\GSE8581\\\\\",\n" +
-                "        \"name\": \"Subjects\",\n" +
-                "        \"visual_attributes\": [ \"FOLDER\" ],\n" +
-                "        \"patient_count\": 15\n" +
-                "     },\n" +
-                "     {\n" +
-                "        \"key\": \"\\\\\\\\i2b2\\\\Studies\\\\GSE8581\\\\Subjects\\\\Age\\\\\",\n" +
-                "        \"parent_key\": \"\\\\\\\\i2b2\\\\Studies\\\\GSE8581\\\\Subjects\\\\\",\n" +
-                "        \"name\": \"Age\",\n" +
-                "        \"visual_attributes\": [ \"NUMERIC\" ],\n" +
-                "        \"patient_count\": 15\n" +
-                "     }\n" +
-                "   ]\n" +
-                "}")
-    }
-
-    def halTestData(){
-        return JSON.parse("{\n" +
-                "    \"_links\": {\n" +
-                "        \"self\": {\n" +
-                "          \"href\": \"/tree_nodes?parent_key=%5C%5Ci2b2%5CStudies%5CGSE8581%5C&depth=2\"\n" +
-                "        }\n" +
-                "    },\n" +
-                "    \"_embedded\": {\n" +
-                "        \"tree_nodes\": [\n" +
-                "             {\n" +
-                "                \"key\": \"\\\\\\\\i2b2\\\\Studies\\\\GSE8581\\\\Subjects\\\\\",\n" +
-                "                \"parent_key\": \"\\\\\\\\i2b2\\\\Studies\\\\GSE8581\\\\\",\n" +
-                "                \"name\": \"Subjects\",\n" +
-                "                \"visual_attributes\": [ \"FOLDER\" ],\n" +
-                "                \"patient_count\": 15,\n" +
-                "                \"_links\": {\n" +
-                "                  \"children\": {\n" +
-                "                    \"href\": \"/tree_nodes?parent_key=%5C%5Ci2b2%5CStudies%5CGSE8581%5CSubjects%5C&depth=1\"\n" +
-                "                  }\n" +
-                "                }\n" +
-                "             },\n" +
-                "             {\n" +
-                "                \"key\": \"\\\\\\\\i2b2\\\\Studies\\\\GSE8581\\\\Subjects\\\\Age\\\\\",\n" +
-                "                \"parent_key\": \"\\\\\\\\i2b2\\\\Studies\\\\GSE8581\\\\Subjects\\\\\",\n" +
-                "                \"name\": \"Age\",\n" +
-                "                \"visual_attributes\": [ \"NUMERIC\" ],\n" +
-                "                \"patient_count\": 15,\n" +
-                "                \"_links\": {}\n" +
-                "            },\n" +
-                "        ]\n" +
-                "    }\n" +
-                "}")
+    def getChildren(node, attribute){
+        def nodeNames = [node.get(attribute)]
+        if (node.children){
+            node.children.each{
+                nodeNames.addAll(getChildren(it, attribute))
+            }
+        }
+        return nodeNames
     }
 }
