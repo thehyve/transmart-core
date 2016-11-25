4fa855dfa3c401061bb74c4ec165da8164df9dea
barteld klasens <barteld@thehyve.nl>
2016-11-25 13:49:26 +0100

    added count=true test

--- transmart-e2e/src/test/groovy/tests/rest/v2/TreeNodesSpec.groovy
+++ transmart-e2e/src/test/groovy/tests/rest/v2/TreeNodesSpec.groovy
@@ -1,17 +1,13 @@
 package tests.rest.v2
 
-import grails.converters.JSON
-import org.hamcrest.core.StringContains
-import spock.lang.Ignore
 import spock.lang.IgnoreIf
 import spock.lang.Requires
 
 import static config.Config.*
-import static org.hamcrest.Matchers.*
-import static spock.util.matcher.HamcrestSupport.that
+
 import base.RESTSpec
 
-class GetTreeNodesSpec extends  RESTSpec{
+class TreeNodesSpec extends  RESTSpec{
 
     /**
      *  given: "Study SHARED_CONCEPTS_STUDY_C_PRIV and SHARED_CONCEPTS_A is loaded, and I have access"
@@ -30,8 +26,8 @@ class GetTreeNodesSpec extends  RESTSpec{
         def studyA = getRootNodeByName(responseData, SHARED_CONCEPTS_A_ID)
         def studyC = getRootNodeByName(responseData, SHARED_CONCEPTS_RESTRICTED_ID)
 
-        assert getChildren(studyA, 'name').containsAll([SHARED_CONCEPTS_A_ID, 'Vital Signs', 'Heart Rate', 'Demography', 'Age'])
-        assert getChildren(studyC, 'name').containsAll([SHARED_CONCEPTS_RESTRICTED_ID, 'Vital Signs', 'Heart Rate', 'Demography', 'Age'])
+        assert getChildrenAtributes(studyA, 'name').containsAll([SHARED_CONCEPTS_A_ID, 'Vital Signs', 'Heart Rate', 'Demography', 'Age'])
+        assert getChildrenAtributes(studyC, 'name').containsAll([SHARED_CONCEPTS_RESTRICTED_ID, 'Vital Signs', 'Heart Rate', 'Demography', 'Age'])
     }
 
 
@@ -52,7 +48,7 @@ class GetTreeNodesSpec extends  RESTSpec{
         def studyA = getRootNodeByName(responseData, SHARED_CONCEPTS_A_ID)
         def studyC = getRootNodeByName(responseData, SHARED_CONCEPTS_RESTRICTED_ID)
 
-        assert getChildren(studyA, 'name').containsAll([SHARED_CONCEPTS_A_ID, 'Vital Signs', 'Heart Rate', 'Demography', 'Age'])
+        assert getChildrenAtributes(studyA, 'name').containsAll([SHARED_CONCEPTS_A_ID, 'Vital Signs', 'Heart Rate', 'Demography', 'Age'])
         assert studyC == null
     }
 
@@ -64,10 +60,41 @@ class GetTreeNodesSpec extends  RESTSpec{
     @Requires({SHARED_CONCEPTS_LOADED})
     def "params limit nodes returned"(){
         given: "Study SHARED_CONCEPTS is loaded"
+        def path = "\\Public Studies\\SHARED_CONCEPTS_STUDY_A\\"
 
         when: "I get the tree_nodes with a subNode and depth"
+        def queryMap = [root : path, depth : 2]
+        def responseData = get(PATH_TREE_NODES, contentTypeForJSON, queryMap)
 
         then: "only de subNodes of that node are returned"
+        assert responseData.size() == 1
+        def studyA = getRootNodeByName(responseData, SHARED_CONCEPTS_A_ID)
+
+        assert getChildrenAtributes(studyA, 'name').containsAll([SHARED_CONCEPTS_A_ID, 'Vital Signs', 'Demography'])
+    }
+
+    /**
+     *  given: "Study SHARED_CONCEPTS is loaded"
+     *  when: "I get the tree_nodes with counts=true"
+     *  then: "then concept nodes have observationCount and patientCount"
+     */
+    @Requires({SHARED_CONCEPTS_LOADED})
+    @IgnoreIf({SUPPRESS_KNOWN_BUGS}) //FIXME: TMPDEV-144 tree_nodes, shared concepts cause access error when requesting counts=true
+                                    //FIXME: TMPDEV-145 tree_nodes, shared concepts counts are 0
+    def "nodes with flag counts true"(){
+        given: "Study SHARED_CONCEPTS is loaded"
+
+        when: "I get the tree_nodes with counts=true"
+        def queryMap = ['counts' : true]
+        def responseData = get(PATH_TREE_NODES, contentTypeForJSON, queryMap)
+
+        then: "then concept nodes have observationCount and patientCount"
+        def studyA = getRootNodeByName(responseData, SHARED_CONCEPTS_A_ID)
+
+        assert getNodeByName(studyA, "Heart Rate").observationCount == 3
+        assert getNodeByName(studyA, "Heart Rate").patientCount == 2
+        assert getNodeByName(studyA, "Age").observationCount == 2
+        assert getNodeByName(studyA, "Age").patientCount == 2
     }
 
     /**
@@ -78,15 +105,16 @@ class GetTreeNodesSpec extends  RESTSpec{
     @Requires({SHARED_CONCEPTS_RESTRICTED_LOADED})
     def "restricted tree_nodes"() {
         given: "Study SHARED_CONCEPTS_RESTRICTED_LOADED is loaded, and I do not have access"
+        def path = "\\Private Studies\\SHARED_CONCEPTS_STUDY_C_PRIV\\"
 
         when: "I try to get the tree_nodes from that study"
-        def queryMap = [parent_key : "\\Public Studies\\CLINICAL_TRIAL\\", depth : 2]
+        def queryMap = [root : path, depth : 1]
         def responseData = get(PATH_TREE_NODES, contentTypeForJSON, queryMap)
 
         then: "I get an access error"
         assert responseData.httpStatus == 403
         assert responseData.type == 'AccessDeniedException'
-        assert responseData.message == "Access denied to study: ${SHARED_CONCEPTS_RESTRICTED_ID}"
+        assert responseData.message == "Access denied to path: ${path}"
     }
 
     def getRootNodeByName(list, name){
@@ -99,11 +127,27 @@ class GetTreeNodesSpec extends  RESTSpec{
         return root
     }
 
-    def getChildren(node, attribute){
+    def getNodeByName(root, name){
+        if (root.name == name){
+            return root
+        }
+        def result
+        if (root.children){
+            root.children.each{
+                def node = getNodeByName(it, name)
+                if (node){
+                    result = node
+                }
+            }
+        }
+        return result
+    }
+
+    def getChildrenAtributes(node, attribute){
         def nodeNames = [node.get(attribute)]
         if (node.children){
             node.children.each{
-                nodeNames.addAll(getChildren(it, attribute))
+                nodeNames.addAll(getChildrenAtributes(it, attribute))
             }
         }
         return nodeNames
