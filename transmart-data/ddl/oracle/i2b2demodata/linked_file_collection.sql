a8370b88af13d09d89c095f75d6bec64ff7f7a6e
Ruslan Forostianov <ruslan@thehyve.nl>
2016-12-15 15:52:14 +0100

    Fix oracle pipeline for the storage tables.

--- transmart-data/ddl/oracle/i2b2demodata/linked_file_collection.sql
+++ transmart-data/ddl/oracle/i2b2demodata/linked_file_collection.sql
@@ -1,7 +1,7 @@
 --
 -- Name: linked_file_collection; Type: TABLE; Schema: i2b2demodata; Owner: -
 --
-CREATE TABLE "i2b2demodata"."linked_file_collection" (
+CREATE TABLE linked_file_collection (
     id integer NOT NULL,
     name character varying(900),
     study integer NOT NULL,
@@ -12,16 +12,16 @@ CREATE TABLE "i2b2demodata"."linked_file_collection" (
 --
 -- Name: linked_file_collection_pkey; Type: CONSTRAINT; Schema: i2b2demodata; Owner: -
 --
-ALTER TABLE ONLY "i2b2demodata"."linked_file_collection"
+ALTER TABLE linked_file_collection
     ADD CONSTRAINT linked_file_collection_pkey PRIMARY KEY (id);
 --
 -- Name: SOURCE_SYSTEM_ID_ID_FK; Type: CONSTRAINT; Schema: i2b2demodata; Owner: -
 --
-ALTER TABLE "i2b2demodata"."linked_file_collection" ADD CONSTRAINT "SOURCE_SYSTEM_ID_ID_FK" FOREIGN KEY ("source_system_id")
- REFERENCES "i2b2demodata"."storage_system" (id);
+ALTER TABLE linked_file_collection ADD CONSTRAINT "SOURCE_SYSTEM_ID_ID_FK" FOREIGN KEY (source_system_id)
+ REFERENCES storage_system (id);
 --
 -- Name: STUDY_ID_FK; Type: CONSTRAINT; Schema: i2b2demodata; Owner: -
 --
-ALTER TABLE "i2b2demodata"."linked_file_collection" ADD CONSTRAINT "STUDY_ID_FK" FOREIGN KEY ("study")
- REFERENCES "i2b2demodata"."study" (study_num);
+ALTER TABLE linked_file_collection ADD CONSTRAINT "STUDY_ID_FK" FOREIGN KEY (study)
+ REFERENCES study (study_num);
 
