85dd4b8c0a3382dec27c1f6f2600446f6e5589d1
Ruslan Forostianov <ruslan@thehyve.nl>
2016-12-15 15:52:14 +0100

    Fix oracle pipeline for the storage tables.

--- transmart-data/ddl/oracle/i2b2demodata/storage_system.sql
+++ transmart-data/ddl/oracle/i2b2demodata/storage_system.sql
@@ -1,18 +1,18 @@
 --
 -- Name: storage_system; Type: TABLE; Schema: i2b2demodata; Owner: -
 --
-CREATE TABLE "i2b2demodata"."storage_system" (
+CREATE TABLE storage_system (
     id integer NOT NULL,
     name character varying(50),
     systemtype character varying(50),
     url character varying(900),
     system_version character varying(50),
-    single_file_collections boolean
+    single_file_collections char(1 byte)
 );
 
 --
 -- Name: storage_system_pkey; Type: CONSTRAINT; Schema: i2b2demodata; Owner: -
 --
-ALTER TABLE ONLY "i2b2demodata"."storage_system"
+ALTER TABLE storage_system
     ADD CONSTRAINT storage_system_pkey PRIMARY KEY (id);
 
