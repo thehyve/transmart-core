6348c5dc5ec01a4b47b0453a12e1d9f9d6b3068a
ewelinagr <ewelina@thehyve.nl>
2016-12-16 12:17:49 +0100

    Change mock-ontology-server to generate only two possible constant responses

--- mock-ontology-server/src/test/groovy/mock/ontology/server/OntologyTermSpec.groovy
+++ mock-ontology-server/src/test/groovy/mock/ontology/server/OntologyTermSpec.groovy
@@ -3,6 +3,8 @@ package mock.ontology.server
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
+import static mock.ontology.server.OntologyTermResponseGenerator.*
+
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
@@ -17,33 +19,74 @@ class OntologyTermSpec extends Specification {
 
     void "test '/search/conceptCode' endpoint"() {
         when:
-        params.conceptCode = "testCode"
+        params.conceptCode = "default_code"
         controller.index()
+        def jsonResponse = response.getJson()
 
         then:
         response.contentType.contains(contentTypeForJSON)
-        response.getJson().size() > 0
-        response.getJson().each {
+        jsonResponse.size() == 2
+        jsonResponse.each {
             assert it.has('score')
             assert it.has('classpath')
             assert it.has('terminology_type')
             assert it.has('label')
         }
+        jsonResponse[0].classpath as List<List<String>> == defaultClasspath
+        jsonResponse[1].classpath as List<List<String>> == snomedClasspath
+
+        when:
+        params.conceptCode = mainSnomedCode
+        response.reset()
+        controller.index()
+        jsonResponse = response.getJson()
+
+        then:
+        response.contentType.contains(contentTypeForJSON)
+        jsonResponse.size() == 2
+        jsonResponse.each {
+            assert it.has('score')
+            assert it.has('classpath')
+            assert it.has('terminology_type')
+            assert it.has('label')
+        }
+        jsonResponse[0].classpath as List<List<String>> == snomedClasspath
+        jsonResponse[1].classpath as List<List<String>> == defaultClasspath
     }
 
     void "test '/idx' endpoint"(){
         when:
-        params.conceptCode = "roxId"
+        def code = "roxId"
+        params.roxId = code
         controller.show()
+        def jsonResponse = response.getJson()
 
         then:
         response.contentType.contains(contentTypeForJSON)
-        response.getJson().size() > 0
-        response.getJson().each {
-            assert it.has('id')
-            assert it.has('node')
-            assert it.has('type')
-        }
+        jsonResponse.size() == 3
+        jsonResponse.has('id')
+        jsonResponse.has('node')
+        jsonResponse.has('type')
+        jsonResponse.id == code
+        jsonResponse.node == 'Default label'
+        jsonResponse.type == sampleDescriptionsMap['Default label']
+
+        when:
+        code = mainDefaultCode
+        params.roxId = code
+        response.reset()
+        controller.show()
+        jsonResponse = response.getJson()
+
+        then:
+        response.contentType.contains(contentTypeForJSON)
+        jsonResponse.size() == 3
+        jsonResponse.has('id')
+        jsonResponse.has('node')
+        jsonResponse.has('type')
+        jsonResponse.id == code
+        jsonResponse.node == labels[mainDefaultCode]
+        jsonResponse.type == sampleDescriptionsMap[labels[mainDefaultCode]]
     }
 }
 
