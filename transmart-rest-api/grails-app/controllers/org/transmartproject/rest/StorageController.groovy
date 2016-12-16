6907d398967a54a3e1de695fc704bf7af6558aa8
Piotr Zakrzewski <piotr@thehyve.nl>
2016-12-16 16:01:46 +0100

    Adapt test to the new ID generation policy. handle 404 with more style.

--- transmart-rest-api/grails-app/controllers/org/transmartproject/rest/StorageController.groovy
+++ transmart-rest-api/grails-app/controllers/org/transmartproject/rest/StorageController.groovy
@@ -42,6 +42,9 @@ class StorageController extends RestfulController {
     @Override
     def show() {
         def fileCollection = queryForResource(params.id)
+        if (fileCollection == null) {
+            respond "Resource not found", [status: CREATED, view:'show']
+        }
         currentUser.checkAccess(READ, fileCollection.study)
         respond fileCollection
     }
