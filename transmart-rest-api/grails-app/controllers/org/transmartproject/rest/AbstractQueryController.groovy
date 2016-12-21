ca19018f8f5b27588a19deb5f50973647c1daef1
Gijs Kant <gijs@thehyve.nl>
2016-12-21 21:08:13 +0100

    Fix parameters checking in REST API.

--- transmart-rest-api/grails-app/controllers/org/transmartproject/rest/AbstractQueryController.groovy
+++ transmart-rest-api/grails-app/controllers/org/transmartproject/rest/AbstractQueryController.groovy
@@ -27,6 +27,27 @@ abstract class AbstractQueryController implements Controller {
 
     def conceptsResourceService
 
+    /**
+     * Checks if there are any parameters that are not in the set of default parameters
+     * (format, action, controller, apiVersion) or the set of additional parameters for
+     * the endpoint passed in <code>acceptedParameters</code>.
+     *
+     * @param parameters the request parameters
+     * @param acceptedParameters the collection of supported non-default parameters.
+     * @throws InvalidArgumentsException iff a parameter is used that is not supported.
+     */
+    static void checkParams(Map parameters, Collection<String> acceptedParameters) {
+        def acceptedParams = (['format', 'action', 'controller', 'apiVersion'] as Set) + acceptedParameters
+        def unacceptableParams = parameters.keySet() - acceptedParams
+        if (!unacceptableParams.empty) {
+            if (unacceptableParams.size() == 1) {
+                throw new InvalidArgumentsException("Parameter not supported: ${unacceptableParams.first()}.")
+            } else {
+                throw new InvalidArgumentsException("Parameters not supported: ${unacceptableParams.join(', ')}.")
+            }
+        }
+    }
+
     protected static Constraint parseConstraint(String constraintText) {
         try {
             Map constraintData = JSON.parse(constraintText) as Map
