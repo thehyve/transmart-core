c764f297999ecd5e5994e9cc59f3cfb8da86ffda
Ruslan Forostianov <ruslan@thehyve.nl>
2017-01-19 19:17:40 +0100

    Move samples and time series migration to one file

--- transmart-data/updatedb/release-17.1/postgres/data_migration.sql
+++ transmart-data/updatedb/release-17.1/postgres/data_migration.sql
@@ -216,3 +216,82 @@ ALTER TABLE ONLY i2b2demodata.observation_fact ALTER COLUMN trial_visit_num SET 
 -- Fix referencial consistency for the visit dimension.
 update i2b2demodata.observation_fact set encounter_num = -1
 where encounter_num <> -1 and not exists(select * from i2b2demodata.visit_dimension v where v.encounter_num = observation_fact.encounter_num);
+
+-- Samples and time series migration 
+-- See https://wiki.transmartfoundation.org/display/transmartwiki/Samples+and+Time+series+data
+insert into i2b2demodata.modifier_dimension(
+  modifier_path,
+  modifier_cd,
+  name_char
+) select '\\Samples\\Sample Codes\\', 'TRANSMART:SMPL', 'Sample Codes'
+where not exists(select * from i2b2demodata.modifier_dimension where modifier_cd = 'TRANSMART:SMPL');
+
+ --delete from i2b2demodata.observation_fact where modifier_cd = 'TRANSMART:SMPL';
+
+insert into I2B2DEMODATA.OBSERVATION_FACT(
+   ENCOUNTER_NUM,
+   PATIENT_NUM,
+   CONCEPT_CD,
+   PROVIDER_ID,
+   START_DATE,
+   MODIFIER_CD,
+   INSTANCE_NUM,
+   TRIAL_VISIT_NUM,
+   VALTYPE_CD,
+   TVAL_CHAR,
+   LOCATION_CD,
+   SOURCESYSTEM_CD
+)
+select 
+  o.encounter_num as ENCOUNTER_NUM,
+  o.patient_num as PATIENT_NUM,
+  o.concept_cd as CONCEPT_CD,
+  o.provider_id as PROVIDER_ID,
+  o.start_date as START_DATE,
+  'TRANSMART:SMPL' as MODIFIER_CD,
+  o.instance_num INSTANCE_NUM,
+  o.trial_visit_num as TRIAL_VISIT_NUM,
+  'T' as VALTYPE_CD,
+  o.sample_cd as TVAL_CHAR,
+  o.location_cd as LOCATION_CD,
+  o.sourcesystem_cd as SOURCESYSTEM_CD
+from i2b2demodata.observation_fact o
+where sample_cd is not null
+  and not exists(select * from i2b2demodata.observation_fact o2 where o2.patient_num = o.patient_num and o2.concept_cd = o.concept_cd and o2.modifier_cd = 'TRANSMART:SMPL');
+
+-- Time Series
+CREATE TEMP TABLE concept_specific_trials AS SELECT
+  distinct
+  s.study_num as STUDY_NUM,
+  (xpath('//SeriesMeta/DisplayName/text()', i.c_metadataxml::xml))[1]::text as REL_TIME_LABEL,
+  (xpath('//SeriesMeta/Value/text()', i.c_metadataxml::xml))[1]::text::integer as REL_TIME_NUM,
+  (xpath('//SeriesMeta/Unit/text()', i.c_metadataxml::xml))[1]::text as REL_TIME_UNIT_CD,
+  c.concept_cd as CONCEPT_CD
+FROM I2B2METADATA.I2B2 i
+INNER JOIN I2B2DEMODATA.STUDY s ON s.study_id = i.sourcesystem_cd
+INNER JOIN I2B2DEMODATA.CONCEPT_DIMENSION c on c.CONCEPT_PATH = i.C_FULLNAME
+WHERE i.C_METADATAXML IS NOT NULL
+  and i.C_METADATAXML LIKE '%SeriesMeta%';
+
+INSERT INTO I2B2DEMODATA.TRIAL_VISIT_DIMENSION(
+  STUDY_NUM,
+  REL_TIME_UNIT_CD,
+  REL_TIME_NUM,
+  REL_TIME_LABEL
+) SELECT 
+  cst.STUDY_NUM,
+  cst.REL_TIME_UNIT_CD,
+  cst.REL_TIME_NUM,
+  cst.REL_TIME_LABEL
+FROM concept_specific_trials cst
+WHERE not exists(select * from i2b2demodata.trial_visit_dimension tv where tv.STUDY_NUM = cst.STUDY_NUM and tv.REL_TIME_UNIT_CD = cst.REL_TIME_UNIT_CD and tv.REL_TIME_NUM = cst.REL_TIME_NUM and tv.REL_TIME_LABEL = cst.REL_TIME_LABEL);
+
+UPDATE I2B2DEMODATA.OBSERVATION_FACT SET TRIAL_VISIT_NUM = (
+  select TRIAL_VISIT_NUM from I2B2DEMODATA.TRIAL_VISIT_DIMENSION tv
+  inner join concept_specific_trials cst on cst.study_num = tv.study_num
+  and cst.REL_TIME_NUM = tv.REL_TIME_NUM
+  and cst.REL_TIME_LABEL = tv.REL_TIME_LABEL
+  and cst.REL_TIME_UNIT_CD = tv.REL_TIME_UNIT_CD)
+WHERE concept_cd in (select concept_cd from concept_specific_trials);
+
+DROP TABLE concept_specific_trials;
