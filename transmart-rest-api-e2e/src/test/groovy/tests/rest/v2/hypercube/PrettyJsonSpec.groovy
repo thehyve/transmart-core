158793e7f42c9546e3c0b720a12ff1db7d20d2b0
barteld klasens <barteld@thehyve.nl>
2017-01-24 19:46:57 +0100

    is json pretty

--- transmart-rest-api-e2e/src/test/groovy/tests/rest/v2/hypercube/PrettyJsonSpec.groovy
+++ transmart-rest-api-e2e/src/test/groovy/tests/rest/v2/hypercube/PrettyJsonSpec.groovy
@@ -1,7 +1,45 @@
 package tests.rest.v2.hypercube
 
-/**
- * Created by barteldklasens on 1/24/17.
- */
-class PrettyJsonSpec {
+import base.RESTSpec
+import spock.lang.Ignore
+
+import static config.Config.EHR_ID
+import static config.Config.PATH_PATIENTS
+import static tests.rest.v2.constraints.StudyNameConstraint
+
+class PrettyJsonSpec extends RESTSpec{
+
+    /**
+     * given: 'we get Json'
+     * when: 'we rate his prettyness'
+     * then: 'he scores at least 8'
+     */
+    @Ignore
+    def "Is Json pretty enough?"(){
+        int index = new Random().nextInt(3)
+
+        given: 'we get json'
+        def Json = get([
+                path: PATH_PATIENTS,
+                acceptType: contentTypeForJSON,
+                query: toQuery([type: StudyNameConstraint, studyId: EHR_ID])
+        ]).patients[index]
+
+        when: 'we rate his prettynis'
+        def score = 0;
+        if (Json.birthDate != null) score++
+        if (Json.deathDate == null) score += 3
+        if (Json.age < 40) score++
+        if (Json.age > 25) score++
+        assert Json.age > 18 : "Json needs to grow up, stop looking at him like that!"
+        if (Json.maritalStatus == null) score++
+        if (Json.sex == 'MALE') score++
+        if (Json.sex == 'YES') score +=3
+        if (Json.race == 'Latino') score++
+
+        then: 'he scores at least 8'
+        println(Json)
+        assert score >= 8 : "score is ${score}, sorry Json is not pretty enough."
+    }
+
 }
