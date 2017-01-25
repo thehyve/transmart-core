74534353cb144c065c4479359df400f81d43dd84
Ruslan Forostianov <ruslan@thehyve.nl>
2017-01-25 18:31:51 +0100

    Make date not locale dependent.

--- transmart-data/updatedb/release-17.1/oracle/observation_fact.sql
+++ transmart-data/updatedb/release-17.1/oracle/observation_fact.sql
@@ -1,5 +1,5 @@
 ALTER TABLE i2b2demodata.observation_fact DROP CONSTRAINT observation_fact_pkey;
-UPDATE i2b2demodata.observation_fact SET start_date = '01-01-01' WHERE start_date is null;
+UPDATE i2b2demodata.observation_fact SET start_date = TO_DATE('01-01-01', 'yy-mm-dd') WHERE start_date is null;
 UPDATE i2b2demodata.observation_fact SET instance_num = 1 WHERE instance_num is null;
 ALTER TABLE i2b2demodata.observation_fact ADD CONSTRAINT observation_fact_pkey PRIMARY KEY (encounter_num, patient_num, concept_cd, provider_id, instance_num, modifier_cd, start_date);
 ALTER TABLE i2b2demodata.observation_fact ADD trial_visit_num numeric(38,0);
