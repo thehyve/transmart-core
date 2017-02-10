b0ad18d76fa1f953c8819669431a5a7229293335
Piotr Zakrzewski <piotr@thehyve.nl>
2017-02-10 12:34:52 +0100

    Make tm-db work with transmart-server

--- transmart-server/grails-app/init/transmart/server/Application.groovy
+++ transmart-server/grails-app/init/transmart/server/Application.groovy
@@ -2,7 +2,10 @@ package transmart.server
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.security.SecurityFilterAutoConfiguration
 
+@EnableAutoConfiguration(exclude = [SecurityFilterAutoConfiguration])
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
