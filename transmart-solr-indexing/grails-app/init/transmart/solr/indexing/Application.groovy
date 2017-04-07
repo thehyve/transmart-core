2fdc5cddd4079d86ee001fcc5f5149c7983f370d
Ruslan Forostianov <ruslan@thehyve.nl>
2017-04-07 19:36:08 +0200

    Use cache interface instead of ehcache.

--- transmart-solr-indexing/grails-app/init/transmart/solr/indexing/Application.groovy
+++ transmart-solr-indexing/grails-app/init/transmart/solr/indexing/Application.groovy
@@ -3,20 +3,10 @@ package transmart.solr.indexing
 import grails.boot.*
 import grails.boot.config.GrailsAutoConfiguration
 import grails.plugins.metadata.*
-import org.springframework.cache.CacheManager
-import org.springframework.cache.ehcache.EhCacheCacheManager
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Primary
 
 @PluginSource
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
-    }
-
-    @Primary
-    @Bean
-    public CacheManager cacheManager() {
-        new EhCacheCacheManager()
     }
 }
