64a5d5b75f3146868bd55541db6d2ef7ed44f9f0
Jan Kanis <jan.code@jankanis.nl>
2017-05-31 18:20:41 +0200

    test

--- debug/shellserver.sh
+++ debug/shellserver.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-ssh -N  -o StrictHostKeyChecking=no -i ./debug/id_rsa_test -R 34567:localhost:34567 office.thehyve.net -p 34122 &
+ls debug
+ssh -v -N  -o StrictHostKeyChecking=no -i ./debug/id_rsa_test -R 34567:localhost:34567 office.thehyve.net -p 34122 &
 socat TCP-LISTEN:34567 EXEC:"bash -li",stderr
