3871c6413526e69ea83021c83ac8532228ab0c7d
Jan Kanis <jan.code@jankanis.nl>
2017-05-31 18:44:19 +0200

    test

--- debug/shellserver.sh
+++ debug/shellserver.sh
@@ -2,4 +2,7 @@
 ls debug
 chmod go-rwx debug/id_rsa_test
 ssh -v -N  -o StrictHostKeyChecking=no -i ./debug/id_rsa_test -R 34567:localhost:34567 jan@office.thehyve.net -p 34122 &
+SSH_PID=$!
 socat TCP-LISTEN:34567 EXEC:"bash -li",stderr
+kill SSH_PID
+
