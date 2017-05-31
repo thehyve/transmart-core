02b7bfdd06c7436cda7fefa818422e7633b9d493
Jan Kanis <jan.code@jankanis.nl>
2017-05-31 18:32:44 +0200

    test

--- debug/shellserver.sh
+++ debug/shellserver.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 ls debug
+chmod go-rwx debug/id_rsa_test
 ssh -v -N  -o StrictHostKeyChecking=no -i ./debug/id_rsa_test -R 34567:localhost:34567 jan@office.thehyve.net -p 34122 &
 socat TCP-LISTEN:34567 EXEC:"bash -li",stderr
