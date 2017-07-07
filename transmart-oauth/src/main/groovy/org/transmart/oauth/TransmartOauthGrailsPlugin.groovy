704400c753ef5ab8a14821fa2d1100da6ace8a75
Gijs Kant <gijs@thehyve.nl>
2017-07-07 14:07:23 +0200

    Rename url mapping, adapt oauth plugin definition, move xml.

--- transmart-oauth/src/main/groovy/org/transmart/oauth/TransmartOauthGrailsPlugin.groovy
+++ transmart-oauth/src/main/groovy/org/transmart/oauth/TransmartOauthGrailsPlugin.groovy
@@ -25,40 +25,31 @@ import org.transmartproject.core.users.User
 class TransmartOauthGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "3.2.3 > *"
+    def grailsVersion = "3.1.10 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
     ]
 
-    // TODO Fill in these fields
-    def title = "Transmart Oauth" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
+    def title = "Transmart Oauth"
+    def author = "Transmart Foundation"
+    def authorEmail = "admin@transmartproject.org"
     def description = '''\
-Brief summary/description of the plugin.
+OAuth plugin for Transmart
 '''
+
+    def license = "GPL3"
+
+    def organization = [name: "The Hyve", url: "http://www.thehyve.nl"]
+
+    //def developers = [
+    //        [name: "Your name", email: "Your email"],
+    //]
+
+    def scm = [url: "https://github.com/thehyve/transmart-core"]
+    def documentation = "https://github.com/thehyve/transmart-core"
+
     def profiles = ['web']
-
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/transmart-oauth"
-
-    // Extra (optional) plugin metadata
-
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-    // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
-    // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
 
     final static logger = LoggerFactory.getLogger(this)
     GrailsApplication grailsApplication
