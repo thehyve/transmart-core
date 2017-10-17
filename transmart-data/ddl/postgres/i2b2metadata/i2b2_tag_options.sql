e0c2bc44ad7fa93d75defb6db1311c9907e76049
Gijs Kant <gijs@thehyve.nl>
2017-10-17 18:42:02 +0200

    Add data commons schema changes to updatedb scripts.

--- transmart-data/ddl/postgres/i2b2metadata/i2b2_tag_options.sql
+++ transmart-data/ddl/postgres/i2b2metadata/i2b2_tag_options.sql
@@ -1,7 +1,7 @@
 --
 -- Name: i2b2_tag_options_tag_option_id_seq; Type: SEQUENCE; Schema: i2b2metadata; Owner: -
 --
-CREATE SEQUENCE i2b2_tag_options_tag_option_id_seq
+CREATE SEQUENCE i2b2metadata.i2b2_tag_options_tag_option_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -11,7 +11,7 @@ CREATE SEQUENCE i2b2_tag_options_tag_option_id_seq
 --
 -- Name: i2b2_tag_options; Type: TABLE; Schema: i2b2metadata; Owner: -
 --
-CREATE TABLE i2b2_tag_options (
+CREATE TABLE i2b2metadata.i2b2_tag_options (
     tag_option_id integer NOT NULL,
     tag_type_id bigint NOT NULL,
     value character varying(1000)
@@ -20,21 +20,23 @@ CREATE TABLE i2b2_tag_options (
 --
 -- Name: tag_option_id; Type: DEFAULT; Schema: i2b2metadata; Owner: -
 --
-ALTER TABLE ONLY i2b2_tag_options ALTER COLUMN tag_option_id SET DEFAULT nextval('i2b2_tag_options_tag_option_id_seq'::regclass);
+ALTER TABLE ONLY i2b2metadata.i2b2_tag_options ALTER COLUMN tag_option_id
+    SET DEFAULT nextval('i2b2_tag_options_tag_option_id_seq'::regclass);
 
 --
 -- Name: i2b2_tag_options_pkey; Type: CONSTRAINT; Schema: i2b2metadata; Owner: -
 --
-ALTER TABLE ONLY i2b2_tag_options
+ALTER TABLE ONLY i2b2metadata.i2b2_tag_options
     ADD CONSTRAINT i2b2_tag_options_pkey PRIMARY KEY (tag_option_id);
 
 --
 -- Name: idx_i2b2_tag_option_pk; Type: INDEX; Schema: i2b2metadata; Owner: -
 --
-CREATE UNIQUE INDEX idx_i2b2_tag_option_pk ON i2b2_tag_options USING btree (tag_option_id);
+CREATE UNIQUE INDEX idx_i2b2_tag_option_pk ON i2b2metadata.i2b2_tag_options USING btree (tag_option_id);
 
 --
 -- Name: i2b2_tag_options_tag_type_fk; Type: FK CONSTRAINT; Schema: i2b2metadata; Owner: -
 --
-ALTER TABLE ONLY i2b2_tag_options
-    ADD CONSTRAINT i2b2_tag_options_tag_type_fk FOREIGN KEY (tag_type_id) REFERENCES i2b2_tag_types(tag_type_id);
+ALTER TABLE ONLY i2b2metadata.i2b2_tag_options
+    ADD CONSTRAINT i2b2_tag_options_tag_type_fk FOREIGN KEY (tag_type_id)
+    REFERENCES i2b2metadata.i2b2_tag_types(tag_type_id);
--- transmart-data/updatedb/dev/README.md
+++ transmart-data/updatedb/dev/README.md
@@ -22,6 +22,12 @@ Only `i2b2_secure` is updated, as the `i2b2` table is redundant and is considere
 `data_migration.sql` takes care of adding the appropriate visual attributes to leaf nodes.
 See script's comments for more details.
 
+### Managed tag types
+
+For managing vocabularies used in certain tag types, two tables have been introduced:
+`i2b2_tag_types` and `i2b2_tag_options`, and a new column `option_id` in the
+`i2b2_tags` table. 
+
 
 How to apply all changes
 ------------------------
