d785664abc47f15577a4df1a9f9b6f6d7c6aadee
Ruslan Forostianov <ruslan@thehyve.nl>
2017-10-24 16:47:29 +0200

    Add relation domain classes and minimalistic tests with data.

---
+++ transmart-core-db/grails-app/domain/org/transmartproject/db/pedigree/Relation.groovy
@@ -0,0 +1,29 @@
+/* (c) Copyright 2017, tranSMART Foundation, Inc. */
+
+package org.transmartproject.db.pedigree
+
+import org.transmartproject.db.i2b2data.PatientDimension
+
+class Relation implements Serializable {
+
+    PatientDimension leftSubject
+    RelationType relationType
+    PatientDimension rightSubject
+    Boolean biological
+
+    static mapping = {
+        table schema: 'I2B2DEMODATA'
+        id composite: [ 'leftSubject', 'relationType', 'rightSubject' ]
+        leftSubject column: 'left_subject_id'
+        relationType column: 'relation_type_id'
+        rightSubject column: 'right_subject_id'
+        version false
+    }
+
+    static constraints = {
+        leftSubject nullable: false
+        relationType nullable: false
+        rightSubject nullable: false
+        biological nullable: true
+    }
+}
---
+++ transmart-core-db/src/integration-test/groovy/org/transmartproject/db/pedigree/RelationSpec.groovy
@@ -0,0 +1,34 @@
+/* (c) Copyright 2017, tranSMART Foundation, Inc. */
+
+package org.transmartproject.db.pedigree
+
+import grails.test.mixin.integration.Integration
+import grails.transaction.Rollback
+import org.transmartproject.db.i2b2data.PatientDimension
+import spock.lang.Specification
+
+@Rollback
+@Integration
+class RelationSpec extends Specification {
+
+    void 'test relation type domain object mapping'() {
+        when:
+        def relation = RelationType.findByLabel('MZ')
+        then:
+        relation.id
+        relation.description == 'Monozygotic twin'
+        relation.symmetrical
+        relation.biological
+    }
+
+    void 'test relation domain object mapping'() {
+        def subject1 = PatientDimension.get(-3000)
+        def subject2 = PatientDimension.get(-3001)
+        when:
+        def relation = Relation.findByLeftSubjectAndRightSubject(subject1, subject2)
+        then:
+        relation.relationType.description == 'Spouse'
+        !relation.biological
+    }
+
+}
