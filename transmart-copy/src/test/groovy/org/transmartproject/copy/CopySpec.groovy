7702428b5e7cc39605185cfe9b54920ef34607ec
Gijs Kant <gijs@thehyve.nl>
2017-11-11 00:47:59 +0100

    Fix transmart-copy tests.

--- transmart-copy/src/test/groovy/org/transmartproject/copy/CopySpec.groovy
+++ transmart-copy/src/test/groovy/org/transmartproject/copy/CopySpec.groovy
@@ -5,43 +5,54 @@ import spock.lang.Specification
 
 class CopySpec extends Specification {
 
+    static String testStudy = 'SURVEY0'
+    static survey_study_query = "select * from ${Studies.study_table} where study_id = '${testStudy}'".toString()
+
+    static defaultConfig = new Copy.Config(
+            dropIndexes: false,
+            unlogged: false,
+            write: false
+    )
+
+    List<String> fetchStudies(Database database) {
+        database.jdbcTemplate.queryForList(survey_study_query).collect { it['study_id'] as String }
+    }
+
     def 'test loading SURVEY0 data'() {
-        def survey_study_query = "select * from ${Studies.study_table} where study_id = 'SURVEY0'".toString()
         given: 'Test database is available, study SURVEY0 is not loaded'
         def copy = new Copy()
         copy.init()
         assert !copy.database.connection.closed
-        def studies = copy.database.sql.rows(survey_study_query).collect { it['study_id'] as String }
+        def studies = fetchStudies(copy.database)
         if (!studies.empty) {
-            copy.deleteStudy('SURVEY0')
-            studies = copy.database.sql.rows(survey_study_query).collect { it['study_id'] as String }
+            copy.deleteStudy(testStudy)
+            studies = fetchStudies(copy.database)
         }
         assert studies.empty
 
         when: 'Loading example study data'
-        copy.run('./src/main/resources/examples/SURVEY0')
-        studies = copy.database.sql.rows(survey_study_query).collect { it['study_id'] as String }
+        copy.run('./src/main/resources/examples/SURVEY0', defaultConfig)
+        studies = fetchStudies(copy.database)
 
         then: 'Expect the study to be loaded'
         studies.size() == 1
     }
 
     def 'test deleting SURVEY0'() {
-        def survey_study_query = "select * from ${Studies.study_table} where study_id = 'SURVEY0'".toString()
         given: 'Test database is available, study SURVEY0 loaded'
         def copy = new Copy()
         copy.init()
         assert !copy.database.connection.closed
-        def studies = copy.database.sql.rows(survey_study_query).collect { it['study_id'] as String }
+        def studies = fetchStudies(copy.database)
         if (studies.empty) {
-            copy.run('./src/main/resources/examples/SURVEY0')
-            studies = copy.database.sql.rows(survey_study_query).collect { it['study_id'] as String }
+            copy.run('./src/main/resources/examples/SURVEY0', defaultConfig)
+            studies = fetchStudies(copy.database)
         }
         assert studies.size() == 1
 
         when: 'Deleting study SURVEY0'
-        copy.deleteStudy('SURVEY0')
-        studies = copy.database.sql.rows(survey_study_query).collect { it['study_id'] as String }
+        copy.deleteStudy(testStudy)
+        studies = fetchStudies(copy.database)
 
         then: 'Expect the study to be loaded'
         studies.empty
