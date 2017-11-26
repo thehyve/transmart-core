7a35bf936a476fde31889d73b7633a06021f20b1
Ruslan Forostianov <ruslan@thehyve.nl>
2017-11-26 22:38:38 +0100

    Add variable levels (aka measurement) to spss export.

--- transmart-rest-api-e2e/src/test/groovy/tests/rest/v2/DataExportSpec.groovy
+++ transmart-rest-api-e2e/src/test/groovy/tests/rest/v2/DataExportSpec.groovy
@@ -361,7 +361,7 @@ class DataExportSpec extends RESTSpec {
         def filesLineNumbers = getFilesLineNumbers(downloadResponse as byte[])
         filesLineNumbers.size() == 2
         filesLineNumbers['spss/data.tsv'] == 15
-        filesLineNumbers['spss/data.sps'] == 50
+        filesLineNumbers['spss/data.sps'] == 65
     }
 
     @RequiresStudy(CATEGORICAL_VALUES_ID)
@@ -401,7 +401,7 @@ class DataExportSpec extends RESTSpec {
         def filesLineNumbers = getFilesLineNumbers(downloadResponse as byte[])
         filesLineNumbers.size() == 2
         filesLineNumbers['spss/data.tsv'] == 4
-        filesLineNumbers['spss/data.sps'] == 33
+        filesLineNumbers['spss/data.sps'] == 44
     }
 
     def "get supported file formats for survey table"() {
