3e631bb6f18c145b4ad94c0e67df302feec26512
Ewelina Grudzien <ewelina@thehyve.nl>
2017-12-19 14:39:12 +0100

    Add plugins for subscription job triggering and email sending

---
+++ transmart-server/grails-app/jobs/org/transmart/server/subscription/QueryDiffSubscriptionDailyJob.groovy
@@ -0,0 +1,47 @@
+package org.transmart.server.subscription
+
+import groovy.util.logging.Slf4j
+import org.transmart.server.subscription.QueryDiffSubscriptionService
+import org.transmart.server.subsctiption.SubscriptionFrequency
+
+/**
+ * Quartz plugin job for user query subscription
+ * See Grails Quartz Plugin documentation for more information:
+ * http://grails-plugins.github.io/grails-quartz/latest/guide/
+ */
+@Slf4j
+class QueryDiffSubscriptionDailyJob {
+
+    QueryDiffSubscriptionService queryDiffSubscriptionService
+    /**
+     * Specifies a cron expression for job trigger
+     cronExpression: "s m h D M W Y"
+         Second, 0-59;
+         Minute, 0-59;
+         Hour, 0-23;
+         Day of Month, 1-31, ?;
+         Month, 1-12 or JAN-DEC;
+         Day of Week, 1-7 or SUN-SAT, ?;
+         Year [optional]
+      Either Day-of-Week or Day-of-Month must be "?".
+      Can't specify both fields, nor leave both as the all values wildcard "*"
+     **/
+    static triggers = {
+        cron name: 'dailySubscriptionTrigger', cronExpression: " 0 0 0 1/1 * ?"
+        // cron name: 'dailySubscriptionTestTrigger', cronExpression: "0 0/1 * 1/1 * ?"
+    }
+
+    /**
+     * A short description of the job
+     */
+    def description = "Daily job to check for user query data updates."
+
+    /**
+     * Runs generating emails
+     */
+    void execute() {
+        queryDiffSubscriptionService.run(SubscriptionFrequency.DAILY)
+        // todo add a new row to AsyncJob table (?)
+        log.info "Daily subscription job executed."
+    }
+}
---
+++ transmart-server/src/main/groovy/org/transmart/server/subsctiption/SubscriptionFrequency.groovy
@@ -0,0 +1,11 @@
+package org.transmart.server.subsctiption
+
+/**
+ * The frequency of emails for user query subscription
+ */
+enum SubscriptionFrequency {
+
+    DAILY,
+    WEEKLY
+
+}
