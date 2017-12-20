49a6343bce938472c5a1290c17e961d1abc519fa
Ewelina Grudzien <ewelina@thehyve.nl>
2017-12-20 15:14:15 +0100

    Add query subscription controllers and services

--- transmart-server/src/main/groovy/org/transmart/server/subsctiption/SubscriptionFrequency.groovy
+++ transmart-server/src/main/groovy/org/transmart/server/subsctiption/SubscriptionFrequency.groovy
@@ -1,11 +1,29 @@
 package org.transmart.server.subsctiption
 
+import groovy.transform.CompileStatic
+
 /**
  * The frequency of emails for user query subscription
  */
+@CompileStatic
 enum SubscriptionFrequency {
 
-    DAILY,
-    WEEKLY
+    DAILY ('DAILY'),
+    WEEKLY ('WEEKLY')
 
+    private String subscriptionFrequency
+
+    SubscriptionFrequency(String subsciptionFrequency) {
+        this.subscriptionFrequency = subsciptionFrequency
+    }
+
+    static SubscriptionFrequency from(String frequency) {
+        SubscriptionFrequency f = values().find { it.subscriptionFrequency == frequency }
+        if (f == null) throw new Exception("Unknown frequency type: ${frequency}")
+        f
+    }
+
+    String value() {
+        subscriptionFrequency
+    }
 }
