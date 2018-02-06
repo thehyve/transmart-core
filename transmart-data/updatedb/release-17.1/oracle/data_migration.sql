a38a4cd577922d03a0eb006aff75def7678a5aa0
Ewelina Grudzien <ewelina@thehyve.nl>
2018-02-06 12:01:40 +0100

    Update db migration scripts for 17.1 release
    - move scripts from a dev folder to the 17.1 folder
    - update description of the changes in README files

--- transmart-data/updatedb/release-17.1/oracle/data_migration.sql
+++ transmart-data/updatedb/release-17.1/oracle/data_migration.sql
@@ -311,3 +311,23 @@ UPDATE I2B2DEMODATA.OBSERVATION_FACT SET TRIAL_VISIT_NUM = (
 WHERE concept_cd in (select concept_cd from concept_specific_trials);
 
 DROP TABLE concept_specific_trials;
+
+
+-- Update visual attributes of leaf nodes to include variable type
+
+update i2b2metadata.i2b2_secure set c_visualattributes = 'LAC'
+where (c_visualattributes = 'LA ' or c_visualattributes = 'LA') and
+(c_metadataxml is null or not c_metadataxml like '%<Oktousevalues>Y</Oktousevalues>%');
+
+update i2b2metadata.i2b2_secure set c_visualattributes = 'LAN'
+where (c_visualattributes = 'LA ' or c_visualattributes = 'LA') and
+c_metadataxml like '%<Oktousevalues>Y</Oktousevalues>%';
+
+-- Add study blob column
+alter table i2b2demodata.study add column study_blob clob;
+
+-- Add reference to tag options in the tags table
+ALTER TABLE "I2B2METADATA"."I2B2_TAGS" ADD COLUMN "TAG_OPTION_ID" NUMBER(18,0);
+
+ALTER TABLE "I2B2METADATA"."I2B2_TAGS" ADD CONSTRAINT "I2B2_TAGS_OPTION_ID_FK" FOREIGN KEY ("TAG_OPTION_ID")
+ REFERENCES "I2B2METADATA"."I2B2_TAG_OPTIONS" ("TAG_OPTION_ID") ON DELETE SET NULL ENABLE;
