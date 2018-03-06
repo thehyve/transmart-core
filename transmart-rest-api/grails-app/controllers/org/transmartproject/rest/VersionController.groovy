abb4ad141f946510793ecca1660a041d54a4d149
Ruslan Forostianov <ruslan@thehyve.nl>
2018-03-06 14:05:09 +0100

    Set snapshot version and repo.

--- transmart-rest-api/grails-app/controllers/org/transmartproject/rest/VersionController.groovy
+++ transmart-rest-api/grails-app/controllers/org/transmartproject/rest/VersionController.groovy
@@ -62,7 +62,7 @@ class VersionController {
                 version: '1.1'
             ],[
                 id: 'v2',
-                version: '2.0',
+                version: '2.1-dev',
                 features: [
                         'versioning',
                         'and-or-constraints',
