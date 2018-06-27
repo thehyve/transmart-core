2b849e089fd0d3ef2a7c0584c4ad748388eba674
ewelinagr <ewelina@thehyve.nl>
2018-06-27 17:08:22 +0200

    Change transmart-notifications default configuration, add configurable trigger time

--- transmart-notifications/grails-app/jobs/org/transmart/notifications/QuerySetSubscriptionDailyJob.groovy
+++ transmart-notifications/grails-app/jobs/org/transmart/notifications/QuerySetSubscriptionDailyJob.groovy
@@ -1,7 +1,8 @@
 package org.transmart.notifications
 
+import grails.util.Holders
 import groovy.util.logging.Slf4j
-import org.transmart.notifications.QuerySetSubscriptionMailService
+import org.quartz.impl.jdbcjobstore.InvalidConfigurationException
 import org.transmartproject.core.userquery.SubscriptionFrequency
 
 /**
@@ -12,22 +13,50 @@ import org.transmartproject.core.userquery.SubscriptionFrequency
 @Slf4j
 class QuerySetSubscriptionDailyJob {
 
+    static jobEnabled =  Holders.config.org.transmart.notifications.enabled
+
     QuerySetSubscriptionMailService querySetSubscriptionMailService
+
+    /**
+     * Parse org.transmart.notifications.dailyJobTriggerTime setting specified in the configuration
+     * Format of the setting is hh-mm, which represent hour and minutes, separated by '-'.
+     * Hours and minutes have to be numbers from a proper range (0-23 and 0-59 respectively).
+     * Specified setting of hour and minutes is changed to a proper cron expression that will be triggered daily.
+     * If the specified setting is invalid, the default one will be set, which is 00:00 every day.
+     *
+     * @return valid cron expression for a daily job
+     */
+    private static String parseConfiguredTriggerTime() {
+        try {
+            def (hour, minute) = Holders.config.org.transmart.notifications.dailyJobTriggerTime.tokenize('-')
+            def validMinute = minute as Integer
+            def validHour = hour as Integer
+            if (validMinute >= 0 && validMinute <= 59 && validHour >= 0 && validHour <= 23){
+                return " 0 $minute $hour 1/1 * ?"
+            } else {
+                throw new InvalidConfigurationException('Property out of allowed range.')
+            }
+        } catch (Exception e) {
+            throw new InvalidConfigurationException(
+                    "Invalid trigger time setting in transmart-notifications configuration. $e.message")
+        }
+    }
+
     /**
      * Specifies a cron expression for job trigger
-     cronExpression: "s m h D M W Y"
-         Second, 0-59;
-         Minute, 0-59;
-         Hour, 0-23;
-         Day of Month, 1-31, ?;
-         Month, 1-12 or JAN-DEC;
-         Day of Week, 1-7 or SUN-SAT, ?;
-         Year [optional]
+         cronExpression: "s m h D M W Y"
+             Second, 0-59;
+             Minute, 0-59;
+             Hour, 0-23;
+             Day of Month, 1-31, ?;
+             Month, 1-12 or JAN-DEC;
+             Day of Week, 1-7 or SUN-SAT, ?;
+             Year [optional]
       Either Day-of-Week or Day-of-Month must be "?".
       Can't specify both fields, nor leave both as the all values wildcard "*"
      **/
     static triggers = {
-        cron name: 'dailySubscriptionTrigger', cronExpression: " 0 0 0 1/1 * ?"
+        cron name: 'dailySubscriptionTrigger', cronExpression: parseConfiguredTriggerTime()
     }
 
     /**
