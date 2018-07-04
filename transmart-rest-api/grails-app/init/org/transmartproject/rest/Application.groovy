295e0992b41c6289d7ba6f52cb154ef699bbbc69
Ruslan Forostianov <ruslan@thehyve.nl>
2018-07-04 13:59:34 +0200

    Move mocking of test user to separate config. file.

--- transmart-rest-api/grails-app/init/org/transmartproject/rest/Application.groovy
+++ transmart-rest-api/grails-app/init/org/transmartproject/rest/Application.groovy
@@ -3,39 +3,12 @@ package org.transmartproject.rest
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 import grails.plugins.metadata.PluginSource
-import grails.util.Environment
-import org.springframework.beans.factory.annotation.Autowired
-import org.transmartproject.core.users.User
-import org.transmartproject.core.users.UsersResource
-import org.transmartproject.rest.user.AuthContext
 
 @PluginSource
 class Application extends GrailsAutoConfiguration {
 
     static void main(String[] args) {
         GrailsApp.run(Application, args)
-    }
-
-    @Override
-    Closure doWithSpring() {
-        if (Environment.current == Environment.TEST) {
-            return { ->
-                //Override the current user with test one
-                authContext(TestAuthContext)
-            }
-        }
-    }
-
-    static class TestAuthContext extends AuthContext {
-
-        @Autowired
-        UsersResource usersResource
-
-        @Delegate
-        @Lazy
-        User user = {
-            usersResource.getUserFromUsername('user_-301')
-        }()
     }
 }
 
