28ca0f29d7b4ba60dafa22b7c86422e521341bdc
Gijs Kant <gijs@thehyve.nl>
2018-09-13 16:02:18 +0200

    Fix syntax error in database update script for observations index.

--- transmart-data/updatedb/release-17.1-HYVE-5/postgres/observation_fact_idx_pkey_and_trial_visit.sql
+++ transmart-data/updatedb/release-17.1-HYVE-5/postgres/observation_fact_idx_pkey_and_trial_visit.sql
@@ -1,2 +1,2 @@
 -- Index on observation_fact that includes the primary key and the trial_visit_num column.
-create observation_fact_tmceppis_idx on i2b2demodata.observation_fact using btree (trial_visit_num, modifier_cd, concept_cd, encounter_num, patient_num, provider_id, instance_num, start_date);
+create index observation_fact_tmceppis_idx on i2b2demodata.observation_fact using btree (trial_visit_num, modifier_cd, concept_cd, encounter_num, patient_num, provider_id, instance_num, start_date);
