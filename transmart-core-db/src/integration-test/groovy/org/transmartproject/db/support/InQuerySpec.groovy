1c2ebb58a304a2e35b66dcb94a59fef84f60d577
Ruslan Forostianov <ruslan@thehyve.nl>
2019-01-09 11:08:24 +0100

    Add CSR test study

--- transmart-core-db/src/integration-test/groovy/org/transmartproject/db/support/InQuerySpec.groovy
+++ transmart-core-db/src/integration-test/groovy/org/transmartproject/db/support/InQuerySpec.groovy
@@ -4,6 +4,7 @@ import grails.orm.HibernateCriteriaBuilder
 import grails.test.mixin.integration.Integration
 import grails.transaction.Rollback
 import org.transmartproject.db.i2b2data.PatientDimension
+import org.transmartproject.db.i2b2data.PatientMapping
 import spock.lang.Specification
 import spock.lang.Unroll
 
@@ -14,22 +15,32 @@ import java.util.function.Supplier
 class InQuerySpec extends Specification {
 
     @Unroll
-    void 'test postgres query splitting for list of #values.size() values'(Integer expectedCount, List<Long> values) {
+    void 'test listIn fetches requested entities'(Integer expectedCount, List<Long> values) {
+        when: 'Querying for patient mappings'
+        Supplier<HibernateCriteriaBuilder> criteriaBuilderProducer = { ->
+            PatientMapping.createCriteria() as HibernateCriteriaBuilder
+        }
+        List result = InQuery.listIn(criteriaBuilderProducer, 'encryptedId', values)
+
+        then: 'The expected amount of patient mappings is returned'
+        result.size() == expectedCount
+
+        where:
+        expectedCount | values
+        0             | []
+        1             | ['CV:40']
+        3             | ['CT:41', 'EHR:42', 'TNS:43']
+    }
+
+    void 'test listIn gets actual patients by wide range of ids'() {
         when: 'Querying for patients'
         Supplier<HibernateCriteriaBuilder> criteriaBuilderProducer = { ->
             PatientDimension.createCriteria() as HibernateCriteriaBuilder
         }
-        List result = InQuery.listIn(criteriaBuilderProducer, 'id', values)
+        List result = InQuery.listIn(criteriaBuilderProducer, 'id', (-20000L..20000L))
 
-        then: 'The expected amount of objects is returned'
-        result.size() == expectedCount
-
-        where: 'sizes range from 0 to more than 30000'
-        expectedCount | values
-        0             | []
-        1             | [-40L]
-        3             | [-41L, -42L, -43L]
-        1250          | (-20000L .. 20000L)
+        then: 'The actual amount of patient is returned'
+        result.size() == PatientDimension.count()
     }
 
 }
