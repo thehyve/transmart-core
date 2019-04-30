3b49a9fb6a93ea88ed83bfa3a223169e552044e1
ewelinagr <ewelina@thehyve.nl>
2019-04-30 15:21:53 +0200

    Fix disabling of notifications quartz jobs

--- transmart-notifications/grails-app/jobs/org/transmartproject/notifications/QuerySetSubscriptionDailyJob.groovy
+++ transmart-notifications/grails-app/jobs/org/transmartproject/notifications/QuerySetSubscriptionDailyJob.groovy
@@ -13,7 +13,14 @@ import org.transmartproject.core.userquery.SubscriptionFrequency
 @Slf4j
 class QuerySetSubscriptionDailyJob {
 
-    static jobEnabled =  Holders.config.org.transmartproject.notifications.enabled
+    /**
+     * Quartz plugin configuration option.
+     * By default all jobs are considered enabled. Setting this property to false can disable the job.
+     *
+     * Value of this option is controlled in the notifications plugin descriptor.
+     * @see {@link org.transmartproject.notifications.TransmartNotificationsGrailsPlugin}
+     */
+    static jobEnabled
 
     QuerySetSubscriptionMailService querySetSubscriptionMailService
 
