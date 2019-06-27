7570996fa909a5de99646f6234f57248845f041a
Gijs Kant <gijs@thehyve.nl>
2019-06-27 14:32:23 +0200

    Fix tests.

--- transmart-core-db/src/integration-test/groovy/org/transmartproject/db/multidimquery/QueryServicePgSpec.groovy
+++ transmart-core-db/src/integration-test/groovy/org/transmartproject/db/multidimquery/QueryServicePgSpec.groovy
@@ -59,204 +59,6 @@ class QueryServicePgSpec extends Specification {
         UTC_DATE_FORMAT.setTimeZone(TimeZone.getTimeZone('UTC'))
     }
 
-    void 'get whole hd data for single node'() {
-        User user = User.findByUsername('test-public-user-1')
-        ConceptConstraint conceptConstraint = new ConceptConstraint(path: '\\Public Studies\\CLINICAL_TRIAL_HIGHDIM\\High Dimensional data\\Expression Lung\\')
-
-        when:
-        Hypercube hypercube = multiDimService.highDimension(conceptConstraint, user, 'autodetect')
-
-        then:
-        hypercube.toList().size() == hypercube.dimensionElements(BIOMARKER).size() *
-                hypercube.dimensionElements(ASSAY).size() *
-                hypercube.dimensionElements(PROJECTION).size()
-        hypercube.dimensionElements(BIOMARKER).size() == 3
-        hypercube.dimensionElements(ASSAY).size() == 6
-        hypercube.dimensionElements(PROJECTION).size() == 10
-
-        cleanup:
-        if (hypercube) hypercube.close()
-    }
-
-    void 'get hd data for selected patients'() {
-        User user = User.findByUsername('test-public-user-1')
-        ConceptConstraint conceptConstraint = new ConceptConstraint(path: '\\Public Studies\\CLINICAL_TRIAL_HIGHDIM\\High Dimensional data\\Expression Lung\\')
-        def malesIn26 = org.transmartproject.db.i2b2data.PatientDimension.find {
-            sourcesystemCd == 'CLINICAL_TRIAL_HIGHDIM:1'
-        }
-        Constraint assayConstraint = new PatientSetConstraint(patientIds: malesIn26*.id)
-        Constraint combinationConstraint = new AndConstraint([conceptConstraint, assayConstraint])
-
-        when:
-        Hypercube hypercube = multiDimService.highDimension(combinationConstraint, user, 'autodetect')
-
-        then:
-        hypercube.toList().size() == hypercube.dimensionElements(BIOMARKER).size() *
-                hypercube.dimensionElements(ASSAY).size() *
-                hypercube.dimensionElements(PROJECTION).size()
-        hypercube.dimensionElements(BIOMARKER).size() == 3
-        hypercube.dimensionElements(ASSAY).size() == 2
-        hypercube.dimensionElements(PROJECTION).size() == 10
-
-        cleanup:
-        if (hypercube) hypercube.close()
-    }
-
-    void 'get hd data for selected biomarkers'() {
-        def user = User.findByUsername('test-public-user-1')
-        ConceptConstraint conceptConstraint = new ConceptConstraint(path: '\\Public Studies\\CLINICAL_TRIAL_HIGHDIM\\High Dimensional data\\Expression Lung\\')
-        BiomarkerConstraint bioMarkerConstraint = new BiomarkerConstraint(
-                biomarkerType: DataConstraint.GENES_CONSTRAINT,
-                params: [
-                        names: ['TP53']
-                ]
-        )
-
-        when:
-        Hypercube hypercube = multiDimService.highDimension(conceptConstraint, bioMarkerConstraint, user, 'mrna')
-
-        then:
-        hypercube.toList().size() == hypercube.dimensionElements(BIOMARKER).size() *
-                hypercube.dimensionElements(ASSAY).size() *
-                hypercube.dimensionElements(PROJECTION).size()
-        hypercube.dimensionElements(BIOMARKER).size() == 2
-        hypercube.dimensionElements(ASSAY).size() == 6
-        hypercube.dimensionElements(PROJECTION).size() == 10
-
-        cleanup:
-        if (hypercube) hypercube.close()
-    }
-
-    void 'get hd data for selected trial visit dimension'() {
-        def user = User.findByUsername('test-public-user-1')
-        def conceptConstraint = new ConceptConstraint(path: '\\Public Studies\\CLINICAL_TRIAL_HIGHDIM\\High Dimensional data\\Expression Lung\\')
-        def trialVisitConstraint = new FieldConstraint(
-                field: new Field(
-                        dimension: TRIAL_VISIT.name,
-                        fieldName: 'relTimeLabel',
-                        type: 'STRING'
-                ),
-                operator: Operator.EQUALS
-        )
-        def combination
-
-        when:
-        trialVisitConstraint.value = 'Baseline'
-        combination = new AndConstraint([conceptConstraint, trialVisitConstraint])
-        Hypercube hypercube = multiDimService.highDimension(combination, user, 'autodetect')
-        hypercube.toList()
-
-        then:
-        hypercube.dimensionElements(BIOMARKER).size() == 3
-        hypercube.dimensionElements(ASSAY).size() == 4
-        hypercube.dimensionElements(PROJECTION).size() == 10
-
-        when:
-        trialVisitConstraint.value = 'Week 1'
-        combination = new Combination(Operator.AND, [conceptConstraint, trialVisitConstraint])
-        hypercube = multiDimService.highDimension(combination, user, 'autodetect')
-        hypercube.toList()
-
-        then:
-
-        hypercube.dimensionElements(ASSAY).size() == 1
-        def patient = hypercube.dimensionElement(PATIENT, 0)
-        patient.subjectIds.get('SUBJ_ID') == 'CTHD:601'
-        patient.age == 26
-
-        cleanup:
-        if (hypercube) hypercube.close()
-    }
-
-    void 'get hd data for selected time constraint'() {
-        def user = User.findByUsername('test-public-user-2')
-        def conceptConstraint = new ConceptConstraint(path: '\\Public Studies\\EHR_HIGHDIM\\High Dimensional data\\Expression Lung\\')
-        def startDateTimeConstraint = new TimeConstraint(
-                field: new Field(
-                        dimension: START_TIME.name,
-                        fieldName: 'startDate',
-                        type: 'DATE'
-                ),
-                values: [DATE_FORMAT.parse('2016-03-29 10:30:30')],
-                operator: Operator.AFTER
-        )
-        def startDateTimeInclusiveConstraint = new TimeConstraint(
-                field: new Field(
-                        dimension: START_TIME.name,
-                        fieldName: 'startDate',
-                        type: 'DATE'
-                ),
-                values: [DATE_FORMAT.parse('2016-03-29 10:30:30')],
-                operator: Operator.GREATER_THAN_OR_EQUALS
-        )
-
-        def endDateTimeConstraint = new TimeConstraint(
-                field: new Field(
-                        dimension: END_TIME.name,
-                        fieldName: 'endDate',
-                        type: 'DATE'
-                ),
-                values: [DATE_FORMAT.parse('2016-04-02 00:00:00')],
-                operator: Operator.BEFORE
-        )
-        def endDateTimeInclusiveConstraint = new TimeConstraint(
-                field: new Field(
-                        dimension: END_TIME.name,
-                        fieldName: 'endDate',
-                        type: 'DATE'
-                ),
-                values: [DATE_FORMAT.parse('2016-04-02 00:00:00')],
-                operator: Operator.LESS_THAN_OR_EQUALS
-        )
-
-        def combination
-        Hypercube hypercube
-        when:
-        combination = new Combination(Operator.AND, [conceptConstraint, startDateTimeConstraint])
-        hypercube = multiDimService.highDimension(combination, user, 'autodetect')
-        hypercube.toList()
-
-        then:
-        hypercube.dimensionElements(ASSAY).size() == 3
-
-        when:
-        combination = new Combination(Operator.AND, [conceptConstraint, endDateTimeConstraint])
-        hypercube = multiDimService.highDimension(combination, user, 'autodetect')
-
-        then:
-        hypercube.toList().empty
-
-        when:
-        combination = new Combination(Operator.AND, [conceptConstraint, startDateTimeInclusiveConstraint])
-        hypercube = multiDimService.highDimension(combination, user, 'autodetect')
-        hypercube.toList()
-
-        then:
-        hypercube.dimensionElements(ASSAY).size() == 4
-
-        when:
-        combination = new Combination(Operator.AND, [conceptConstraint, endDateTimeInclusiveConstraint])
-        hypercube = multiDimService.highDimension(combination, user, 'autodetect')
-        hypercube.toList()
-
-        then:
-        hypercube.dimensionElements(ASSAY).size() == 1
-
-        cleanup:
-        if (hypercube) hypercube.close()
-    }
-
-    void "get hd data types"() {
-        User user = User.findByUsername('test-public-user-1')
-        ConceptConstraint conceptConstraint = new ConceptConstraint(path: '\\Public Studies\\CLINICAL_TRIAL_HIGHDIM\\High Dimensional data\\Expression Lung\\')
-
-        when:
-        def result = multiDimService.retrieveHighDimDataTypes(conceptConstraint, user)
-        then:
-        result != null
-        result.contains("mrna")
-    }
-
     void 'Clinical data selected on visit dimension'() {
         def user = User.findByUsername('test-public-user-1')
         def minDate = DATE_FORMAT.parse('2016-04-01 10:00:00')
@@ -282,35 +84,6 @@ class QueryServicePgSpec extends Specification {
         hypercube.dimensionElements(VISIT).each {
             assert (it.getAt('startDate') as Date) > minDate
         }
-
-        cleanup:
-        if (hypercube) hypercube.close()
-    }
-
-    void 'HD data selected on visit dimension'() {
-        def user = User.findByUsername('test-public-user-1')
-        def timeDimensionConstraint = new FieldConstraint(
-                operator: Operator.AFTER,
-                value: DATE_FORMAT.parse('2016-05-05 10:00:00'),
-                field: new Field(
-                        dimension: VISIT.name,
-                        fieldName: 'endDate',
-                        type: 'DATE'
-                )
-        )
-        def conceptConstraint = new ConceptConstraint(
-                path: '\\Public Studies\\EHR_HIGHDIM\\High Dimensional data\\Expression Lung\\'
-        )
-        def combination = new Combination(
-                Operator.AND,
-                [timeDimensionConstraint, conceptConstraint]
-        )
-        when:
-        Hypercube hypercube = multiDimService.highDimension(combination, user, 'autodetect')
-        hypercube.toList()
-
-        then:
-        hypercube.dimensionElements(ASSAY).size() == 1
 
         cleanup:
         if (hypercube) hypercube.close()
@@ -347,30 +120,6 @@ class QueryServicePgSpec extends Specification {
         if (hypercube) hypercube.close()
     }
 
-    void 'Test for empty set of assayIds'() {
-        def user = User.findByUsername('test-public-user-1')
-        def conceptConstraint = new ConceptConstraint(path: '\\Public Studies\\EHR_HIGHDIM\\High Dimensional data\\Expression Lung\\')
-        def endDateTimeConstraint = new TimeConstraint(
-                field: new Field(
-                        dimension: END_TIME.name,
-                        fieldName: 'endDate',
-                        type: 'DATE'
-                ),
-                values: [DATE_FORMAT.parse('2016-04-02 01:00:00')],
-                operator: Operator.AFTER //only exist one before, none after
-        )
-        when:
-        Combination combination = new Combination(Operator.AND, [conceptConstraint, endDateTimeConstraint])
-        Hypercube hypercube = multiDimService.highDimension(combination, user, 'autodetect')
-
-
-        then:
-        hypercube.toList().empty
-
-        cleanup:
-        if (hypercube) hypercube.close()
-    }
-
     void 'get data for the patient with given id'() {
         def user = User.findByUsername('test-public-user-1')
         Constraint subjectIdConstraint = new PatientSetConstraint(subjectIds: ['2'])
@@ -381,67 +130,6 @@ class QueryServicePgSpec extends Specification {
         then:
         hypercube.dimensionElements(PATIENT).size() == 1
         hypercube.dimensionElements(PATIENT).first().subjectIds.get('SUBJ_ID') == '2'
-
-        cleanup:
-        if (hypercube) hypercube.close()
-    }
-
-    void 'get transcript data for selected patients and selected transcripts'() {
-        def user = User.findByUsername('test-public-user-1')
-        ConceptConstraint conceptConstraint = new ConceptConstraint(path: '\\Public Studies\\RNASEQ_TRANSCRIPT\\HD\\Breast\\')
-        def secondSubject = org.transmartproject.db.i2b2data.PatientDimension.find {
-            sourcesystemCd == 'RNASEQ_TRANSCRIPT:2'
-        }
-        Constraint assayConstraint = new PatientSetConstraint(patientIds: secondSubject*.id)
-        Constraint combinationConstraint = new Combination(
-                Operator.AND,
-                [
-                        conceptConstraint,
-                        assayConstraint
-                ]
-        )
-        BiomarkerConstraint bioMarkerConstraint = new BiomarkerConstraint(
-                biomarkerType: DataConstraint.TRANSCRIPTS_CONSTRAINT,
-                params: [
-                        names: ['tr2']
-                ]
-        )
-
-        when:
-        Hypercube hypercube = multiDimService.highDimension(combinationConstraint, bioMarkerConstraint, user, 'rnaseq_transcript')
-
-        then:
-        hypercube.toList().size() == hypercube.dimensionElements(BIOMARKER).size() *
-                hypercube.dimensionElements(ASSAY).size() *
-                hypercube.dimensionElements(PROJECTION).size()
-        hypercube.dimensionElements(BIOMARKER).size() == 1
-        hypercube.dimensionElements(ASSAY).size() == 1
-        hypercube.dimensionElements(PROJECTION).size() == 13
-
-        cleanup:
-        if (hypercube) hypercube.close()
-    }
-
-    void 'get transcript data for selected genes'() {
-        def user = User.findByUsername('test-public-user-1')
-        ConceptConstraint conceptConstraint = new ConceptConstraint(path: '\\Public Studies\\RNASEQ_TRANSCRIPT\\HD\\Breast\\')
-        BiomarkerConstraint bioMarkerConstraint = new BiomarkerConstraint(
-                biomarkerType: DataConstraint.GENES_CONSTRAINT,
-                params: [
-                        names: ['AURKA']
-                ]
-        )
-
-        when:
-        Hypercube hypercube = multiDimService.highDimension(conceptConstraint, bioMarkerConstraint, user, 'rnaseq_transcript')
-
-        then:
-        hypercube.toList().size() == hypercube.dimensionElements(BIOMARKER).size() *
-                hypercube.dimensionElements(ASSAY).size() *
-                hypercube.dimensionElements(PROJECTION).size()
-        hypercube.dimensionElements(BIOMARKER).size() == 1
-        hypercube.dimensionElements(ASSAY).size() == 3
-        hypercube.dimensionElements(PROJECTION).size() == 13
 
         cleanup:
         if (hypercube) hypercube.close()
@@ -642,6 +330,7 @@ class QueryServicePgSpec extends Specification {
 
         when:
         def values = multiDimService.retrieveClinicalData(constraint, user).asList()*.value
+        values.sort()
 
         then:
         values.size() == 2
