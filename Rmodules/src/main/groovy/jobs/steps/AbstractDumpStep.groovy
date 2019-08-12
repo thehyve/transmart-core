58522e9b47fa9fd9b811d5c85766ea7fe3eaa662
Gijs Kant <gijs@thehyve.nl>
2019-08-13 00:10:01 +0200

    Remove Rmodules.

--- Rmodules/src/main/groovy/jobs/steps/AbstractDumpStep.groovy
+++
@@ -1,9 +0,0 @@
-package jobs.steps
-
-abstract class AbstractDumpStep implements Step {
-
-    static final String DEFAULT_OUTPUT_FILE_NAME = 'outputfile.txt'
-
-    String outputFileName = DEFAULT_OUTPUT_FILE_NAME
-
-}
