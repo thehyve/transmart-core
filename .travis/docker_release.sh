e03d378f51e9e14bd02a85bec6de8d44de5f44cc
Gijs Kant <gijs@thehyve.nl>
2020-06-05 21:44:54 +0200

    Fix Docker release script for Travis.

--- .travis/docker_release.sh
+++ .travis/docker_release.sh
@@ -2,7 +2,7 @@
 
 here=$(dirname "${0}")
 pushd "${here}/.."
-TRANSMART_VERSION=$(gradle properties | grep '^version: ' - | awk '{print $2}')
+TRANSMART_VERSION=$(${GRADLE_SCRIPT} properties | grep '^version: ' - | awk '{print $2}')
 popd
 
 docker build --build-arg "TRANSMART_VERSION=${TRANSMART_VERSION}" -t "thehyve/transmart-api-server:${TRANSMART_VERSION}" "${here}/../docker/transmart-api-server" && \
